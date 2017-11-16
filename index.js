394cbfd10f1989eacc9917d830070de4dae9fedf
Nicola Molinari <nicola.molinari@commercetools.de>
2017-11-16 10:16:32 +0100

    chore(packages/constants): define core/constants as a dependency

---
+++ index.js
@@ -0,0 +1,98 @@
+// STORAGE
+export const STORAGE_KEYS = {
+  TOKEN: 'token',
+};
+
+// PLUGINS
+export const ACTIVATE_PLUGIN = 'ACTIVATE_PLUGIN';
+export const DEACTIVATE_PLUGIN = 'DEACTIVATE_PLUGIN';
+export const INIT_PLUGIN = 'INIT_PLUGIN';
+export const __LOCAL = 'LOCAL';
+export const __GLOBAL = 'GLOBAL';
+
+// APPLICATION STATE
+export const LOGGED_IN = 'LOGGED_IN';
+export const LOGGED_OUT = 'LOGGED_OUT';
+export const SWITCH_LOCALE = 'SWITCH_LOCALE';
+export const SWITCH_PROJECT_LANGUAGE = 'SWITCH_PROJECT_LANGUAGE';
+export const USER_CHANGED = 'USER_CHANGED';
+export const FORCE_MENU_TOGGLE = 'FORCE_MENU_TOGGLE';
+
+// NOTIFICATIONS
+export const SHOW_LOADING = 'SHOW_LOADING';
+export const HIDE_LOADING = 'HIDE_LOADING';
+export const HIDE_ALL_PAGE_NOTIFICATIONS = 'HIDE_ALL_PAGE_NOTIFICATIONS';
+export const DOMAINS = {
+  GLOBAL: 'global',
+  PAGE: 'page',
+  SIDE: 'side',
+};
+
+// ERRORS
+export const UNAUTHORIZED = 'UNAUTHORIZED';
+
+// CORE ACTIONS
+export const INITIALIZE_SEARCH_SLICE = 'INITIALIZE_SEARCH_SLICE';
+export const SET_SEARCH_FILTER_STATE = 'SET_SEARCH_FILTER_STATE';
+export const SET_SEARCH_FILTER_RESULT = 'SET_SEARCH_FILTER_RESULT';
+export const REMOVE_ENTITY_BY_ID_FROM_SEARCH_RESULTS =
+  'REMOVE_ENTITY_BY_ID_FROM_SEARCH_RESULTS';
+export const PRODUCTS_REFERENCE_SEARCH_FETCHED =
+  'PRODUCTS_REFERENCE_SEARCH_FETCHED';
+export const CATEGORIES_REFERENCE_SEARCH_FETCHED =
+  'CATEGORIES_REFERENCE_SEARCH_FETCHED';
+
+// MANAGEMENT
+export const INITIAL_STATE_FETCHED = 'INITIAL_STATE_FETCHED';
+
+// IMPEX
+export const IMPORT_JOB_CREATED = 'IMPORT_JOB_CREATED';
+export const EXPORT_JOB_CREATED = 'EXPORT_JOB_CREATED';
+
+// NO VALUE FALLBACK
+export const NO_VALUE_FALLBACK = '- - - -';
+
+export const PRODUCT_STATUSES = {
+  PUBLISHED: 'published',
+  UNPUBLISHED: 'unpublished',
+  MODIFIED: 'modified',
+};
+
+export const PRODUCT_ACTIONS = {
+  PUBLISH: 'publish',
+  UNPUBLISH: 'unpublish',
+};
+
+export const STATUS_CODES = {
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  UNAUTHENTICATED: 299,
+  NOT_FOUND: 404,
+};
+
+export const LOGOUT_REASONS = {
+  NO_PROJECTS: 'no-projects',
+  USER: 'user',
+  UNAUTHORIZED: 'unauthorized',
+  INVALID: 'invalid',
+};
+
+export const MANAGE_PRODUCTS_PERMISSION = {
+  mode: 'manage',
+  resource: 'products',
+};
+
+export const VIEW_PRODUCTS_PERMISSION = {
+  mode: 'view',
+  resource: 'products',
+};
+
+export const MANAGE_CUSTOMERS_PERMISSION = {
+  mode: 'manage',
+  resource: 'customers',
+};
+
+export const MANAGE_ORDERS_PERMISSION = {
+  mode: 'manage',
+  resource: 'orders',
+};
