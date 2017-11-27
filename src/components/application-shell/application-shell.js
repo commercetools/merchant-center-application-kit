30f44eaefa192e3cabb85b82773552e1ecfe2769
Nicola Molinari <nicola.molinari@commercetools.de>
2017-11-27 22:00:54 +0100

    refactor(app-shell/intl): pass to IntlProvider the user locale or guess the one from the browser

--- src/components/application-shell/application-shell.js
+++ src/components/application-shell/application-shell.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { IntlProvider } from 'react-intl';
 import { ApolloProvider } from 'react-apollo';
 import apolloClient from '../../configure-apollo';
 import Authenticated from '../authenticated';
+import LocaleProvider from '../locale-provider';
 import AppBar from '../app-bar';
 import ProjectContainer from '../project-container';
-
-const ProtectedRoutes = () => (
-  <Authenticated>
-    <AppBar />
-
-    <Switch>
-      {/* Non-project routes */}
-      <Route path="/profile" render={() => <div>{'PROFILE VIEW'}</div>} />
-      <Route path="/organizations" render={() => <div>{'ORGS VIEW'}</div>} />
-
-      {/* Project routes */}
-      <Route path="/:projectKey" component={ProjectContainer} />
-      <Route render={() => <div>{'Another route'}</div>} />
-    </Switch>
-  </Authenticated>
-);
-ProtectedRoutes.displayName = 'ProtectedRoutes';
 
 export default class ApplicationShell extends React.PureComponent {
   static displayName = 'ApplicationShell';
@@ -32,38 +15,54 @@ export default class ApplicationShell extends React.PureComponent {
     children: PropTypes.element.isRequired,
   };
   render() {
-    /**
-     * TODO: fix locale value for <IntlProvider>.
-     *
-     * The `locale` comes from the `user`. However the user is only
-     * fetched if the route is protected.
-     * I guess we have two options:
-     *
-     * 1. render <IntlProvider> in the public routes with a "hardcoded" locale
-     * (maybe even guess it from the user's browser). Then render a separate
-     * <IntlProvider> in protected routes, wrapped to a <WithUser>.
-     *
-     * 2. have a <WithIntlProvider> component that keeps a state of a locale,
-     * which might be the default, and updates it with a setter function which
-     * is called by a child component after the user has been loaded.
-     *
-     * @emmenko: I prefer more the first one, it's a bit verbose but simpler
-     */
-
     return (
-      <IntlProvider locale={'en'} messages={this.props.i18n.en}>
-        <ApolloProvider client={apolloClient}>
-          <Router>
-            <Switch>
-              {/* Public routes */}
-              <Route path="/login" render={() => <div>{'LOGIN PAGE'}</div>} />
+      <ApolloProvider client={apolloClient}>
+        <Router>
+          <Switch>
+            {/* Public routes */}
+            <Route
+              path="/login"
+              render={() => (
+                <LocaleProvider i18n={this.props.i18n}>
+                  <div>{'LOGIN PAGE'}</div>
+                </LocaleProvider>
+              )}
+            />
 
-              {/* Protected routes */}
-              <Route component={ProtectedRoutes} />
-            </Switch>
-          </Router>
-        </ApolloProvider>
-      </IntlProvider>
+            {/* Protected routes */}
+            <Route
+              render={() => (
+                <Authenticated>
+                  <LocaleProvider i18n={this.props.i18n}>
+                    <div>
+                      <AppBar />
+
+                      <Switch>
+                        {/* Non-project routes */}
+                        <Route
+                          path="/profile"
+                          render={() => <div>{'PROFILE VIEW'}</div>}
+                        />
+                        <Route
+                          path="/organizations"
+                          render={() => <div>{'ORGS VIEW'}</div>}
+                        />
+
+                        {/* Project routes */}
+                        <Route
+                          path="/:projectKey"
+                          component={ProjectContainer}
+                        />
+                        <Route render={() => <div>{'Another route'}</div>} />
+                      </Switch>
+                    </div>
+                  </LocaleProvider>
+                </Authenticated>
+              )}
+            />
+          </Switch>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
