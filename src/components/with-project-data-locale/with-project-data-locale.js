196a17af0d3f0c191f8bb6bae6d54592c2fe49d2
Nicola Molinari <nicola.molinari@commercetools.de>
2017-11-27 11:41:43 +0100

    test(app-shell/with-project-data-locale): add tests

--- src/components/with-project-data-locale/with-project-data-locale.js
+++ src/components/with-project-data-locale/with-project-data-locale.js
@@ -5,21 +5,26 @@ import * as storage from '@commercetools-local/utils/storage';
 
 const defaultLocale = 'en';
 
-const getSelectedDataLocaleForProject = projectLanguages => {
+const getSelectedDataLocaleForProject = projectLocales => {
   // In case the project is not present (e.g. 404), we just return `en`
   // as a fallback value but it doesn't really matter at end what the
   // value is. When the user access a proper project, this value will
   // be correctly picked.
-  if (!projectLanguages) return defaultLocale;
-  const cachedLanguage = storage.get(CORE_STORAGE_KEYS.SELECTED_DATA_LOCALE);
-  // Make sure the cached language is listed in the selected project
-  const isCachedLanguageIncludedInProjectLanguages = projectLanguages.includes(
-    cachedLanguage
+  if (!projectLocales) return defaultLocale;
+  const cachedDataLocale = storage.get(CORE_STORAGE_KEYS.SELECTED_DATA_LOCALE);
+  // Make sure the cached locale is listed in the selected project
+  const isCachedDataLocaleIncludedInProjectLanguages = projectLocales.includes(
+    cachedDataLocale
   );
-  if (isCachedLanguageIncludedInProjectLanguages) return cachedLanguage;
-  const defaultProjectLanguage = projectLanguages[0];
-  storage.put(CORE_STORAGE_KEYS.SELECTED_DATA_LOCALE, defaultProjectLanguage);
-  return defaultProjectLanguage;
+  if (isCachedDataLocaleIncludedInProjectLanguages) return cachedDataLocale;
+  // Pick the first locale from the list
+  const defaultDataLocaleForProject = projectLocales[0];
+  // Cache it
+  storage.put(
+    CORE_STORAGE_KEYS.SELECTED_DATA_LOCALE,
+    defaultDataLocaleForProject
+  );
+  return defaultDataLocaleForProject;
 };
 
 export default class WithProjectDataLocale extends React.PureComponent {
---
+++ src/components/with-project-data-locale/with-project-data-locale.spec.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { STORAGE_KEYS as CORE_STORAGE_KEYS } from '@commercetools-local/constants';
+import * as storage from '@commercetools-local/utils/storage';
+import WithProjectDataLocale from './with-project-data-locale';
+
+jest.mock('@commercetools-local/utils/storage');
+
+const createTestProps = props => ({
+  locales: null,
+  children: jest.fn(),
+  ...props,
+});
+
+describe('rendering', () => {
+  let props;
+  let wrapper;
+
+  describe('when list of locales is not defined', () => {
+    beforeEach(() => {
+      props = createTestProps();
+      wrapper = shallow(<WithProjectDataLocale {...props} />);
+    });
+    it('should call render function with default locale "en"', () => {
+      expect(props.children).toHaveBeenCalledWith(
+        expect.objectContaining({ locale: 'en' })
+      );
+    });
+    it('should call render function with setter function', () => {
+      expect(props.children).toHaveBeenCalledWith(
+        expect.objectContaining({
+          setProjectDataLocale: wrapper.instance().handleSetProjectDataLocale,
+        })
+      );
+    });
+  });
+
+  describe('when list of locales is defined', () => {
+    describe('when locale is not cached yet', () => {
+      beforeEach(() => {
+        props = createTestProps();
+        wrapper = shallow(<WithProjectDataLocale {...props} />);
+        wrapper.setProps({ locales: ['it', 'de'] });
+      });
+      it('should call render function with first locale from the list', () => {
+        expect(props.children).toHaveBeenCalledWith(
+          expect.objectContaining({ locale: 'it' })
+        );
+      });
+    });
+    describe('when locale has been already cached', () => {
+      beforeEach(() => {
+        storage.put(CORE_STORAGE_KEYS.SELECTED_DATA_LOCALE, 'de');
+        props = createTestProps();
+        wrapper = shallow(<WithProjectDataLocale {...props} />);
+        wrapper.setProps({ locales: ['it', 'de'] });
+      });
+      it('should call render function with the locale from the cache', () => {
+        expect(props.children).toHaveBeenCalledWith(
+          expect.objectContaining({ locale: 'de' })
+        );
+      });
+    });
+    describe('when cached locale is not listed in the project locales', () => {
+      beforeEach(() => {
+        storage.put(CORE_STORAGE_KEYS.SELECTED_DATA_LOCALE, 'de');
+        props = createTestProps();
+        wrapper = shallow(<WithProjectDataLocale {...props} />);
+        wrapper.setProps({ locales: ['it', 'en'] });
+      });
+      it('should call render function with first locale from the list', () => {
+        expect(props.children).toHaveBeenCalledWith(
+          expect.objectContaining({ locale: 'it' })
+        );
+      });
+    });
+  });
+});
+
+describe('interactions', () => {
+  let props;
+  let wrapper;
+  describe('updating the project data locale', () => {
+    beforeEach(() => {
+      props = createTestProps();
+      wrapper = shallow(<WithProjectDataLocale {...props} />);
+      wrapper.instance().handleSetProjectDataLocale('de');
+    });
+    it('should update the state with the new locale', () => {
+      expect(wrapper).toHaveState('locale', 'de');
+    });
+  });
+});
