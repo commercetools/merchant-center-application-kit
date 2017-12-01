589b48cb512ba92927083fe71ffbd6cbdff78b4f
philippspo <philipp.sporrer@commercetools.de>
2017-12-01 09:12:54 +0100

    feat(app-shell/dev): add more menu items for testing

--- src/example/for-development.js
+++ src/example/for-development.js
@@ -21,9 +21,88 @@ const testMenuLinks = [
     },
   },
   {
+    name: 'mcng-categories',
+    menu: {
+      name: 'categories',
+      link: 'categories',
+      label: 'Menu.Categories.title',
+      icon: 'CategoryTreeIcon',
+      permissions: [
+        {
+          mode: 'view',
+          resource: 'products',
+        },
+        {
+          mode: 'manage',
+          resource: 'products',
+        },
+      ],
+      submenu: [
+        {
+          name: 'categories-list',
+          label: 'Menu.Categories.list',
+          link: 'categories?mode=list',
+        },
+        {
+          name: 'categories-search',
+          label: 'Menu.Categories.search',
+          link: 'categories?mode=search',
+        },
+        {
+          name: 'categories-add',
+          label: 'Menu.Categories.add',
+          link: 'categories/new',
+          permissions: [
+            {
+              mode: 'manage',
+              resource: 'products',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    name: 'mcng-customers',
+    menu: {
+      name: 'customers',
+      link: 'customers',
+      label: 'Menu.Customers.title',
+      icon: 'CustomerFilledIcon',
+      permissions: [
+        {
+          mode: 'view',
+          resource: 'customers',
+        },
+        {
+          mode: 'manage',
+          resource: 'customers',
+        },
+      ],
+      submenu: [
+        {
+          name: 'customers-list',
+          label: 'Menu.Customers.list',
+          link: 'customers',
+        },
+        {
+          name: 'customers-add',
+          label: 'Menu.Customers.add',
+          link: 'customers/new',
+          permissions: [
+            {
+              mode: 'manage',
+              resource: 'customers',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
     name: 'mcng-orders',
     menu: {
-      name: 'Orders',
+      name: 'orders',
       link: 'orders',
       label: 'Menu.Orders.title',
       icon: 'CartIcon',
@@ -39,15 +118,93 @@ const testMenuLinks = [
       ],
       submenu: [
         {
-          name: 'Orders',
+          name: 'orders-list',
           label: 'Menu.Orders.list',
           link: 'orders',
         },
         {
-          name: 'AddOrders',
+          name: 'orders-add',
           label: 'Menu.Orders.add',
           link: 'orders/new',
           permissions: [
+            {
+              mode: 'manage',
+              resource: 'orders',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    name: 'mcng-discounts',
+    menu: {
+      name: 'discounts',
+      link: 'discounts',
+      label: 'Menu.Discounts.title',
+      icon: 'TagMultiIcon',
+      permissions: [
+        {
+          mode: 'view',
+          resource: 'products',
+        },
+        {
+          mode: 'manage',
+          resource: 'products',
+        },
+        {
+          mode: 'view',
+          resource: 'orders',
+        },
+        {
+          mode: 'manage',
+          resource: 'orders',
+        },
+      ],
+      submenu: [
+        {
+          name: 'product-discounts-list',
+          label: 'Menu.Discounts.ProductDiscounts.list',
+          link: 'discounts/products',
+          permissions: [
+            {
+              mode: 'view',
+              resource: 'products',
+            },
+          ],
+        },
+        {
+          name: 'cart-discounts-list',
+          label: 'Menu.Discounts.CartDiscounts.list',
+          link: 'discounts/carts',
+          permissions: [
+            {
+              mode: 'view',
+              resource: 'orders',
+            },
+          ],
+        },
+        {
+          name: 'discount-codes-list',
+          label: 'Menu.Discounts.DiscountCodes.list',
+          link: 'discounts/codes',
+          featureToggle: 'discountCodes',
+          permissions: [
+            {
+              mode: 'view',
+              resource: 'orders',
+            },
+          ],
+        },
+        {
+          name: 'discounts-add',
+          label: 'Menu.Discounts.add',
+          link: 'discounts/new',
+          permissions: [
+            {
+              mode: 'manage',
+              resource: 'products',
+            },
             {
               mode: 'manage',
               resource: 'orders',
