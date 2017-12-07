950f003a69961a55adf749930d4c7e5e30f39523
Nicola Molinari <nicola.molinari@commercetools.de>
2017-12-07 17:03:45 +0100

    test(app-shell/requests-in-flight-loader): add tests for reducer

---
+++ src/components/requests-in-flight-loader/reducer.spec.js
@@ -0,0 +1,50 @@
+import reducer from './reducer';
+
+describe('initial state', () => {
+  it('should return empty array', () => {
+    expect(reducer()).toEqual([]);
+  });
+});
+
+describe('actions', () => {
+  let state;
+  let action;
+  describe('SHOW_LOADING', () => {
+    beforeEach(() => {
+      state = [1];
+      action = { type: 'SHOW_LOADING', payload: 2 };
+    });
+    it('should append an entry to the list', () => {
+      expect(reducer(state, action)).toEqual([1, 2]);
+    });
+  });
+  describe('HIDE_LOADING', () => {
+    describe('if the list contains the given payload', () => {
+      beforeEach(() => {
+        state = [1, 2, 3, 2];
+        action = { type: 'HIDE_LOADING', payload: 2 };
+      });
+      it('should remove the first occurrence in the list', () => {
+        expect(reducer(state, action)).toEqual([1, 3, 2]);
+      });
+    });
+    describe('if the list does not contain the given payload', () => {
+      beforeEach(() => {
+        state = [1, 2];
+        action = { type: 'HIDE_LOADING', payload: 5 };
+      });
+      it('should return the reference of the existing state', () => {
+        expect(reducer(state, action)).toBe(state);
+      });
+    });
+  });
+  describe('when no actions match', () => {
+    beforeEach(() => {
+      state = [1, 2];
+      action = { type: 'FOO' };
+    });
+    it('should return the reference to the existing state', () => {
+      expect(reducer(state, action)).toBe(state);
+    });
+  });
+});
