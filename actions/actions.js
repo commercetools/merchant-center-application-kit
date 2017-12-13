934f51a9a79116538824949a4834ec6f63ebbe28
tdeekens <tobias.deekens@commercetools.de>
2017-12-13 09:52:23 +0100

    feat(sdk): add 'del' action

--- actions/actions.js
+++ actions/actions.js
@@ -2,6 +2,10 @@ export function get(payload) {
   return { type: 'SDK', payload: { method: 'GET', ...payload } };
 }
 
+export function remove(payload) {
+  return { type: 'SDK', payload: { method: 'DELETE', ...payload } };
+}
+
 // TODO improve this to support passing payload to the update method
 export function post(payload) {
   return { type: 'SDK', payload: { method: 'POST', ...payload } };
--- actions/actions.spec.js
+++ actions/actions.spec.js
@@ -1,19 +1,70 @@
 import * as actions from './actions';
 
-describe('fetch', () => {
-  it('should match snapshot', () => {
-    expect(actions.get({ foo: true })).toEqual({
-      payload: { foo: true, method: 'GET' },
+describe('get', () => {
+  it('should have `type`', () => {
+    expect(actions.get({})).toEqual({
+      payload: expect.any(Object),
       type: 'SDK',
+    });
+  });
+
+  it('should have `payload`', () => {
+    expect(actions.get({ foo: true })).toEqual({
+      payload: expect.objectContaining({ foo: true }),
+      type: expect.any(String),
+    });
+  });
+
+  it('should have `GET` `method` in payload', () => {
+    expect(actions.get({ foo: true })).toEqual({
+      payload: expect.objectContaining({ method: 'GET' }),
+      type: expect.any(String),
     });
   });
 });
 
-describe('update', () => {
-  it('should match snapshot', () => {
-    expect(actions.post({ foo: true })).toEqual({
-      payload: { foo: true, method: 'POST' },
+describe('post', () => {
+  it('should have `type`', () => {
+    expect(actions.post({})).toEqual({
+      payload: expect.any(Object),
       type: 'SDK',
+    });
+  });
+
+  it('should have `payload`', () => {
+    expect(actions.post({ foo: true })).toEqual({
+      payload: expect.objectContaining({ foo: true }),
+      type: expect.any(String),
+    });
+  });
+
+  it('should have `POST` `method` in payload', () => {
+    expect(actions.post({ foo: true })).toEqual({
+      payload: expect.objectContaining({ method: 'POST' }),
+      type: expect.any(String),
+    });
+  });
+});
+
+describe('remove', () => {
+  it('should have `type`', () => {
+    expect(actions.remove({})).toEqual({
+      payload: expect.any(Object),
+      type: 'SDK',
+    });
+  });
+
+  it('should have `payload`', () => {
+    expect(actions.remove({ foo: true })).toEqual({
+      payload: expect.objectContaining({ foo: true }),
+      type: expect.any(String),
+    });
+  });
+
+  it('should have `DELETE` `method` in payload', () => {
+    expect(actions.remove({ foo: true })).toEqual({
+      payload: expect.objectContaining({ method: 'DELETE' }),
+      type: expect.any(String),
     });
   });
 });
