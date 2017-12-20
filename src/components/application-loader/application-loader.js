805c3f310298c839e6f0dcff46de0adac105a010
lufego <luis.gomes@commercetools.de>
2017-12-20 10:32:31 +0100

    refactor(app-loader): adds loading-spinner from ui-kit

--- src/components/application-loader/application-loader.js
+++ src/components/application-loader/application-loader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import LogoSVG from '@commercetools-local/ui-kit/materials/images/ct-logo.svg';
-import LoadingSpinner from '@commercetools-local/core/components/loading-spinner';
+import LoadingSpinner from '@commercetools-local/ui-kit/loading-spinner';
 import styles from './application-loader.mod.css';
 
 const ApplicationLoader = () => (
