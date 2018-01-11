924c0c557300d98a61defb40df8ed85889c02b29
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-01-11 09:52:14 +0100

    docs: add clarification about function name

--- actions/actions.js
+++ actions/actions.js
@@ -2,6 +2,8 @@ export function get(payload) {
   return { type: 'SDK', payload: { method: 'GET', ...payload } };
 }
 
+// contrary to the other methods this does not bear the exact name of the HTTP-verb
+// because `delete` is a reserved keyword in ECMAScript
 export function del(payload) {
   return { type: 'SDK', payload: { method: 'DELETE', ...payload } };
 }
