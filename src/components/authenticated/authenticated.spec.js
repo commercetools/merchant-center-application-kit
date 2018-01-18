30f88aa20bc3e56e44a179cd402d66a33b11bb24
philippspo <philipp.sporrer@commercetools.de>
2018-01-18 11:40:40 +0100

    test(app-shell/authenticated): fix test

--- src/components/authenticated/authenticated.spec.js
+++ src/components/authenticated/authenticated.spec.js
@@ -1,77 +1,84 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LOGOUT_REASONS } from '@commercetools-local/constants';
 import { Authenticated } from './authenticated';
 
 const createTestProps = custom => ({
   isLoggedIn: true,
-  location: {
-    pathname: '/',
-  },
-  children: <div>{'choo choo'}</div>,
+  children: jest.fn(),
   ...custom,
 });
 const createWrapper = props => shallow(<Authenticated {...props} />);
 
 describe('rendering', () => {
   let props;
-  let wrapper;
   describe('when the user is logged in', () => {
     beforeEach(() => {
       props = createTestProps();
-      wrapper = createWrapper(props);
+      createWrapper(props);
     });
-    it('should render the children', () => {
-      expect(wrapper).toContainReact(<div>{'choo choo'}</div>);
+    it('should call children with `authenticated` set to true', () => {
+      expect(props.children).toHaveBeenCalledTimes(1);
+      expect(props.children).toHaveBeenCalledWith({ authenticated: true });
     });
   });
   describe('when the user is not logged in', () => {
-    describe('when being on the / route', () => {
-      beforeEach(() => {
-        props = createTestProps({ isLoggedIn: false });
-        wrapper = createWrapper(props);
-      });
-      it('should render a Redirect component', () => {
-        expect(wrapper).toRender('Redirect');
-      });
-      it('should redirect to /logout', () => {
-        expect(wrapper.find('Redirect').prop('to').pathname).toBe('/logout');
-      });
-      it('should set the reason param to `UNAUTHORIZED`', () => {
-        expect(wrapper.find('Redirect').prop('to').search).toContain(
-          `reason=${LOGOUT_REASONS.UNAUTHORIZED}`
-        );
-      });
-      it('should not set a `redirectTo` param', () => {
-        expect(wrapper.find('Redirect').prop('to').search).not.toContain(
-          'redirectTo'
-        );
-      });
+    beforeEach(() => {
+      props = createTestProps({ isLoggedIn: false });
+      createWrapper(props);
     });
-    describe('when being on the /foo-bar route', () => {
-      beforeEach(() => {
-        props = createTestProps({
-          isLoggedIn: false,
-          location: { pathname: '/foo-bar' },
-        });
-        wrapper = createWrapper(props);
-      });
-      it('should render a Redirect component', () => {
-        expect(wrapper).toRender('Redirect');
-      });
-      it('should redirect to /logout', () => {
-        expect(wrapper.find('Redirect').prop('to').pathname).toBe('/logout');
-      });
-      it('should set the reason param to `UNAUTHORIZED`', () => {
-        expect(wrapper.find('Redirect').prop('to').search).toContain(
-          `reason=${LOGOUT_REASONS.UNAUTHORIZED}`
-        );
-      });
-      it('should set a redirectTo param to `foo-bar`', () => {
-        expect(wrapper.find('Redirect').prop('to').search).toContain(
-          `redirectTo=${encodeURIComponent('/foo-bar')}`
-        );
-      });
+    it('should call children with `authenticated` set to true', () => {
+      expect(props.children).toHaveBeenCalledTimes(1);
+      expect(props.children).toHaveBeenCalledWith({ authenticated: false });
     });
   });
+  // TODO move test to the app shell
+  // describe('when the user is not logged in', () => {
+  //   describe('when being on the / route', () => {
+  //     beforeEach(() => {
+  //       props = createTestProps({ isLoggedIn: false });
+  //       wrapper = createWrapper(props);
+  //     });
+  //     it('should render a Redirect component', () => {
+  //       expect(wrapper).toRender('Redirect');
+  //     });
+  //     it('should redirect to /logout', () => {
+  //       expect(wrapper.find('Redirect').prop('to').pathname).toBe('/logout');
+  //     });
+  //     it('should set the reason param to `UNAUTHORIZED`', () => {
+  //       expect(wrapper.find('Redirect').prop('to').search).toContain(
+  //         `reason=${LOGOUT_REASONS.UNAUTHORIZED}`
+  //       );
+  //     });
+  //     it('should not set a `redirectTo` param', () => {
+  //       expect(wrapper.find('Redirect').prop('to').search).not.toContain(
+  //         'redirectTo'
+  //       );
+  //     });
+  //   });
+  //   describe('when being on the /foo-bar route', () => {
+  //     beforeEach(() => {
+  //       props = createTestProps({
+  //         isLoggedIn: false,
+  //         location: { pathname: '/foo-bar' },
+  //       });
+  //       wrapper = createWrapper(props);
+  //     });
+  //     it('should render a Redirect component', () => {
+  //       expect(wrapper).toRender('Redirect');
+  //     });
+  //     it('should redirect to /logout', () => {
+  //       expect(wrapper.find('Redirect').prop('to').pathname).toBe('/logout');
+  //     });
+  //     it('should set the reason param to `UNAUTHORIZED`', () => {
+  //       expect(wrapper.find('Redirect').prop('to').search).toContain(
+  //         `reason=${LOGOUT_REASONS.UNAUTHORIZED}`
+  //       );
+  //     });
+  //     it('should set a redirectTo param to `foo-bar`', () => {
+  //       expect(wrapper.find('Redirect').prop('to').search).toContain(
+  //         `redirectTo=${encodeURIComponent('/foo-bar')}`
+  //       );
+  //     });
+  //   });
+  // });
 });
