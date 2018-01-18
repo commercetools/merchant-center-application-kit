16dde8643db1f1a6a949876d93a512739f7920fd
tdeekens <nerd@tdeekens.name>
2018-01-18 13:34:03 +0100

    refactor: use IntrospectionFragmentMatcher without any types

--- src/configure-apollo.js
+++ src/configure-apollo.js
@@ -3,7 +3,10 @@ import createHistory from 'history/createBrowserHistory';
 import ApolloClient from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher,
+} from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
 import {
   LOGOUT_REASONS,
@@ -58,9 +61,44 @@ const errorLink = onError(({ networkError }) => {
     history.push(`/logout?reason=${LOGOUT_REASONS.UNAUTHORIZED}`);
 });
 const link = ApolloLink.from([headerMiddlewareLink, errorLink, httpLink]);
+
+/**
+ * Note:
+ *
+ * Apollo supports two types of fragment matchers at the time of this writing. One
+ * for heuristic matching, another for introspection based matching.
+ *
+ * The heuristic matcher is the default and attempts to deduct the needed information
+ * for efficiently caching entities from the query itself. It however warns as soon
+ * as abtract types are used (union and interface). It recommends using the introspection
+ * based matcher.
+ *
+ * The introspection based matcher takes the GraphQL schema as input to more accurately
+ * be able to make assumption for caching when looking at a query. The schema is usally
+ * queried at build time and injected into the matcher.
+ * We can not provide it the schema for mainly two reasons:
+ *
+ * 1. The CTP schema needs auth and during build not valid auth token is present
+ * 2. The schema is different per customer as parts of it are generated
+ *
+ * Another solution would be to extend the introspection matcher and inject the schema
+ * at runtime of the application. However, when passing empty types to the matcher
+ * we currently do not loose any of the abilities we use it provides as it only disables
+ * caching for union and interface types (https://github.com/apollographql/apollo-client/blob/0166da079da4a9f88eecc7c976ac9d34225f5bf8/packages/apollo-cache-inmemory/src/fragmentMatcher.ts#L150).
+ * The InMemoryCache itself uses the matcher to determine if caching is intended (https://github.com/apollographql/apollo-client/blob/0166da079da4a9f88eecc7c976ac9d34225f5bf8/packages/apollo-cache-inmemory/src/writeToStore.ts#L242-L266).
+ */
+const introspectionQueryResultData = {
+  __schema: {
+    types: [],
+  },
+};
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData,
+});
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache({
+    fragmentMatcher,
     dataIdFromObject: object => object.id,
   }),
 });
