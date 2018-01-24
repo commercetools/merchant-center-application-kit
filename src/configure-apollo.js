26f7ece5716d73f0d453853c124677f95f4dd226
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-01-24 11:17:40 +0100

    fix(graphql-target): add pim-indexer as valid `target`

--- src/configure-apollo.js
+++ src/configure-apollo.js
@@ -26,7 +26,8 @@ const httpLink = createHttpLink({
   // here. https://www.apollographql.com/docs/link/links/http.html#fetch
   fetch,
 });
-const isKnownTarget = target => ['mc', 'ctp', 'dashboard'].includes(target);
+const isKnownTarget = target =>
+  ['mc', 'ctp', 'dashboard', 'pim-indexer'].includes(target);
 // Use a middleware to update the request headers with the correct params.
 const headerMiddlewareLink = new ApolloLink((operation, forward) => {
   const target = operation.variables.target;
