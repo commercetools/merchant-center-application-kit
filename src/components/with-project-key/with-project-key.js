59974c46f779e45fca8f9f96eccb998d8bb66ba6
Nicola Molinari <nicola.molinari@commercetools.de>
2018-01-29 10:52:22 +0100

    refactor(app-shell): simplify WithProjectKey setup

--- src/components/with-project-key/with-project-key.js
+++ src/components/with-project-key/with-project-key.js
@@ -4,52 +4,74 @@ import { Switch, Route } from 'react-router-dom';
 import { STORAGE_KEYS as CORE_STORAGE_KEYS } from '@commercetools-local/constants';
 import * as storage from '@commercetools-local/utils/storage';
 
-const WithProjectKey = props => (
-  <Switch>
-    {/**
-     * For some components that require the projectKey param from the URL, we need
-     * to be careful to differentiate between the actual projectKey or an internal
-     * protected route.
-     * For example: `/:projectKey` can match both `/my-project-key` and `/account/profile`
-     * In this case `account` is not meant to be considered a project key,
-     * therefore we select the project key from local storage.
-     */}
-    <Route
-      render={({ match }) => {
-        if (!(match.url === '/' || match.url.startsWith('/account')))
-          return null;
-        if (!props.user) return null;
-        const cachedProjectKey = storage.get(
-          CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY
-        );
-        // At this point `projectKey` can still be `null`.
-        // This happens e.g. when the user is not logged in, tries to
-        // access a route under `/account` directly. After being logged
-        // in again, there is no projectKey in localStorage. In this case we
-        // attempt to get the first project key from the list of available
-        // projects of the user. In case there are no available projects, we
-        // do not render anything.
-        const projectKey =
-          !cachedProjectKey && props.user.availableProjects.length > 0
-            ? props.user.availableProjects[0].key
-            : cachedProjectKey;
+export const WithProjectKeyFromCacheOrUser = props => {
+  const cachedProjectKey = storage.get(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY);
+  // At this point `projectKey` can still be `null`.
+  // This happens e.g. when the user is not logged in, tries to
+  // access a route under `/account` directly. After being logged
+  // in again, there is no projectKey in localStorage. In this case we
+  // attempt to get the first project key from the list of available
+  // projects of the user. In case there are no available projects, we
+  // do not render anything.
+  const projectKey =
+    !cachedProjectKey && props.user.availableProjects.length > 0
+      ? props.user.availableProjects[0].key
+      : cachedProjectKey;
 
-        if (!projectKey) return null;
-        // Render only if there is a project key
-        return props.render({ projectKey });
-      }}
-    />
-    <Route
-      path="/:projectKey"
-      render={routerProps => {
-        if (!props.user) return null;
-        return props.render({
-          projectKey: routerProps.match.params.projectKey,
-        });
-      }}
-    />
-  </Switch>
-);
+  if (!projectKey) return null;
+  // Render only if there is a project key
+  return props.render({ projectKey });
+};
+WithProjectKeyFromCacheOrUser.displayName = 'WithProjectKeyFromCacheOrUser';
+WithProjectKeyFromCacheOrUser.propTypes = {
+  user: PropTypes.shape({
+    availableProjects: PropTypes.array.isRequired,
+  }),
+  render: PropTypes.func.isRequired,
+};
+
+const WithProjectKey = props => {
+  if (!props.user) return null;
+  return (
+    <Switch>
+      {/**
+       * For some components that require the projectKey param from the URL, we need
+       * to be careful to differentiate between the actual projectKey or an internal
+       * protected route.
+       * For example: `/:projectKey` can match both `/my-project-key` and `/account/profile`
+       * In this case `account` is not meant to be considered a project key,
+       * therefore we select the project key from local storage.
+       */}
+      <Route
+        exact={true}
+        path="/"
+        render={() => (
+          <WithProjectKeyFromCacheOrUser
+            user={props.user}
+            render={props.render}
+          />
+        )}
+      />
+      <Route
+        path="/account"
+        render={() => (
+          <WithProjectKeyFromCacheOrUser
+            user={props.user}
+            render={props.render}
+          />
+        )}
+      />
+      <Route
+        path="/:projectKey"
+        render={routerProps =>
+          props.render({
+            projectKey: routerProps.match.params.projectKey,
+          })
+        }
+      />
+    </Switch>
+  );
+};
 WithProjectKey.displayName = 'WithProjectKey';
 WithProjectKey.propTypes = {
   user: PropTypes.shape({
--- src/components/with-project-key/with-project-key.spec.js
+++ src/components/with-project-key/with-project-key.spec.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { STORAGE_KEYS as CORE_STORAGE_KEYS } from '@commercetools-local/constants';
 import * as storage from '@commercetools-local/utils/storage';
-import WithProjectKey from './with-project-key';
+import WithProjectKey, {
+  WithProjectKeyFromCacheOrUser,
+} from './with-project-key';
 
 jest.mock('@commercetools-local/utils/storage');
 
@@ -15,272 +17,89 @@ const createTestProps = props => ({
 describe('rendering', () => {
   let props;
   let wrapper;
-  let renderWrapper;
-  beforeEach(() => {
-    props = createTestProps({ user: null });
-    wrapper = shallow(<WithProjectKey {...props} />);
+  describe('when user is not defined', () => {
+    beforeEach(() => {
+      props = createTestProps({ user: null });
+      wrapper = shallow(<WithProjectKey {...props} />);
+    });
+    it('should render nothing', () => {
+      expect(wrapper.type()).toBe(null);
+    });
   });
-  it('should render catch-all Route', () => {
-    expect(wrapper.find('Route').at(0)).not.toHaveProp('path');
+  describe('when user is defined', () => {
+    beforeEach(() => {
+      props = createTestProps({ user: { availableProjects: [{ key: 'p1' }] } });
+      wrapper = shallow(<WithProjectKey {...props} />);
+    });
+    it('should render Route for "/"', () => {
+      expect(wrapper).toRender({ exact: true, path: '/' });
+    });
+    it('should render Route for "/account"', () => {
+      expect(wrapper).toRender({ path: '/account' });
+    });
+    it('should render Route for "/:projectKey"', () => {
+      expect(wrapper).toRender({ path: '/:projectKey' });
+    });
   });
-  it('should render Route for "/:projectKey"', () => {
-    expect(wrapper).toRender({ path: '/:projectKey' });
-  });
-  describe('catch-all Route', () => {
-    describe('when route does not match "/" or "/account"', () => {
+
+  describe('<WithProjectKeyFromCacheOrUser>', () => {
+    describe('when projectKey is not cached in localStorage', () => {
+      describe('when the user has no available projects', () => {
+        beforeEach(() => {
+          storage.remove(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY);
+          props = createTestProps({ user: { availableProjects: [] } });
+          wrapper = shallow(<WithProjectKeyFromCacheOrUser {...props} />);
+        });
+        it('should render nothing', () => {
+          expect(wrapper.type()).toBe(null);
+        });
+      });
+      describe('when user has at least one available project', () => {
+        beforeEach(() => {
+          storage.remove(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY);
+          props = createTestProps({
+            user: { availableProjects: [{ key: 'p1' }] },
+          });
+          wrapper = shallow(<WithProjectKeyFromCacheOrUser {...props} />);
+        });
+        it('should call render callback with the first project key from the available projects list', () => {
+          expect(props.render).toHaveBeenCalledWith(
+            expect.objectContaining({ projectKey: 'p1' })
+          );
+        });
+      });
+    });
+    describe('when projectKey is cached in localStorage', () => {
       beforeEach(() => {
-        renderWrapper = shallow(
-          <div>
-            {wrapper
-              .find('Route')
-              .at(0)
-              .prop('render')({
-              match: { url: '/foo' },
-            })}
-          </div>
+        storage.put(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY, 'cached-project-key');
+        props = createTestProps({ user: { availableProjects: [] } });
+        wrapper = shallow(<WithProjectKeyFromCacheOrUser {...props} />);
+      });
+      it('should call render callback with the cached project key', () => {
+        expect(props.render).toHaveBeenCalledWith(
+          expect.objectContaining({ projectKey: 'cached-project-key' })
         );
-      });
-      it('should render nothing', () => {
-        expect(renderWrapper).toContainReact(<div />);
-      });
-    });
-    describe('when route matches "/account"', () => {
-      describe('when user is not defined', () => {
-        beforeEach(() => {
-          renderWrapper = shallow(
-            <div>
-              {wrapper
-                .find('Route')
-                .at(0)
-                .prop('render')({ match: { url: '/account' } })}
-            </div>
-          );
-        });
-        it('should render nothing', () => {
-          expect(renderWrapper).toContainReact(<div />);
-        });
-      });
-      describe('when user is defined', () => {
-        beforeEach(() => {
-          props = createTestProps({
-            user: { availableProjects: [{ key: 'p1' }] },
-          });
-          wrapper = shallow(<WithProjectKey {...props} />);
-        });
-        describe('when is an "account" route', () => {
-          describe('when projectKey is not cached in localStorage', () => {
-            describe('when the user has no available projects', () => {
-              beforeEach(() => {
-                props = createTestProps({ user: { availableProjects: [] } });
-                wrapper = shallow(<WithProjectKey {...props} />);
-                storage.remove(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY);
-                renderWrapper = shallow(
-                  <div>
-                    {wrapper
-                      .find('Route')
-                      .at(0)
-                      .prop('render')({
-                      match: {
-                        url: '/account/profile',
-                        params: { projectKey: 'account' },
-                      },
-                    })}
-                  </div>
-                );
-              });
-              it('should render nothing', () => {
-                expect(renderWrapper).toContainReact(<div />);
-              });
-            });
-            describe('when user has at least one available project', () => {
-              beforeEach(() => {
-                wrapper = shallow(<WithProjectKey {...props} />);
-                storage.remove(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY);
-                renderWrapper = shallow(
-                  <div>
-                    {wrapper
-                      .find('Route')
-                      .at(0)
-                      .prop('render')({
-                      match: {
-                        url: '/account/profile',
-                        params: { projectKey: 'account' },
-                      },
-                    })}
-                  </div>
-                );
-              });
-              it('should call render callback with the first project key from the available projects list', () => {
-                expect(props.render).toHaveBeenCalledWith(
-                  expect.objectContaining({ projectKey: 'p1' })
-                );
-              });
-            });
-          });
-          describe('when projectKey is cached in localStorage', () => {
-            beforeEach(() => {
-              storage.put(
-                CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY,
-                'cached-project-key'
-              );
-              renderWrapper = shallow(
-                <div>
-                  {wrapper
-                    .find('Route')
-                    .at(0)
-                    .prop('render')({
-                    match: {
-                      url: '/account/profile',
-                      params: { projectKey: 'account' },
-                    },
-                  })}
-                </div>
-              );
-            });
-            it('should call render callback with the cached project key', () => {
-              expect(props.render).toHaveBeenCalledWith(
-                expect.objectContaining({ projectKey: 'cached-project-key' })
-              );
-            });
-          });
-        });
-      });
-    });
-    describe('when route matches "/"', () => {
-      describe('when user is not defined', () => {
-        beforeEach(() => {
-          renderWrapper = shallow(
-            <div>
-              {wrapper
-                .find('Route')
-                .at(0)
-                .prop('render')({ match: { url: '/' } })}
-            </div>
-          );
-        });
-        it('should render nothing', () => {
-          expect(renderWrapper).toContainReact(<div />);
-        });
-      });
-      describe('when user is defined', () => {
-        beforeEach(() => {
-          props = createTestProps({
-            user: { availableProjects: [{ key: 'p1' }] },
-          });
-          wrapper = shallow(<WithProjectKey {...props} />);
-        });
-        describe('when projectKey is not cached in localStorage', () => {
-          describe('when the user has no available projects', () => {
-            beforeEach(() => {
-              props = createTestProps({ user: { availableProjects: [] } });
-              wrapper = shallow(<WithProjectKey {...props} />);
-              storage.remove(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY);
-              renderWrapper = shallow(
-                <div>
-                  {wrapper
-                    .find('Route')
-                    .at(0)
-                    .prop('render')({
-                    match: {
-                      url: '/',
-                      params: {},
-                    },
-                  })}
-                </div>
-              );
-            });
-            it('should render nothing', () => {
-              expect(renderWrapper).toContainReact(<div />);
-            });
-          });
-          describe('when user has at least one available project', () => {
-            beforeEach(() => {
-              wrapper = shallow(<WithProjectKey {...props} />);
-              storage.remove(CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY);
-              renderWrapper = shallow(
-                <div>
-                  {wrapper
-                    .find('Route')
-                    .at(0)
-                    .prop('render')({
-                    match: {
-                      url: '/',
-                      params: {},
-                    },
-                  })}
-                </div>
-              );
-            });
-            it('should call render callback with the first project key from the available projects list', () => {
-              expect(props.render).toHaveBeenCalledWith(
-                expect.objectContaining({ projectKey: 'p1' })
-              );
-            });
-          });
-        });
-        describe('when projectKey is cached in localStorage', () => {
-          beforeEach(() => {
-            storage.put(
-              CORE_STORAGE_KEYS.ACTIVE_PROJECT_KEY,
-              'cached-project-key'
-            );
-            renderWrapper = shallow(
-              <div>
-                {wrapper
-                  .find('Route')
-                  .at(0)
-                  .prop('render')({
-                  match: {
-                    url: '/',
-                    params: {},
-                  },
-                })}
-              </div>
-            );
-          });
-          it('should call render callback with the cached project key', () => {
-            expect(props.render).toHaveBeenCalledWith(
-              expect.objectContaining({ projectKey: 'cached-project-key' })
-            );
-          });
-        });
       });
     });
   });
   describe('Route for "/:projectKey"', () => {
-    describe('when user is not defined', () => {
-      beforeEach(() => {
-        renderWrapper = shallow(
-          <div>
-            {wrapper.find({ path: '/:projectKey' }).prop('render')({
-              match: { params: { projectKey: 'test-1' } },
-            })}
-          </div>
-        );
+    beforeEach(() => {
+      props = createTestProps({
+        user: { availableProjects: [{ key: 'p1' }] },
       });
-      it('should render nothing', () => {
-        expect(renderWrapper).toContainReact(<div />);
-      });
+      wrapper = shallow(<WithProjectKey {...props} />);
+      shallow(
+        <div>
+          {wrapper.find({ path: '/:projectKey' }).prop('render')({
+            match: { params: { projectKey: 'test-1' } },
+          })}
+        </div>
+      );
     });
-    describe('when user is defined', () => {
-      beforeEach(() => {
-        props = createTestProps({
-          user: { availableProjects: [{ key: 'p1' }] },
-        });
-        wrapper = shallow(<WithProjectKey {...props} />);
-        renderWrapper = shallow(
-          <div>
-            {wrapper.find({ path: '/:projectKey' }).prop('render')({
-              match: { params: { projectKey: 'test-1' } },
-            })}
-          </div>
-        );
-      });
-      it('should call render callback with matched projectKey', () => {
-        expect(props.render).toHaveBeenCalledWith(
-          expect.objectContaining({ projectKey: 'test-1' })
-        );
-      });
+    it('should call render callback with matched projectKey', () => {
+      expect(props.render).toHaveBeenCalledWith(
+        expect.objectContaining({ projectKey: 'test-1' })
+      );
     });
   });
 });
