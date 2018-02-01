fab945d8247e85a7c130277f0f23bb37d08b5440
Nicola Molinari <nicola.molinari@commercetools.de>
2018-02-01 11:37:51 +0100

    refactor(browser-history): use RR4 browser history

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import { createHistory, useBeforeUnload } from 'history';
-import { useRouterHistory } from 'react-router';
-// NOTE: this is hopefully a temporary workaround until RR v4 is released.
-// https://github.com/mjackson/history/issues/379#issuecomment-257007458
-// The problem was that somehow the `listenBeforeUnload` function was not
-// available anymore in the history, causing the router leave hook to do nothing
-// This solution has been taken from:
-// https://github.com/ReactTraining/react-router/issues/3147#issuecomment-200572190
-export default useBeforeUnload(useRouterHistory(createHistory))();
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
+
+export default history;
