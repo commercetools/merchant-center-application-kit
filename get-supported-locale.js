5e5e14bd5d72e955f61a4d2f245d4e369ae66ac1
qmateub <qmateub@yahoo.es>
2018-05-03 17:55:53 +0200

    refactor(l10n): add comment to function for supported locale

--- get-supported-locale.js
+++ get-supported-locale.js
@@ -1,5 +1,7 @@
 const supportedLocales = ['en', 'de'];
 
+// This function is reponsible of passing the fallback language "en"
+// in case the given language is not supported by the application yet
 export default function getSupportedLocale(locale) {
   return supportedLocales.includes(locale) ? locale : 'en';
 }
