bf54237defcee170c891018afeec9b6eb978058e
lufego <luis.gomes@commercetools.de>
2018-05-15 17:51:38 +0200

    fix(i18n): improves wording for plural

--- src/components/project-container/messages.js
+++ src/components/project-container/messages.js
@@ -3,6 +3,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   trialDaysLeft: {
     id: 'Project.trialDaysLeft',
-    defaultMessage: `Your project trial period will expire in {daysLeft} days. For any inquiries please contact sales@commercetools.com`,
+    defaultMessage:
+      'Your project trial period will expire in {daysLeft} {daysLeft, plural, one {day} other {days}}. For any inquiries please contact sales@commercetools.com',
   },
 });
