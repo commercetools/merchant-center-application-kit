f492c5db4fe4f9aa0175925d27f44fc65df7c620
qmateub <qmateub@yahoo.es>
2018-05-24 15:13:59 +0200

    refactor(locale): rename variables and functions to be consistent with the difference between language vs locale

--- get-supported-locale.js
+++ get-supported-locale.js
@@ -1,14 +1,14 @@
-const supportedLocales = ['en', 'de'];
+const supportedLanguages = ['en', 'de'];
 
 // This function is reponsible of passing the fallback language "en"
 // in case the given language is not supported by the application yet
-export function getSupportedLocale(locale) {
-  return supportedLocales.includes(locale) ? locale : 'en';
+export function getSupportedLanguage(language) {
+  return supportedLanguages.includes(language) ? language : 'en';
 }
 
-/* This function is needed in order to parse the locale + region that we allow in the user
-*  settings. Because we only have messages for `en` and `de`, we need to always get just the
-*  first part of the locale (`en-GB` would be `en`) so the app does not break.
+/* This function is needed in order to parse the locale (<language-code>-<country-code>) that we allow in the user
+*  we allow in the user settings. Because we only have messages for `en` and `de`, we need to always get just the
+*  first part of the locale, the language, (`en-GB` would be `en`) so the app does not break.
 */
 export function extractLanguageFromLocale(locale) {
   return locale.includes('-') ? locale.split('-')[0] : locale;
