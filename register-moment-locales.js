f76e8c6e33990c8ea77042c66e2ec235d72f6939
Nicola Molinari <nicola.molinari@commercetools.de>
2018-06-04 12:35:52 +0200

    refactor: move moment locale registration into l10n package

---
+++ register-moment-locales.js
@@ -0,0 +1,17 @@
+// We need to import the moment locales (language + country) for each
+// supported language (en, de).
+// NOTE: when you add a new language, ensure to update the `MomentLocalesPlugin`
+// in the webpack config.
+
+// EN
+import 'moment/locale/en-au';
+import 'moment/locale/en-ca';
+import 'moment/locale/en-gb';
+import 'moment/locale/en-ie';
+import 'moment/locale/en-il';
+import 'moment/locale/en-nz';
+
+// DE
+import 'moment/locale/de';
+import 'moment/locale/de-at';
+import 'moment/locale/de-ch';
