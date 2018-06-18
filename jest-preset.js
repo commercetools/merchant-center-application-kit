a2e0c391cb283352c1b55bc280c1388760447213
Nicola Molinari <nicola.molinari@commercetools.de>
2018-06-18 11:08:11 +0200

    chore(devtools): move jest preset into own package, remove test cmd from mc-scripts

---
+++ jest-preset.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+// Resolve the absolute path of the caller location.
+const rootPath = path.resolve('.');
+const resolveRelativePath = relativePath =>
+  path.resolve(__dirname, relativePath);
+
+module.exports = {
+  displayName: 'test',
+  globals: {
+    'process.env': {
+      NODE_ENV: 'test',
+    },
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': resolveRelativePath(
+      './transform-file.js'
+    ),
+    '\\.css$': 'identity-obj-proxy',
+  },
+  rootDir: rootPath,
+  setupFiles: [
+    'raf/polyfill',
+    resolveRelativePath('./setup-tests.js'),
+    'jest-localstorage-mock',
+  ],
+  setupTestFrameworkScriptFile: resolveRelativePath(
+    './setup-test-framework.js'
+  ),
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testEnvironment: 'jsdom',
+  testURL: 'https://mc.commercetools.com/',
+  testPathIgnorePatterns: ['node_modules', 'cypress'],
+  testRegex: '\\.spec\\.js$',
+  transform: {
+    '^.+\\.js$': resolveRelativePath('./transform-babel-jest.js'),
+    '^.+\\.graphql$': 'jest-transform-graphql',
+    '^.+\\.pegjs$': 'pegjs-jest',
+  },
+  watchPlugins: ['jest-plugin-filename'],
+};
