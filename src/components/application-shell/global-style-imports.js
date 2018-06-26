1252a8d325ecdb0d957e4e539929e012c3c2d06a
Nicola Molinari <nicola.molinari@commercetools.de>
2018-06-26 09:15:01 +0200

    chore: fix imports

--- src/components/application-shell/global-style-imports.js
+++ src/components/application-shell/global-style-imports.js
@@ -1,7 +1,5 @@
 // Vendor styles
-import 'awesomplete/awesomplete.css';
 import 'react-select/dist/react-select.css';
-import 'react-dates/lib/css/_datepicker.css';
 // Global MC styles
 import '@commercetools-frontend/ui-kit/materials/grid.mod.css';
 import '@commercetools-frontend/ui-kit/materials/reset.mod.css';
