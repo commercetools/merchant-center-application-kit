0c2b8f830acfc178ebe5cfe6033e92f659dd9f8e
Nicola Molinari <nicola.molinari@commercetools.de>
2018-06-26 17:01:19 +0200

    fix: jest config pattern for graphql transforms

--- jest.test.config.js
+++ jest.test.config.js
@@ -5,6 +5,6 @@ module.exports = {
     // This option tells Jest to ignore specific folders from being transpiled
     // (e.g. with babel).
     // However we need to instruct jest to actually transpile some packages.
-    '/node_modules/(?!(@commercetools-frontend.*?\\.js$))',
+    '/node_modules/(?!(@commercetools-frontend.*?\\.(js|graphql)$))',
   ],
 };
