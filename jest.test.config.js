9cdae7c2a9f0396a3cb06d469ee3d42fa108ca0e
Nicola Molinari <nicola.molinari@commercetools.de>
2018-06-27 08:54:23 +0200

    chore: upgrade to latest deps

--- jest.test.config.js
+++ jest.test.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   preset: '@commercetools-frontend/jest-preset-mc-app',
-  moduleDirectories: ['src', 'node_modules'],
-  transformIgnorePatterns: [
-    // This option tells Jest to ignore specific folders from being transpiled
-    // (e.g. with babel).
-    // However we need to instruct jest to actually transpile some packages.
-    '/node_modules/(?!(@commercetools-frontend.*?\\.(js|graphql)$))',
-  ],
 };
