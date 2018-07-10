64d08d3fc64649475e7f662a857309efdf368529
Adnan Asani <adnasa@users.noreply.github.com>
2018-07-10 17:32:54 +0200

    refactor(measure-first-paint): use class over recompose

--- src/components/performance-timing/measure-first-paint.js
+++ src/components/performance-timing/measure-first-paint.js
@@ -1,14 +1,10 @@
-import { lifecycle, compose } from 'recompose';
+import React from 'react';
+import PropTypes from 'prop-types';
 import snakeCase from 'lodash.snakecase';
 import oneLineTrim from 'common-tags/lib/oneLineTrim';
 import { reportErrorToSentry } from '@commercetools-frontend/sentry';
 import { injectConfiguration } from '@commercetools-frontend/application-shell-connectors';
 import convertToClosestMs from './conversions';
-
-const MeasureFirstPaint = () => null;
-MeasureFirstPaint.displayName = 'MeasureFirstPaint';
-
-const metricSummariesEndpoint = '/proxy/mc-metrics/metrics/summaries';
 
 const transformPaintToMcMetrics = ({ paintMetric, labels }) => ({
   metricName: oneLineTrim`
@@ -21,36 +17,45 @@ const transformPaintToMcMetrics = ({ paintMetric, labels }) => ({
   metricLabels: labels,
 });
 
-export default compose(
-  injectConfiguration(['mcApiUrl'], 'mcApiUrl'),
-  lifecycle({
-    componentDidMount() {
-      // We are using the Performance API, since registering `paint`
-      // on the `PerformanceObserver` doesn't give us the startTimes that we need
-      // if we follow the pattern of initiating the `PerformanceObserver` upon mount
-      const convertedMetrics = performance
-        .getEntriesByType('paint')
-        .map(paintMetric =>
-          transformPaintToMcMetrics({
-            paintMetric,
-            labels: { application: this.props.applicationLabel },
-          })
-        );
+const metricSummariesEndpoint = '/proxy/mc-metrics/metrics/summaries';
 
-      const headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      headers.append('Accept', 'application/json');
+class MeasureFirstPaint extends React.Component {
+  static displayName = 'MeasureFirstPaint';
+  static propTypes = {
+    applicationLabel: PropTypes.string.isRequired,
+    mcApiUrl: PropTypes.string.isRequired,
+  };
+  componentDidMount() {
+    // We are using the Performance API, since registering `paint`
+    // on the `PerformanceObserver` doesn't give us the startTimes that we need
+    // if we follow the pattern of initiating the `PerformanceObserver` upon mount
+    const convertedMetrics = performance
+      .getEntriesByType('paint')
+      .map(paintMetric =>
+        transformPaintToMcMetrics({
+          paintMetric,
+          labels: { application: this.props.applicationLabel },
+        })
+      );
 
-      fetch(`${this.props.mcApiUrl}${metricSummariesEndpoint}`, {
-        method: 'POST',
-        body: JSON.stringify(convertedMetrics),
-        credentials: 'include',
-        headers,
-      }).catch(error => {
-        reportErrorToSentry(new Error('Unable to push first-paint metrics'), {
-          extra: error,
-        });
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+
+    fetch(`${this.props.mcApiUrl}${metricSummariesEndpoint}`, {
+      method: 'POST',
+      body: JSON.stringify(convertedMetrics),
+      credentials: 'include',
+      headers,
+    }).catch(error => {
+      reportErrorToSentry(new Error('Unable to push first-paint metrics'), {
+        extra: error,
       });
-    },
-  })
-)(MeasureFirstPaint);
+    });
+  }
+  render() {
+    return null;
+  }
+}
+
+export default injectConfiguration(['mcApiUrl'], 'mcApiUrl')(MeasureFirstPaint);
