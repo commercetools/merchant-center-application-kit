6797cd580ca6ec28cdd36eeb2abcb0fd28470c6b
Malcolm Laing <malcolm.laing@commercetools.de>
2018-07-11 11:55:37 +0200

    refactor(vendor-css): move to explicit react-select css import

--- src/components/application-shell/global-style-imports.js
+++ src/components/application-shell/global-style-imports.js
@@ -1,5 +1,3 @@
-// Vendor styles
-import 'react-select/dist/react-select.css';
 // Global MC styles
 import '@commercetools-frontend/ui-kit/materials/grid.mod.css';
 import '@commercetools-frontend/ui-kit/materials/reset.mod.css';
