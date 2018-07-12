d8b90b93c7fdf636d6080a8dd8bfb0a8256e598c
Malcolm Laing <malcolm.laing@commercetools.de>
2018-07-12 10:49:47 +0200

    refactor(app-shell): remove iunnecessary nner application

--- load-i18n.js
+++ load-i18n.js
@@ -1,6 +1,6 @@
 export default function loadI18n(locale) {
   // Use lazy once so that subsequent calls to import() will use the same
   // network response. https://webpack.js.org/api/module-methods/#import-
-  return import(/* webpackChunkName: "i18n-locale-data", webpackMode: "lazy-once" */
+  return import(/* webpackChunkName: "i18n-locale-data" */
   `./data/${locale}.json`);
 }
