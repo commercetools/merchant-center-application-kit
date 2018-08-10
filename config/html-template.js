4af85b68b3cc52725edf54d0adda94b0b224456d
tdeekens <tobias.deekens@commercetools.de>
2018-08-10 15:16:46 +0200

    refactor: inline css for loading screen

--- config/html-template.js
+++ config/html-template.js
@@ -42,7 +42,58 @@ module.exports = templateParams => {
     <noscript>You need to enable JavaScript to run this app.</noscript>
 
     <div id="app-loader">
-      <style>__LOADING_SCREEN_CSS__</style>
+      <style>
+        .loading-screen {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+          width: 100vw;
+        }
+
+        .loading-screen--hidden {
+          display: none;
+        }
+
+        .loading-screen>*+* {
+          margin: 24px 0 0;
+        }
+
+        .loading-spinner {
+          width: 32px;
+          height: 32px;
+        }
+
+        .long-loading-notice {
+          color: #999;
+          font-family: 'Open Sans', sans-serif;
+          font-size: 12px;
+        }
+
+        .long-loading-notice--hidden {
+          visibility: hidden;
+        }
+
+        .loading-spinner-circle {
+          fill: #213c45;
+          opacity: 0.2;
+        }
+
+        @keyframes loading-spinner-animation {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+
+        .loading-spinner-pointer {
+          transform-origin: 20px 20px 0;
+          animation: loading-spinner-animation 0.5s infinite linear;
+        }
+      </style>
 
       <div class="loading-screen loading-screen--hidden">
         <svg class="loading-spinner" viewBox="0 0 40 40">
