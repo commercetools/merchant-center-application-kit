41b0b38ecda3a4df9041948ac79c592c85c7aa23
tdeekens <tobias.deekens@commercetools.de>
2018-08-10 15:15:22 +0200

    docs: add reference to MDN for CORS

--- utils/create-asset-hash.js
+++ utils/create-asset-hash.js
@@ -6,8 +6,12 @@ module.exports = content => {
     .update(content)
     .digest('base64');
 
-  // NOTE: We prefix the hash function type as the browser
-  // needs it when validating the contents of a script against
-  // CSP headers sent.
+  /**
+   * NOTE:
+   *   We prefix the hash function type as the browser
+   *   needs it when validating the contents of a script against
+   *   CSP headers sent.
+   *   For more information head to: developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src#Sources
+   */
   return `sha256-${sha256Hash}`;
 };
