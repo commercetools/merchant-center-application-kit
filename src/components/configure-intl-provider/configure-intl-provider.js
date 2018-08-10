c02e13b814183624727a6c0fb7b6e75a038ab263
tdeekens <tobias.deekens@commercetools.de>
2018-08-10 15:20:57 +0200

    docs: add explanation to hiding loading screen

--- src/components/configure-intl-provider/configure-intl-provider.js
+++ src/components/configure-intl-provider/configure-intl-provider.js
@@ -5,6 +5,18 @@ import { AppShellProviderForUserTimeZone } from '@commercetools-frontend/applica
 import { IntlProvider } from 'react-intl';
 
 const hideAppLoader = once(() => {
+  /**
+   * NOTE:
+   *   This function is defined in the `index.html` in a script-tag
+   *   by the `html-template.js` in the `mc-scripts`. There are
+   *   alternative ways of acheiving this namely:
+   *   1. Using custom events and dispatching here
+   *     - Not supported in IE11 and would need a polyfill
+   *   2. Removing the DOM node here
+   *     - Both `index.html` and this component would have to
+   *       now the div's id/class. If one would change the index.html
+   *       the app would never show (always show the loading screen)
+   */
   if (window.onAppLoaded) window.onAppLoaded();
 });
 
