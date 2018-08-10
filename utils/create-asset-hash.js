3013d592c0906a9f0aa3ae09ac93eb77a8dde316
tdeekens <tobias.deekens@commercetools.de>
2018-08-10 12:22:50 +0200

    docs: add comments to explain CSP header generation

--- utils/create-asset-hash.js
+++ utils/create-asset-hash.js
@@ -6,5 +6,8 @@ module.exports = content => {
     .update(content)
     .digest('base64');
 
+  // NOTE: We prefix the hash function type as the browser
+  // needs it when validating the contents of a script against
+  // CSP headers sent.
   return `sha256-${sha256Hash}`;
 };
--- utils/get-assets.js
+++ utils/get-assets.js
@@ -1,3 +1,9 @@
+/**
+ * NOTE:
+ *   These files can not be extracted for now as compiling the `html-template.js`
+ *   runs in a webpack-loader context which does not "allow" access to use normal
+ *   `fs.readFileSync` as it runs in a different context.
+ */
 const dataLayerScript = `
 dataLayer = [{ 'gtm.start': new Date().getTime(), event: 'gtm.js' }];
 `;
