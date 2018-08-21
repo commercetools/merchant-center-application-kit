a200c865621247f1442d107688ea58507d49efe3
Nicola Molinari <nicola.molinari@commercetools.de>
2018-08-21 17:14:05 +0200

    docs(devtools): add comment about overriding env.json config

--- webpack-plugins/local-html-webpack-plugin.js
+++ webpack-plugins/local-html-webpack-plugin.js
@@ -8,6 +8,11 @@ const localEnv = loadEnv(path.join(sourcePath, 'env.json'));
 const enhancedLocalEnv = Object.assign(
   {},
   localEnv,
+  // Now that the app config is defined as a `env.json`, when we start the FE app
+  // to point to the local backend API by passing the `MC_API_URL` env does not
+  // work anymore). To make it work again, we can override the `env.json` config
+  // with the env variable before injecting the values into the index.html.
+  // NOTE: this is only necessary for development.
   process.env.MC_API_URL
     ? {
         mcApiUrl: process.env.MC_API_URL,
