d5d12643c6ca3b5cc62bebaac3b4ee8df08ea107
Nicola Molinari <nicola.molinari@commercetools.de>
2018-08-21 16:47:22 +0200

    fix(devtools): allow to override MC API URL with env var for local env

--- webpack-plugins/local-html-webpack-plugin.js
+++ webpack-plugins/local-html-webpack-plugin.js
@@ -1,10 +1,19 @@
-/* eslint-disable class-methods-use-this,no-param-reassign */
+/* eslint-disable class-methods-use-this,no-param-reassign,prefer-object-spread/prefer-object-spread */
 const path = require('path');
 const loadEnv = require('@commercetools-frontend/mc-html-template/load-env');
 const replaceHtmlPlaceholders = require('@commercetools-frontend/mc-html-template/utils/replace-html-placeholders');
 
 const sourcePath = process.cwd();
 const localEnv = loadEnv(path.join(sourcePath, 'env.json'));
+const enhancedLocalEnv = Object.assign(
+  {},
+  localEnv,
+  process.env.MC_API_URL
+    ? {
+        mcApiUrl: process.env.MC_API_URL,
+      }
+    : {}
+);
 
 class LocalHtmlWebpackPlugin {
   apply(compiler) {
@@ -12,7 +21,7 @@ class LocalHtmlWebpackPlugin {
       compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
         'local-html-webpack-plugin',
         (data, cb) => {
-          data.html = replaceHtmlPlaceholders(data.html, localEnv);
+          data.html = replaceHtmlPlaceholders(data.html, enhancedLocalEnv);
           cb(null, data);
         }
       );
