7eab369110906fca825fbbecf0c394ce5af14ea0
Nicola Molinari <nicola.molinari@commercetools.de>
2018-10-08 21:10:43 +0200

    chore: simplify jest test config

--- jest.eslint.config.js
+++ jest.eslint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'eslint',
-  testMatch: ['<rootDir>/packages/**/*.js'],
+  testMatch: ['<rootDir>/*.js', '<rootDir>/packages/**/*.js'],
   watchPlugins: ['jest-plugin-filename', 'jest-watch-master'],
 };
--- jest.test.config.js
+++ jest.test.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   preset: './packages/jest-preset-mc-app',
-  moduleDirectories: [
-    'src',
-    'packages',
-    'node_modules',
-  ],
+  moduleDirectories: ['src', 'packages', 'node_modules'],
   transformIgnorePatterns: [
     // This option tells Jest to ignore specific folders from being transpiled
     // (e.g. with babel).
@@ -13,25 +9,7 @@ module.exports = {
     // in node_modules. Since the name of those packages are scoped with
     // `@commercetools-local` we simply match the JS files within those folders.
     '/node_modules/',
-    'node_modules/(?!(@commercetools-frontend/actions-global)/)',
-    'node_modules/(?!(@commercetools-frontend/application-shell)/)',
-    'node_modules/(?!(@commercetools-frontend/application-shell-connectors)/)',
-    'node_modules/(?!(@commercetools-frontend/babel-preset-mc-app)/)',
-    'node_modules/(?!(@commercetools-frontend/browser-history)/)',
-    'node_modules/(?!(@commercetools-frontend/constants)/)',
-    'node_modules/(?!(@commercetools-frontend/eslint-config-mc-app)/)',
-    'node_modules/(?!(@commercetools-frontend/i18n)/)',
-    'node_modules/(?!(@commercetools-frontend/jest-preset-mc-app)/)',
-    'node_modules/(?!(@commercetools-frontend/l10n)/)',
-    'node_modules/(?!(@commercetools-frontend/mc-html-template)/)',
-    'node_modules/(?!(@commercetools-frontend/mc-http-server)/)',
-    'node_modules/(?!(@commercetools-frontend/mc-scripts)/)',
-    'node_modules/(?!(@commercetools-frontend/notifications)/)',
-    'node_modules/(?!(@commercetools-frontend/permissions)/)',
-    'node_modules/(?!(@commercetools-frontend/react-notifications)/)',
-    'node_modules/(?!(@commercetools-frontend/sdk)/)',
-    'node_modules/(?!(@commercetools-frontend/sentry)/)',
-    'node_modules/(?!(@commercetools-frontend/storage)/)',
-    'node_modules/(?!(@commercetools-frontend/url-utils)/)',
+    'node_modules/@commercetools-frontend/ui-kit',
+    'node_modules/(?!(@commercetools-frontend)/)',
   ],
 };
