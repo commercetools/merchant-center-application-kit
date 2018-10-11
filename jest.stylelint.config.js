f84f01c461892fce7e7afc5885fbcf07a5b04f1a
Nicola Molinari <nicola.molinari@commercetools.de>
2018-10-11 13:36:22 +0200

    chore: ignore dist folder for stylelint

--- jest.stylelint.config.js
+++ jest.stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   runner: 'jest-runner-stylelint',
   displayName: 'stylelint',
   moduleFileExtensions: ['css'],
+  modulePathIgnorePatterns: ['dist'],
   testMatch: ['<rootDir>/packages/**/*.css', '<rootDir>/playground/**/*.css'],
   watchPlugins: ['jest-plugin-filename', 'jest-watch-master'],
 };
