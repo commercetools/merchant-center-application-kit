6c5a15f8fcd76f423793054318dca51d4a0e7643
Malcolm Laing <malcolm.laing@commercetools.de>
2018-11-02 13:24:19 +0100

    chore: remove dynamic import of quick-access

--- packages/application-shell/src/components/quick-access/index.js
+++ packages/application-shell/src/components/quick-access/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Loadable from 'react-loadable';
-import ButlerContainer from './butler-container';
+// import Loadable from 'react-loadable';
+// import ButlerContainer from './butler-container';
 import * as gtm from '../../utils/gtm';
 import trackingEvents from './tracking-events';
-
-const QuickAccessModal = Loadable({
-  loader: () => import('./quick-access' /* webpackChunkName: "quick-access" */),
-  loading: ButlerContainer,
-});
+import QuickAccessModal from './quick-access';
+// TODO: make this work in different environments.
+// const QuickAccessModal = Loadable({
+//   loader: () => import('./quick-access' /* webpackChunkName: "quick-access" */),
+//   loading: ButlerContainer,
+// });
 
 export default class QuickAccessTrigger extends React.Component {
   static displayName = 'QuickAccessTrigger';
