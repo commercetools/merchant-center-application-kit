5010503cec9b1ffcb54bbee2ba9c84fdd1277745
Malcolm Laing <malcolm.laing@commercetools.de>
2018-11-02 11:54:00 +0100

    chore: move i18n to named exports

--- packages/i18n/load-i18n/load-i18n.js
+++ packages/i18n/load-i18n/load-i18n.js
@@ -1,5 +1,5 @@
 import { addLocaleData } from 'react-intl';
-import { getMatchingMomentCode } from './utils';
+import { getMatchingMomentCode } from '../utils';
 
 export default function loadI18n(lang) {
   // Use default (lazy) so that we will receive one chunk per
@@ -9,16 +9,16 @@ export default function loadI18n(lang) {
 
   const reactIntlChunkImport = import(/* webpackChunkName: "react-intl-data-[request]" */
   /* webpackMode: "lazy" */
-  `./locale-data/${lang}`);
+  `../locale-data/${lang}`);
 
   const momentChunkImport = import(/* webpackChunkName: "i18n-moment-locale-[request]" */
-  `./moment-data/${momentLocaleCode}`);
+  `../moment-data/${momentLocaleCode}`);
 
   const localeDataPromises = [reactIntlChunkImport, momentChunkImport];
 
   return Promise.all(localeDataPromises).then(response => {
     addLocaleData([...response[0].default]);
     return import(/* webpackChunkName: "react-intl-localized-strings-[request]" */
-    `./data/${lang}.json`);
+    `../data/${lang}.json`);
   });
 }
