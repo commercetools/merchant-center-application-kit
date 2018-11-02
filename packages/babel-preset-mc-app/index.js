0a968a3b83a9a03c01428df6547beafd86c36f13
suǝʞǝǝpʇ <nerd@tdeekens.name>
2018-11-02 16:38:06 +0100

    feat(babel-preset-mc-app): add @babel/plugin-proposal-nullish-coalescing-operator (#39)

--- packages/babel-preset-mc-app/index.js
+++ packages/babel-preset-mc-app/index.js
@@ -116,6 +116,7 @@ module.exports = function getBabePresetConfigForMcApp() {
       ],
       require('@babel/plugin-proposal-do-expressions').default,
       require('@babel/plugin-proposal-optional-chaining').default,
+      require('@babel/plugin-proposal-nullish-coalescing-operator').default,
       // Adds syntax support for import()
       require('@babel/plugin-syntax-dynamic-import').default,
       isEnvTest &&
