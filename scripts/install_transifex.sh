bfefb6f659e6bdd97d69373e0a932fffe44c3ec2
Nicola Molinari <nicola.molinari@commercetools.de>
2018-11-07 23:15:29 +0100

    chore(travis): avoid prompts in apt commands

--- scripts/install_transifex.sh
+++ scripts/install_transifex.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-PYTHON_VERSION=python3.6
+PYTHON_VERSION="python3.6"
 
 echo "Installing $PYTHON_VERSION"
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install "$PYTHON_VERSION"
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update -q
+sudo apt-get install "$PYTHON_VERSION" -y
 
 echo "Installing pip for $PYTHON_VERSION"
 curl https://bootstrap.pypa.io/get-pip.py | sudo -H "$PYTHON_VERSION"
