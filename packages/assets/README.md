88956fc753100102ade10c6f39d9f03599c11c72
Malcolm Laing <malcolm.laing@commercetools.de>
2018-11-08 15:04:13 +0100

    chore: remove app specific images

--- packages/assets/README.md
+++ packages/assets/README.md
@@ -7,13 +7,13 @@ This package contains image assets that can be used in the creation of Merchant 
 ```js
 import React from 'react';
 // Named export - without the need for a bundler and appropriate loader
-import { CTLogo } from '@commercetools-frontend/assets';
+import { CTLogoSVG } from '@commercetools-frontend/assets';
 // Direct import requires a bundler to deal with SVG files
-import Fly from '@commercetools-frontend/assets/images/orders/fly.svg';
+import ProjectExpired from '@commercetools-frontend/assets/project-expired.svg';
 
 const Component = () => (
   <div>
-    <img src={Fly} />
+    <img src={ProjectExpired} />
     <img src={CTLogo} />
   </div>
 );
--- packages/assets/index.js
+++ packages/assets/index.js
@@ -1,66 +1,34 @@
-// Maintenance SVGs
 export {
   default as FailedAuthenticationSVG,
-} from './images/maintenance/failed-authentication.svg';
+} from './images/failed-authentication.svg';
 
 export {
   default as FailedAuthorizationSVG,
-} from './images/maintenance/failed-authorization.svg';
+} from './images/failed-authorization.svg';
 
-export {
-  default as PageNotFoundSVG,
-} from './images/maintenance/page-not-found.svg';
+export { default as PageNotFoundSVG } from './images/page-not-found.svg';
 
-export {
-  default as ProjectExpiredSVG,
-} from './images/maintenance/project-expired.svg';
+export { default as ProjectExpiredSVG } from './images/project-expired.svg';
 
-export {
-  default as ProjectSuspendedSVG,
-} from './images/maintenance/project-suspended.svg';
+export { default as ProjectSuspendedSVG } from './images/project-suspended.svg';
 
-export {
-  default as UnexpectedErrorSVG,
-} from './images/maintenance/unexpected-error.svg';
+export { default as UnexpectedErrorSVG } from './images/unexpected-error.svg';
 
-// Discount SVGs
-export {
-  default as CartDiscountSVG,
-} from './images/discounts/cart-discount.svg';
-
-export {
-  default as CartPredicateInfoSVG,
-} from './images/discounts/cart-predicate-info.svg';
-
-export {
-  default as DiscountCodeSVG,
-} from './images/discounts/discount-code.svg';
-
-export {
-  default as ProductDiscountSVG,
-} from './images/discounts/product-discount.svg';
-
-// Order SVGs
-export { default as FlySVG } from './images/orders/fly.svg';
-
-// Product SVGs
-export { default as ImageBrokenSVG } from './images/products/image__broken.svg';
+export { default as ImageBrokenSVG } from './images/image__broken.svg';
 
 export {
   default as ImageMissingImageSVG,
-} from './images/products/image__missing_image.svg';
+} from './images/image__missing_image.svg';
 
-export {
-  default as ImageNoImageSVG,
-} from './images/products/image__no-image.svg';
+export { default as ImageNoImageSVG } from './images/image__no-image.svg';
 
 export {
   default as LinkExternalBroken,
-} from './images/products/link-external__broken.svg';
+} from './images/link-external__broken.svg';
 
 export {
   default as LinkExternalWorking,
-} from './images/products/link-external__working.svg';
+} from './images/link-external__working.svg';
 
 export { default as ChevronDownPNG } from './images/chevron-down.png';
 export { default as RadioCheckIcons } from './images/radio-check-icons.png';
