9e08d6046c8b94abf16679bfeff493431ce3075e
Malcolm Laing <malcolm.laing@commercetools.de>
2018-11-09 10:29:39 +0100

    fix: mock assets so tests snapshots pass locally

---
+++ packages/application-shell/__mocks__/@commercetools-frontend/assets.js
@@ -0,0 +1,11 @@
+const stub = 'test-file-stub';
+
+export const ProjectExpiredSVG = stub;
+export const FailedAuthorizationSVG = stub;
+export const PageNotFoundSVG = stub;
+export const ProjectSuspendedSVG = stub;
+export const UnexpectedErrorSVG = stub;
+export const LogoSVG = stub;
+export const LogoBWWhiteSVG = stub;
+export const CTLogoSVG = stub;
+export const LogoBWSVG = stub;
