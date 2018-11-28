f11b7fc13b6845d7d715fd9b6193ce46fa5cdbfb
suǝʞǝǝpʇ <nerd@tdeekens.name>
2018-11-28 13:12:11 +0100

    feat(app-shell): re-export flopflip and loosen dep ranges (#144)
    * feat(app-shell): re-export flopflip
    
    * fix(app-shell): use ranges for utility dependencies
    
    * fix(app-shell): move prop-types to be a devDep again
    
    * fix(app-shell): change ranges of more dependencies

--- packages/application-shell/src/index.js
+++ packages/application-shell/src/index.js
@@ -27,3 +27,14 @@ export {
 } from './utils/setup-global-error-listener';
 export { selectUserId, selectProjectKeyFromUrl } from './utils';
 export { default as AsyncChunkLoader } from './components/async-chunk-loader';
+
+/**
+ * NOTE:
+ *    These are library re-exports which are shared between app-kit and
+ *    MC apps. Not having them re-exported here often leads to bundling
+ *    them twice given app-kit and any MC app require different versions.
+ *    Something they never should anyways and causes breakage.
+ *
+ *    More information can be found here: https://github.com/commercetools/fe-chapter-notes/issues/61
+ */
+export * from '@flopflip/react-broadcast';
