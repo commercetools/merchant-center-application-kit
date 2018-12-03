db2946d72e7c551893db8c1addca666b894c20e0
Malcolm Laing <malcolmclaing@gmail.com>
2018-12-03 16:41:54 +0100

    chore: remove ui-kit materials transpo (#177)

--- playground/webpack.config.prod.js
+++ playground/webpack.config.prod.js
@@ -3,24 +3,7 @@ const createWebpackConfigForProduction = require('@commercetools-frontend/mc-scr
 
 const distPath = path.resolve(__dirname, 'dist');
 const entryPoint = path.resolve(__dirname, 'src/index.js');
-const sourceFolders = [
-  path.resolve(__dirname, 'src'),
-  // NOTE: we still need to include the materials folder in order for `postcss`
-  // to compile the css module files.
-  path.resolve(
-    require.resolve('@commercetools-frontend/ui-kit'),
-    '../../materials'
-  ),
-  // Fall back to main node_modules. This might be needed in case the playground
-  // uses a different version of the ui-kit compared to the app-kit packages.
-  // Consequently, yarn workspaces will now hoist the ui-kit package with the
-  // different version. However, ui-kit imports from the app-kit packages should
-  // still resolve to the main hoisted ui-kit package.
-  path.resolve(
-    __dirname,
-    '../node_modules/@commercetools-frontend/ui-kit/materials'
-  ),
-];
+const sourceFolders = [path.resolve(__dirname, 'src')];
 
 module.exports = createWebpackConfigForProduction({
   distPath,
