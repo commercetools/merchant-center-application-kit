513a85545067409b874927a7c57a2738a7a440ed
Quique Mateu <qmateub@yahoo.es>
2018-12-05 16:50:53 +0100

    MCD-1523 : Refactor CT logo to be 🏠button (#183)

--- packages/application-shell/src/components/app-bar/app-bar.js
+++ packages/application-shell/src/components/app-bar/app-bar.js
@@ -12,79 +12,93 @@ import { REQUESTS_IN_FLIGHT_LOADER_DOM_ID } from '../requests-in-flight-loader/c
 import messages from './messages';
 import styles from './app-bar.mod.css';
 
-const AppBar = props => (
-  <div className={styles['app-bar']} data-test="top-navigation">
-    <Spacings.Inline>
-      <div className={styles.logo}>
-        <img src={LogoSVG} className={styles['logo-img']} alt="Logo" />
-      </div>
-
-      <div
-        id={REQUESTS_IN_FLIGHT_LOADER_DOM_ID}
-        className={styles['loader-container']}
-      />
-    </Spacings.Inline>
-
-    <div className={styles.navigation}>
-      <Spacings.Inline scale="m" alignItems="center">
-        <div className={styles.switchers}>
-          <Spacings.Inline alignItems="center">
-            {/* This node is used by a react portal */}
-            <div id="locale-switcher" />
-            {do {
-              const previouslyUsedProjectKey = selectProjectKeyFromLocalStorage();
-              if (!props.user) {
-                <LoadingPlaceholder shape="rect" size="s" />;
-              }
-              // The `<ProjectSwitcher>` should be rendered only if the
-              // user is fetched and the user has projects while the app runs in an project context.
-              else if (props.user.projects.total > 0 && props.projectKeyFromUrl)
-                <ProjectSwitcher
-                  // In this case it's not necessary to check if the `projectKey` param
-                  // is included in the list of projects. In such case
-                  // the dropdown will still be rendered but no project will be selected.
-                  // This is fine becase the user has still the possibility to "switch"
-                  // to a project.
-                  projectKey={
-                    props.projectKeyFromUrl ||
-                    previouslyUsedProjectKey ||
-                    props.user.defaultProjectKey
-                  }
-                  total={props.user.projects.total}
-                />;
-              else
-                <Text.Detail>
-                  <Link
-                    to={`/${previouslyUsedProjectKey ||
-                      props.user.defaultProjectKey}`}
-                  >
-                    <Spacings.Inline
-                      alignItems="center"
-                      data-test="back-to-project"
-                    >
-                      <AngleLeftIcon theme="green" size="small" />
-                      <FormattedMessage {...messages.backToProjectLink} />
-                    </Spacings.Inline>
-                  </Link>
-                </Text.Detail>;
-            }}
-          </Spacings.Inline>
+const AppBar = props => {
+  const previouslyUsedProjectKey = selectProjectKeyFromLocalStorage();
+  return (
+    <div className={styles['app-bar']} data-test="top-navigation">
+      <Spacings.Inline>
+        <div className={styles.logo}>
+          {!props.user ? (
+            <img src={LogoSVG} className={styles['logo-img']} alt="Logo" />
+          ) : (
+            <Link
+              to={`/${previouslyUsedProjectKey ||
+                props.user.defaultProjectKey}`}
+            >
+              <img src={LogoSVG} className={styles['logo-img']} alt="Logo" />
+            </Link>
+          )}
         </div>
-        <div className={styles.spacer} />
-        {props.user ? (
-          <UserSettingsMenu
-            firstName={props.user.firstName}
-            lastName={props.user.lastName}
-            gravatarHash={props.user.gravatarHash}
-            email={props.user.email}
-          />
-        ) : (
-          <LoadingPlaceholder shape="dot" size="l" />
-        )}
+
+        <div
+          id={REQUESTS_IN_FLIGHT_LOADER_DOM_ID}
+          className={styles['loader-container']}
+        />
       </Spacings.Inline>
+
+      <div className={styles.navigation}>
+        <Spacings.Inline scale="m" alignItems="center">
+          <div className={styles.switchers}>
+            <Spacings.Inline alignItems="center">
+              {/* This node is used by a react portal */}
+              <div id="locale-switcher" />
+              {do {
+                if (!props.user) {
+                  <LoadingPlaceholder shape="rect" size="s" />;
+                }
+                // The `<ProjectSwitcher>` should be rendered only if the
+                // user is fetched and the user has projects while the app runs in an project context.
+                else if (
+                  props.user.projects.total > 0 &&
+                  props.projectKeyFromUrl
+                )
+                  <ProjectSwitcher
+                    // In this case it's not necessary to check if the `projectKey` param
+                    // is included in the list of projects. In such case
+                    // the dropdown will still be rendered but no project will be selected.
+                    // This is fine becase the user has still the possibility to "switch"
+                    // to a project.
+                    projectKey={
+                      props.projectKeyFromUrl ||
+                      previouslyUsedProjectKey ||
+                      props.user.defaultProjectKey
+                    }
+                    total={props.user.projects.total}
+                  />;
+                else
+                  <Text.Detail>
+                    <Link
+                      to={`/${previouslyUsedProjectKey ||
+                        props.user.defaultProjectKey}`}
+                    >
+                      <Spacings.Inline
+                        alignItems="center"
+                        data-test="back-to-project"
+                      >
+                        <AngleLeftIcon theme="green" size="small" />
+                        <FormattedMessage {...messages.backToProjectLink} />
+                      </Spacings.Inline>
+                    </Link>
+                  </Text.Detail>;
+              }}
+            </Spacings.Inline>
+          </div>
+          <div className={styles.spacer} />
+          {props.user ? (
+            <UserSettingsMenu
+              firstName={props.user.firstName}
+              lastName={props.user.lastName}
+              gravatarHash={props.user.gravatarHash}
+              email={props.user.email}
+            />
+          ) : (
+            <LoadingPlaceholder shape="dot" size="l" />
+          )}
+        </Spacings.Inline>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 AppBar.displayName = 'AppBar';
 AppBar.propTypes = {
   user: PropTypes.shape({
