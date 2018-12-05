d72f2be39e510b233b7279cfe0110f8077f344bb
qmateub <qmateub@yahoo.es>
2018-12-05 16:05:03 +0100

    Revert "feat(loading-placeholder): add square shape"
    This reverts commit 4fc4f9abdc3295a6fe8d651d2dfa71efcdd8978d.

--- packages/application-shell/src/components/loading-placeholder/loading-placeholder.js
+++ packages/application-shell/src/components/loading-placeholder/loading-placeholder.js
@@ -7,7 +7,7 @@ const LoadingPlaceholder = props => (
 );
 LoadingPlaceholder.displayName = 'LoadingPlaceholder';
 LoadingPlaceholder.propTypes = {
-  shape: PropTypes.oneOf(['dot', 'rect', 'square']).isRequired,
+  shape: PropTypes.oneOf(['dot', 'rect']).isRequired,
   size: PropTypes.oneOf(['s', 'm', 'l', 'xl']).isRequired,
 };
 
