d8647cf4159131df27acb145f7de6c7afaccdc5f
tdeekens <tobias.deekens@commercetools.de>
2018-12-05 19:12:10 +0100

    refactor: simplify menu permissions with using derived permissions

--- packages/application-shell/src/components/navbar/config.js
+++ packages/application-shell/src/components/navbar/config.js
@@ -16,28 +16,24 @@ const itemsCategories = {
   labelKey: 'NavBar.Categories.title',
   icon: 'TreeStructureIcon',
   featureToggle: CAN_VIEW_CATEGORIES,
-  permissions: [permissions.ViewProducts, permissions.ManageProducts],
   submenu: [
     {
       key: 'Categories',
       labelKey: 'NavBar.Categories.list',
       uriPath: 'categories?mode=list',
-      permissions: [permissions.ViewCategories, permissions.ViewCategoriesList],
+      permissions: [permissions.ViewCategoriesList],
     },
     {
       key: 'CategoriesSearch',
       labelKey: 'NavBar.Categories.search',
       uriPath: 'categories?mode=search',
-      permissions: [
-        permissions.ViewCategories,
-        permissions.ViewCategoriesSearch,
-      ],
+      permissions: [permissions.ViewCategoriesSearch],
     },
     {
       key: 'AddCategory',
       labelKey: 'NavBar.Categories.add',
       uriPath: 'categories/new',
-      permissions: [permissions.ManageProducts, permissions.AddCategories],
+      permissions: [permissions.AddCategories],
     },
   ],
 };
@@ -47,33 +43,32 @@ const itemsCustomers = {
   uriPath: 'customers',
   labelKey: 'NavBar.Customers.title',
   icon: 'UserFilledIcon',
-  permissions: [permissions.ViewCustomers, permissions.ManageCustomers],
   submenu: [
     {
       key: 'Customers',
       labelKey: 'NavBar.Customers.list',
       uriPath: 'customers',
-      permissions: [permissions.ViewCustomers, permissions.ViewCustomersList],
+      permissions: [permissions.ViewCustomersList],
     },
     {
       key: 'AddCustomer',
       labelKey: 'NavBar.Customers.add',
       uriPath: 'customers/new',
-      permissions: [permissions.ManageCustomers, permissions.AddCustomers],
+      permissions: [permissions.AddCustomers],
     },
     {
       key: 'CustomerGroups',
       labelKey: 'NavBar.CustomerGroups.list',
       featureToggle: CUSTOMER_GROUPS,
       uriPath: 'customers/customer-groups',
-      permissions: [permissions.ViewCustomers, permissions.ViewCustomerGroups],
+      permissions: [permissions.ViewCustomerGroups],
     },
     {
       key: 'AddCustomerGroup',
       labelKey: 'NavBar.CustomerGroups.add',
       featureToggle: CUSTOMER_GROUPS,
       uriPath: 'customers/customer-groups/new',
-      permissions: [permissions.ManageCustomers, permissions.AddCustomerGroups],
+      permissions: [permissions.AddCustomerGroups],
     },
   ],
 };
@@ -83,11 +78,7 @@ const itemsDashboard = {
   uriPath: 'dashboard',
   labelKey: 'NavBar.Dashboard.title',
   icon: 'SpeedometerIcon',
-  permissions: [
-    permissions.ViewOrders,
-    permissions.ManageProducts,
-    permissions.ViewDashboard,
-  ],
+  permissions: [permissions.ViewDashboard],
   submenu: [],
 };
 
@@ -97,40 +88,30 @@ const itemsDiscounts = {
   labelKey: 'NavBar.Discounts.title',
   icon: 'TagMultiIcon',
   featureToggle: CAN_VIEW_DISCOUNTS,
-  permissions: [
-    permissions.ViewProducts,
-    permissions.ManageProducts,
-    permissions.ViewOrders,
-    permissions.ManageOrders,
-  ],
   submenu: [
     {
       key: 'ProductDiscounts',
       labelKey: 'NavBar.ProductDiscounts.list',
       uriPath: 'discounts/products',
-      permissions: [permissions.ViewProducts, permissions.ViewProductDiscounts],
+      permissions: [permissions.ViewProductDiscounts],
     },
     {
       key: 'CartDiscounts',
       labelKey: 'NavBar.CartDiscounts.list',
       uriPath: 'discounts/carts',
-      permissions: [permissions.ViewOrders, permissions.ViewCartDiscounts],
+      permissions: [permissions.ViewCartDiscounts],
     },
     {
       key: 'DiscountCodes',
       labelKey: 'NavBar.DiscountCodes.list',
       uriPath: 'discounts/codes',
-      permissions: [permissions.ViewOrders, permissions.ViewDiscountCodes],
+      permissions: [permissions.ViewDiscountCodes],
     },
     {
       key: 'AddDiscount',
       labelKey: 'NavBar.Discounts.add',
       uriPath: 'discounts/new',
-      permissions: [
-        permissions.ManageProducts,
-        permissions.ManageOrders,
-        permissions.AddDiscounts,
-      ],
+      permissions: [permissions.AddDiscounts],
     },
   ],
 };
@@ -141,19 +122,18 @@ const itemsOrders = {
   labelKey: 'NavBar.Orders.title',
   icon: 'CartIcon',
   featureToggle: CAN_VIEW_ORDERS,
-  permissions: [permissions.ViewOrders, permissions.ManageOrders],
   submenu: [
     {
       key: 'Orders',
       labelKey: 'NavBar.Orders.list',
       uriPath: 'orders',
-      permissions: [permissions.ViewOrders, permissions.ViewOrdersList],
+      permissions: [permissions.ViewOrdersList],
     },
     {
       key: 'AddOrders',
       labelKey: 'NavBar.Orders.add',
       uriPath: 'orders/new',
-      permissions: [permissions.ManageOrders, permissions.AddOrders],
+      permissions: [permissions.AddOrders],
     },
   ],
 };
@@ -163,39 +143,31 @@ const itemsProducts = {
   uriPath: 'products',
   labelKey: 'NavBar.Products.title',
   icon: 'BoxIcon',
-  permissions: [permissions.ViewProducts, permissions.ManageProducts],
   submenu: [
     {
       key: 'ProductList',
       labelKey: 'NavBar.Products.list',
       uriPath: 'products',
-      permissions: [permissions.ViewProducts, permissions.ViewProductsList],
+      permissions: [permissions.ViewProductsList],
     },
     {
       key: 'ModifiedProducts',
       labelKey: 'NavBar.Products.reviewModifiedProducts',
       uriPath: 'products/modified',
-      permissions: [
-        permissions.ManageProducts,
-        permissions.ViewModifiedProducts,
-      ],
+      permissions: [permissions.ViewModifiedProducts],
     },
     {
       key: 'PimSearchList',
       featureToggle: PIM_SEARCH,
       labelKey: 'NavBar.Products.pimSearchList',
       uriPath: 'products/pim-search',
-      permissions: [
-        permissions.ViewProducts,
-        permissions.ManageProducts,
-        permissions.ViewPimSearch,
-      ],
+      permissions: [permissions.ViewPimSearch],
     },
     {
       key: 'AddProduct',
       labelKey: 'NavBar.Products.add',
       uriPath: 'products/new',
-      permissions: [permissions.ManageProducts, permissions.AddProducts],
+      permissions: [permissions.AddProducts],
     },
     {
       key: 'DirectAccess',
@@ -212,50 +184,33 @@ const itemsProjectSettings = {
   uriPath: 'settings',
   labelKey: 'NavBar.Settings.title',
   icon: 'GearIcon',
-  permissions: [
-    permissions.ManageProject,
-    permissions.ManageProducts,
-    permissions.ViewProducts,
-    permissions.ViewSettings,
-  ],
+  permissions: [permissions.ManageProject],
   submenu: [
     {
       key: 'Project settings',
       labelKey: 'NavBar.ProjectSettings.title',
       uriPath: 'settings/project',
-      permissions: [permissions.ManageProject, permissions.ViewProjectSettings],
+      permissions: [permissions.ManageProject],
     },
     {
       key: 'Product types',
       labelKey: 'NavBar.ProductTypes.title',
       uriPath: 'settings/product-types',
-      permissions: [
-        permissions.ManageProject,
-        permissions.ManageProducts,
-        permissions.ViewProducts,
-        permissions.ViewProductTypes,
-      ],
+      permissions: [permissions.ViewProductTypes],
     },
     {
       key: 'Developer settings',
       featureToggle: DEVELOPER_SETTINGS,
       labelKey: 'NavBar.DeveloperSettings.title',
       uriPath: 'settings/developer',
-      permissions: [
-        permissions.ManageProject,
-        permissions.ManageApiClients,
-        permissions.ViewDeveloperSettings,
-      ],
+      permissions: [permissions.ViewDeveloperSettings],
     },
     {
       key: 'Custom Applications',
       featureToggle: CUSTOM_APPLICATIONS,
       labelKey: 'NavBar.CustomApplications.title',
       uriPath: 'settings/custom-applications',
-      permissions: [
-        permissions.ManageProject,
-        permissions.ViewCustomApplications,
-      ],
+      permissions: [permissions.ViewCustomApplications],
     },
   ],
 };
