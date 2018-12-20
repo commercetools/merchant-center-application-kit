dde07c598a664c6b845774032304ff3ac9ed091d
Nicola Molinari <nicola.molinari@commercetools.de>
2018-12-20 08:58:42 +0100

    refactor(examples/starter): migrate to latest version

---
+++ examples/starter/src/store.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+
+// Usage:
+//   <Provider store context={ApplicationStoreContext} />
+//   <ConnectedComponent context={ApplicationStoreContext} />
+export const ApplicationStoreContext = React.createContext(null);
