7d926278113abe9a5d0f7a745ce2bac3f430fc9c
tdeekens <tobias.deekens@commercetools.de>
2018-12-20 15:34:49 +0100

    refactor(permissions): remove support for legacy permissions

--- packages/permissions/src/components/authorized/authorized.js
+++ packages/permissions/src/components/authorized/authorized.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { wrapDisplayName } from 'recompose';
-import { defaultMemoize } from 'reselect';
-import warning from 'warning';
-import camelCase from 'lodash.camelcase';
-import upperFirst from 'lodash.upperfirst';
 import { ApplicationContext } from '@commercetools-frontend/application-shell-connectors';
 import {
   hasSomePermissions,
   hasEveryPermissions,
   getUnconfiguredPermissions,
 } from '../../utils/has-permissions';
-
-const ensurePermissionsKeyShape = defaultMemoize(demandedPermissions =>
-  demandedPermissions.map(permission =>
-    typeof permission === 'string'
-      ? permission
-      : upperFirst(camelCase(`${permission.mode} ${permission.resource}`))
-  )
-);
 
 class Authorized extends React.Component {
   static displayName = 'Authorized';
@@ -47,10 +35,7 @@ class Authorized extends React.Component {
       }
 
       // Legacy shape of permissions
-      return PropTypes.shape({
-        mode: PropTypes.string.isRequired,
-        resource: PropTypes.string.isRequired,
-      });
+      return null;
     },
     actualPermissions: PropTypes.objectOf(PropTypes.bool).isRequired,
     render: PropTypes.func,
@@ -58,28 +43,17 @@ class Authorized extends React.Component {
   static defaultProps = {
     shouldMatchSomePermissions: false,
   };
-  hasAlreadyLoggedDeprecationWarning = false;
-  componentDidUpdate() {
-    if (this.hasAlreadyLoggedDeprecationWarning) return;
-    const hasDemandedPermissionsWithDeprecatedFormat = this.props.demandedPermissions.some(
-      permission => typeof permission !== 'string'
-    );
-    const shouldSkipWarning =
-      process.env.NODE_ENV === 'production' ||
-      !hasDemandedPermissionsWithDeprecatedFormat;
-    warning(
-      shouldSkipWarning,
-      'The permission format with "{ mode, resource }" has been deprecated. Please use the constant values from the "@commercetools-frontend/permissions" package.'
-    );
-    if (!shouldSkipWarning) this.hasAlreadyLoggedDeprecationWarning = true;
-  }
   render() {
-    const demandedPermissions = ensurePermissionsKeyShape(
-      this.props.demandedPermissions
-    );
     const isAuthorized = this.props.shouldMatchSomePermissions
-      ? hasSomePermissions(demandedPermissions, this.props.actualPermissions)
-      : hasEveryPermissions(demandedPermissions, this.props.actualPermissions);
+      ? hasSomePermissions(
+          this.props.demandedPermissions,
+          this.props.actualPermissions
+        )
+      : hasEveryPermissions(
+          this.props.demandedPermissions,
+          this.props.actualPermissions
+        );
+
     return this.props.render(isAuthorized);
   }
 }
