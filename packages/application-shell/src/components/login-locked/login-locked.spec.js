6ea6e4a90fc61c72e8192cb24a143cb0e2cc4a46
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-02 09:56:32 +0100

    test(app-shell): convert LoginLocked test to RTL

--- packages/application-shell/src/components/login-locked/login-locked.spec.js
+++ packages/application-shell/src/components/login-locked/login-locked.spec.js
@@ -1,46 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import LockedAccount, { HelpDeskLink, ResetPasswordLink } from './login-locked';
+import { render, waitForElement } from '../../test-utils';
+import LockedAccount from './async';
 
-describe('LockedAccount', () => {
-  describe('rendering', () => {
-    let wrapper;
-    describe('rendering', () => {
-      beforeEach(() => {
-        wrapper = shallow(<LockedAccount />);
-      });
-      it('should match snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-      it('should render layout component', () => {
-        expect(wrapper).toRender('ServicePageResponseLayout');
-      });
-    });
-  });
-});
-describe('HelpDeskLink', () => {
-  let wrapper;
-  describe('rendering', () => {
-    beforeEach(() => {
-      wrapper = shallow(<HelpDeskLink />);
-    });
-    it('should match snapshot', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-});
-describe('ResetPasswordLink', () => {
-  describe('rendering', () => {
-    let wrapper;
-    describe('rendering', () => {
-      beforeEach(() => {
-        wrapper = shallow(
-          <ResetPasswordLink adminCenterUrl="https://ac.ct.com" />
-        );
-      });
-      it('should match snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-    });
-  });
+it('renders maintenance page for locked account', async () => {
+  const { getByText, queryByText } = render(<LockedAccount />);
+  await waitForElement(() => getByText(/Account Locked/i));
+  expect(queryByText(/reset your password/i)).toBeDefined();
+  expect(queryByText(/Help Desk/i)).toBeDefined();
 });
