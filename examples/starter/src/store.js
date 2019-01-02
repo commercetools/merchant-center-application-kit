a906b044811478d531533dc2f93b8ddf49cc5c7b
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-02 11:17:25 +0100

    chore: remove unused file

--- examples/starter/src/store.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import React from 'react';
-
-// Usage:
-//   <Provider store context={ApplicationStoreContext} />
-//   <ConnectedComponent context={ApplicationStoreContext} />
-export const ApplicationStoreContext = React.createContext(null);
