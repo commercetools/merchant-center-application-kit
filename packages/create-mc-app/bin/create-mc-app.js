e94e46c13d906d06d7bff184a1eb34a06aa4a3fa
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-07 14:31:50 +0100

    fix(create-mc-app): move templates into separate workspace and make them public, require template as dependency

--- packages/create-mc-app/bin/create-mc-app.js
+++ packages/create-mc-app/bin/create-mc-app.js
@@ -71,12 +71,18 @@ const packageManager = useYarn ? 'yarn' : 'npm';
 // TODO: we could check for min yarn/npm versions
 // See https://github.com/facebook/create-react-app/blob/0f4781e8507249ce29a9ac1409fece67c1a53c38/packages/create-react-app/createReactApp.js#L225-L254
 
-const templatePath = path.join(__dirname, '../templates', templateName);
+const templatePackageName = `@commercetools-frontend/application-template-${templateName}`;
+const modulePathForPackage = require.resolve
+  .paths(templatePackageName)
+  .find(modulesPath =>
+    fs.existsSync(path.join(modulesPath, templatePackageName))
+  );
+const templatePath = path.join(modulePathForPackage, templatePackageName);
 
 try {
   execSync(`cp -R ${templatePath}/ ${projectDirectoryPath}/`);
 } catch (error) {
-  console.error(
+  throw new Error(
     `Error while installing template "${templateName}" into "${projectDirectoryPath}":\n`,
     error
   );
@@ -106,7 +112,7 @@ fs.writeFileSync(
 
 // Install the dependencies
 try {
-  execSync(`${packageManager} install`);
+  execSync(`${packageManager} install`, { stdio: 'inherit' });
 } catch (error) {
   console.error(
     `Error while installing dependencies in "${projectDirectoryPath}":\n`,
