e5441e69e5a67a51d1d240ebb07df6386ef80c9a
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-07 21:06:05 +0100

    fix(scripts/app-templates): to pick correct branch name from travis env vars

--- scripts/install_app_templates.sh
+++ scripts/install_app_templates.sh
@@ -2,21 +2,13 @@
 
 set -e
 
-if [[ ! "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-  TEMPLATE_VERSION="$TRAVIS_PULL_REQUEST_BRANCH"
-else
-  TEMPLATE_VERSION=master
-fi
+TEMPLATE_VERSION="${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}"
 
-if [ -z "$TEMPLATE_VERSION" ]; then
-  echo "Skipping build"
-else
-  TEST_APP_NAME=test-install-app-starter
-  TEMPLATE=starter
+TEST_APP_NAME=test-install-app-starter
+TEMPLATE=starter
 
-  echo "Installing application for template $TEMPLATE"
-  yarn run create-mc-app \
-    --template="$TEMPLATE" \
-    --template-version="$TEMPLATE_VERSION" \
-    "$TEST_APP_NAME"
-fi
+echo "Installing application for template $TEMPLATE"
+yarn run create-mc-app \
+  --template="$TEMPLATE" \
+  --template-version="$TEMPLATE_VERSION" \
+  "$TEST_APP_NAME"
