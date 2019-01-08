7990a11bde848c7ec6dbc2ac40a8daa9cbb6fd86
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-08 14:24:31 +0100

    chore(scripts/app-template): run tests and production build of installed template

--- scripts/install_app_template.sh
+++ scripts/install_app_template.sh
@@ -20,3 +20,7 @@ node "$REPO_BINARIES/create-mc-app" \
   --template="$TEMPLATE" \
   --template-version="$TEMPLATE_VERSION" \
   "$TEST_APP_NAME"
+echo "Running tests for template $TEMPLATE"
+yarn --cwd "$TEST_APP_NAME" test
+echo "Running the production build of template $TEMPLATE"
+yarn --cwd "$TEST_APP_NAME" build
