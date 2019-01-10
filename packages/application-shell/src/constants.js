1fed9af50dbce53743ce0d68abbbd404be422fa4
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-10 20:10:05 +0100

    chore(app-shell): remove unused components

--- packages/application-shell/src/constants.js
+++ packages/application-shell/src/constants.js
@@ -1,10 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
-export const INTERCOM_TRACKING_STATUS = {
-  active: 'ACTIVE',
-  inactive: 'INACTIVE',
-  pending: 'PENDING',
-};
-
 export const STORAGE_KEYS = {
   NONCE: 'nonce',
   IS_AUTHENTICATED: 'isAuthenticated',
