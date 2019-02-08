a7e64d3a0f7e86faf0335b89955e2405ce69a290
tdeekens <tobias.deekens@commercetools.de>
2019-02-08 15:16:13 +0100

    fix(browser-history): to omit the question mark

--- packages/browser-history/index.js
+++ packages/browser-history/index.js
@@ -2,8 +2,9 @@ import createBrowserHistory from 'history/createBrowserHistory';
 import withQuery from 'history-query-enhancer';
 import { encode, decode } from 'qss';
 
-const history = withQuery({ parse: decode, stringify: encode })(
-  createBrowserHistory()
-);
+// Note: the "?" needs to be removed.
+const parse = search => decode(search.substring(1));
+
+const history = withQuery({ parse, stringify: encode })(createBrowserHistory());
 
 export default history;
