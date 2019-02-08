commercetools/merchant-center-application-kit

commit 3469093330f4d4061fd5118590295798d5a444fe

Replace `query-string` with `qss` (#348)
* refactor: remove query-string for qss

* refactor: tests to match qss api (casting etc)

packages/browser-history/index.js
@@ -1,7 +1,9 @@
 import createBrowserHistory from 'history/createBrowserHistory';
 import withQuery from 'history-query-enhancer';
-import queryString from 'query-string';
+import { encode, decode } from 'qss';
 
-const history = withQuery(queryString)(createBrowserHistory());
+const history = withQuery({ parse: decode, stringify: encode })(
+  createBrowserHistory()
+);
 
 export default history;

packages/application-shell/src/components/login-sso-callback/login-sso-callback.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { compose, withProps } from 'recompose';
-import qs from 'query-string';
+import { decode } from 'qss';
 import jwtDecode from 'jwt-decode';
 import { connect } from 'react-redux';
 import * as storage from '@commercetools-frontend/storage';
@@ -41,7 +41,7 @@ export class LoginSSOCallback extends React.PureComponent {
   };
 
   componentDidMount() {
-    const fragments = qs.parse(this.props.location.hash.substring(1));
+    const fragments = decode(this.props.location.hash.substring(1));
     const idToken = fragments.id_token;
     const decodedIdToken = jwtDecode(idToken);
     // Validate the nonce.

packages/application-shell/src/components/login-sso-callback/login-sso-callback.spec.js
@@ -9,7 +9,7 @@ jest.mock('@commercetools-frontend/storage');
 
 const createTestProps = props => ({
   location: {
-    hash: '#id_token=111',
+    hash: '#id_token=abcTestIdToken',
   },
   redirectTo: jest.fn(),
   requestAccessToken: jest.fn(),
@@ -64,7 +64,7 @@ describe('lifecylcle', () => {
       });
       it('should call requestAccessToken with idToken', () => {
         expect(props.requestAccessToken).toHaveBeenCalledWith(
-          expect.objectContaining({ idToken: '111' })
+          expect.objectContaining({ idToken: 'abcTestIdToken' })
         );
       });
       it('should call requestAccessToken with organization id', () => {

packages/sdk/src/utils/utils.js
@@ -1,4 +1,4 @@
-import qs from 'query-string';
+import { decode } from 'qss';
 import logger from './logger';
 
 export const parseUri = uri => {
@@ -7,7 +7,7 @@ export const parseUri = uri => {
 
   return {
     pathname: parser.pathname,
-    search: qs.parse(parser.search),
+    search: decode(parser.search.substring(1)),
   };
 };
 

packages/sdk/src/utils/utils.spec.js
@@ -18,13 +18,13 @@ describe('parseUri', () => {
     beforeEach(() => {
       result = parseUri('/project/products/new?foo=true&bar=5');
     });
-    it('should extract the', () => {
+    it('should extract the pathname', () => {
       expect(result.pathname).toBe('/project/products/new');
     });
     it('should extract the search', () => {
       expect(result.search).toEqual({
-        foo: 'true',
-        bar: '5',
+        foo: true,
+        bar: 5,
       });
     });
   });

commit d2534e8fe094a7479442f3a08d0b9e37bfc95cdb

fix(browser-history): to omit the question mark (#355)
#### Summary

Caused by: https://github.com/commercetools/merchant-center-application-kit/pull/348

We need to omit the `?` otherwise parts of applications relying on it will break.

packages/browser-history/index.js
@@ -2,8 +2,9 @@ import createBrowserHistory from 'history/createBrowserHistory';
 import withQuery from 'history-query-enhancer';
 import { encode, decode } from 'qss';
 
-const history = withQuery({ parse: decode, stringify: encode })(
-  createBrowserHistory()
-);
+// Note: the "?" needs to be removed.
+const parse = search => decode(search.substring(1));
+
+const history = withQuery({ parse, stringify: encode })(createBrowserHistory());
 
 export default history;