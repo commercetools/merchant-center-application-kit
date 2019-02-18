0ad6152a0d36317959a2333158c5eec3450df349
Malcolm Laing <malcolm.laing@commercetools.de>
2019-02-18 16:36:00 +0100

    chore: try again

--- scripts/run-percy.js
+++ scripts/run-percy.js
@@ -3,8 +3,21 @@
 const shelljs = require('shelljs');
 const runWhen = require('run-when');
 
+const getChangedFiles = () => {
+  // TODO: Remove "origin"?
+  const stdout = shelljs.exec(
+    `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE}`
+  ).stdout;
+  const files = stdout.split('\n').filter(f => f.length);
+
+  return files;
+};
+
+const changedFiles = getChangedFiles();
+
 runWhen([
   {
+    changedFiles: () => Promise.resolve(changedFiles),
     glob: ['packages/application-components', 'rollup.config.js', 'yarn.lock'],
     task() {
       console.log('Building visual testing app');
