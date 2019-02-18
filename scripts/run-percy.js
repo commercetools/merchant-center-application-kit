332aa90a5dbed3b38ec2361bfcecb3967e42e309
Malcolm Laing <malcolm.laing@commercetools.de>
2019-02-18 16:50:09 +0100

    chore: another go

--- scripts/run-percy.js
+++ scripts/run-percy.js
@@ -1,28 +1,32 @@
 /* eslint-disable no-console */
 
+const multimatch = require('multimatch');
 const shelljs = require('shelljs');
-const runWhen = require('run-when');
 
-const getChangedFiles = () => {
+const getChangedFiles = range => {
   // TODO: Remove "origin"?
-  const stdout = shelljs.exec(
-    `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE}`
-  ).stdout;
+  const stdout = shelljs.exec(`git diff --name-only ${range}`).stdout;
   const files = stdout.split('\n').filter(f => f.length);
 
   return files;
 };
 
-const changedFiles = getChangedFiles();
+const range = process.env.TRAVIS_COMMIT_RANGE || 'origin/master';
 
-runWhen([
-  {
-    changedFiles: () => Promise.resolve(changedFiles),
-    glob: ['packages/application-components', 'rollup.config.js', 'yarn.lock'],
-    task() {
-      console.log('Building visual testing app');
-      shelljs.exec('yarn visual-testing-app:build');
-      shelljs.exec('yarn percy --reporters jest-silent-reporter');
-    },
-  },
-]);
+const changedFiles = getChangedFiles(range);
+
+const rulesGlob = [
+  'packages/application-components/**',
+  'rollup.config.js',
+  'yarn.lock',
+];
+
+const diff = multimatch(changedFiles, rulesGlob);
+console.log('diff', diff);
+if (diff.length > 0) {
+  console.log('Building visual testing app');
+  shelljs.exec('yarn visual-testing-app:build');
+  shelljs.exec('yarn percy --reporters jest-silent-reporter');
+} else {
+  console.log('No files changed, skipping visual tests');
+}
