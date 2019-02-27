05c21274ac5f77ce3d0bb8e1e6dc32ff4ef86fdd
qmateub <qmateub@yahoo.es>
2019-02-27 18:01:00 +0100

    refactor(configure-apollo): add special mapping for deliveryItem type

--- packages/application-shell/src/configure-apollo.js
+++ packages/application-shell/src/configure-apollo.js
@@ -66,6 +66,8 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 
 const typeNamesWithoutIdAsIdentifier = ['Project', 'BaseMenu', 'NavbarMenu'];
 
+const typeNameWithReferencedEntity = ['Reference', 'DeliveryItem'];
+
 export const createApolloClient = () =>
   new ApolloClient({
     link,
@@ -82,7 +84,10 @@ export const createApolloClient = () =>
         // However, a reference has the shape { typeId, id } where the id is the
         // id of the referenced entity. If we didn't prefix ids of References
         // they would clash with the referenced resource.
-        if (result.__typename === 'Reference')
+        // The same use case happens for DeliveryItem type in which the `id`
+        // is really the lineItem id referenced in the delivery
+        // https://docs.commercetools.com/http-api-projects-orders.html#deliveryitem
+        if (typeNameWithReferencedEntity.includes(result.__typename))
           return `${result.__typename}:${result.id}`;
 
         return result.id;
