14e8c0792bf83f02c15c0c7824fbf42af95172aa
Nicola Molinari <nicola.molinari@commercetools.de>
2019-03-15 12:24:31 +0100

    chore(dev-server): add note about cookie

--- packages/mc-scripts/config/webpack-dev-server.config.js
+++ packages/mc-scripts/config/webpack-dev-server.config.js
@@ -120,6 +120,9 @@ module.exports = ({ proxy, allowedHost, contentBase, publicPath }) => ({
     });
     // Intercept the /logout page and "remove" the auth cookie value
     app.use('/logout', (request, response, next) => {
+      // NOTE: removing the cookie only works if your are running the MC API
+      // locally, otherwise the cookie won't get removed as it's set to a
+      // proper domain (e.g. commercetools.com), which we can't unset from localhost.
       response.setHeader(
         'Set-Cookie',
         [
