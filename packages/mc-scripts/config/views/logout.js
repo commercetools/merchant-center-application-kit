8ed3bd6bd4f5754838f5adef0579f4637dbf42a7
Nicola Molinari <nicola.molinari@commercetools.de>
2019-03-15 12:25:07 +0100

    refactor(dev-server/logout): explain the caveats about the session token on logout for local development

--- packages/mc-scripts/config/views/logout.js
+++ packages/mc-scripts/config/views/logout.js
@@ -1,6 +1,3 @@
 window.localStorage.removeItem('isAuthenticated');
 window.localStorage.removeItem('loginStrategy');
 window.localStorage.removeItem('activeProjectKey');
-window.location.replace(
-  `${window.location.origin}/login${window.location.search}`
-);
