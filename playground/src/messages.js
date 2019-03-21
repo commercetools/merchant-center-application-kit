956b7800f1473f3594978e820172b7ace3f04408
tdeekens <tobias.deekens@commercetools.de>
2019-03-21 20:31:12 +0100

    fix: extract message loading

---
+++ playground/src/messages.js
@@ -0,0 +1,28 @@
+const loadMessages = lang => {
+  let loadAppI18nPromise;
+  switch (lang) {
+    case 'de':
+      loadAppI18nPromise = import('./i18n/data/de.json' /* webpackChunkName: "app-i18n-de" */);
+      break;
+    case 'es':
+      loadAppI18nPromise = import('./i18n/data/es.json' /* webpackChunkName: "app-i18n-es" */);
+      break;
+    default:
+      loadAppI18nPromise = import('./i18n/data/en.json' /* webpackChunkName: "app-i18n-en" */);
+  }
+
+  return loadAppI18nPromise.then(
+    result => result.default,
+    error => {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Something went wrong while loading the app messages for ${lang}`,
+        error
+      );
+
+      return {};
+    }
+  );
+};
+
+export default loadMessages;
