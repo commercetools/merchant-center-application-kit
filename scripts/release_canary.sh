7577c89d2a0459156ce1b7783fa00250c5371060
tdeekens <tobias.deekens@commercetools.de>
2019-03-28 18:50:34 +0100

    fix: syntax error in release script

--- scripts/release_canary.sh
+++ scripts/release_canary.sh
@@ -4,7 +4,7 @@ set -e
 
 : "${NPM_TOKEN?Required env variable NPM_TOKEN}"
 
-COMMIT_MESSAGE=$(git log --format=oneline -n 1 $CIRCLE_SHA1))
+COMMIT_MESSAGE=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 
 # Only trigger the canary release when:
 # - the commit message does not contain `[skip publish]`
