7019d2d0fde9346031aff17b62643347e036d22c
tdeekens <tobias.deekens@commercetools.de>
2019-04-01 11:33:09 +0200

    refactor: remove public page container

--- packages/application-shell/src/components/public-page-container/messages.js
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  privacyPolicy: {
-    id: 'LoginPage.privacyPolicy',
-    description: 'The label for privacy policy link',
-    defaultMessage: 'Privacy Policy',
-  },
-});
