419d97ac7559a509cdaefe5dfb7cc4377e3616fc
tdeekens <tobias.deekens@commercetools.de>
2019-04-16 09:07:43 +0200

    feat(app-shell): add project not intialized status screen

---
+++ packages/application-shell/src/components/project-not-initialized/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProjectNotInitialized = React.lazy(() =>
+  import('./project-not-initialized' /* webpackChunkName: "project-not-initialzied" */)
+);
+
+export { ProjectNotInitialized as default };
---
+++ packages/application-shell/src/components/project-not-initialized/messages.js
@@ -0,0 +1,13 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  title: {
+    id: 'ProjectNotInitialized.title',
+    defaultMessage: 'Your project is not initialized yet',
+  },
+  paragraph1: {
+    id: 'ProjectNotInitialized.paragraph1',
+    defaultMessage:
+      'Initialization should not take longer than a few minutes. Please contact us at {mailto} in case the project does not get initialized.',
+  },
+});
