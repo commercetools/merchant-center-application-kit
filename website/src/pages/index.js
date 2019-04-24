d503c874b1bb74aaa9a2d99933a8966311e5c6d6
Nicola Molinari <nicola.molinari@commercetools.de>
2019-04-24 10:20:12 +0200

    refactor(website): do not render sidebar on home screen

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import pkg from '../../package.json';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+  <Layout showSidebar={false}>
+    <SEO title="Home" keywords={pkg.keywords} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
