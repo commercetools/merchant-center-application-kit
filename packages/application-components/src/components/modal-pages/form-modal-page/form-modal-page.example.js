322708a2932cbb047c0a514478eb1225f35552a5
jonnybel <jonnybel91@gmail.com>
2019-06-06 15:55:17 +0200

    refactor(website): to improve modal page examples

--- packages/application-components/src/components/modal-pages/form-modal-page/form-modal-page.example.js
+++ packages/application-components/src/components/modal-pages/form-modal-page/form-modal-page.example.js
@@ -1,60 +1,97 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { FormModalPage } from '@commercetools-frontend/application-components';
-import { TextField, TextInput } from '@commercetools-frontend/ui-kit';
+import {
+  Text,
+  Spacings,
+  TextInput,
+  TextField,
+  FlameIcon,
+  IconButton,
+  SearchIcon,
+  ToggleInput,
+  BinLinearIcon,
+} from '@commercetools-frontend/ui-kit';
 import ExampleWrapper from '../../internals/for-docs/example-wrapper';
 
+const exampleCustomControls = (
+  <Spacings.Inline>
+    <IconButton icon={<SearchIcon />} onClick={() => {}} />
+    <IconButton icon={<FlameIcon />} onClick={() => {}} />
+    <IconButton icon={<BinLinearIcon />} onClick={() => {}} />
+  </Spacings.Inline>
+);
+
 // This component is supposed to be used in the mdx documentation
-const FormModalPageExample = () => (
-  <ExampleWrapper
-    containerId="form-modal-page"
-    containerHeight="600px"
-    controllerTitle="Open the Form Modal Page by clicking on the button"
-    controllerButtonLabel="Open Form Modal Page"
-  >
-    {({ isOpen, toggle }) => (
-      <Formik
-        initialValues={{ email: '' }}
-        validate={formikValues => {
-          if (TextInput.isEmpty(formikValues.email)) {
-            return { email: { missing: true } };
-          }
-          return {};
-        }}
-        onSubmit={formikValues => {
-          alert(`email: ${formikValues.email}`);
-          toggle(false);
-        }}
-        render={formikProps => (
-          <FormModalPage
-            title="Lorem Ipsum"
-            isOpen={isOpen}
-            onClose={() => toggle(false)}
-            isPrimaryButtonDisabled={formikProps.isSubmitting}
-            onSecondaryButtonClick={() => {
-              formikProps.resetForm();
+const FormModalPageExample = () => {
+  const [useCustomControls, setUseCustomControls] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <ExampleWrapper
+        containerId="form-modal-page"
+        containerHeight="600px"
+        controllerTitle="Open the Form Modal Page by clicking on the button"
+        controllerButtonLabel="Open Form Modal Page"
+      >
+        {({ isOpen, toggle }) => (
+          <Formik
+            initialValues={{ email: '' }}
+            validate={formikValues => {
+              if (TextInput.isEmpty(formikValues.email)) {
+                return { email: { missing: true } };
+              }
+              return {};
+            }}
+            onSubmit={formikValues => {
+              alert(`email: ${formikValues.email}`);
               toggle(false);
             }}
-            onPrimaryButtonClick={formikProps.handleSubmit}
-            parentSelector={() => document.querySelector('#form-modal-page')}
-          >
-            <TextField
-              name="email"
-              title="Email"
-              isRequired={true}
-              value={formikProps.values.email}
-              errors={formikProps.errors.email}
-              touched={formikProps.touched.email}
-              onChange={formikProps.handleChange}
-              onBlur={formikProps.handleBlur}
-              onFocus={formikProps.handleFocus}
-            />
-          </FormModalPage>
+            render={formikProps => (
+              <FormModalPage
+                title="Lorem Ipsum"
+                isOpen={isOpen}
+                onClose={() => toggle(false)}
+                customControls={useCustomControls && exampleCustomControls}
+                isPrimaryButtonDisabled={formikProps.isSubmitting}
+                onSecondaryButtonClick={() => {
+                  formikProps.resetForm();
+                  toggle(false);
+                }}
+                onPrimaryButtonClick={formikProps.handleSubmit}
+                parentSelector={() =>
+                  document.querySelector('#form-modal-page')
+                }
+              >
+                <TextField
+                  name="email"
+                  title="Email"
+                  isRequired={true}
+                  value={formikProps.values.email}
+                  errors={formikProps.errors.email}
+                  touched={formikProps.touched.email}
+                  onChange={formikProps.handleChange}
+                  onBlur={formikProps.handleBlur}
+                  onFocus={formikProps.handleFocus}
+                />
+              </FormModalPage>
+            )}
+          />
         )}
-      />
-    )}
-  </ExampleWrapper>
-);
+      </ExampleWrapper>
+      <Spacings.Inline alignItems="center">
+        <ToggleInput
+          name="Use Custom Controls"
+          isChecked={useCustomControls}
+          onChange={event => {
+            setUseCustomControls(event.target.checked);
+          }}
+        />
+        <Text.Body>Use Custom Controls</Text.Body>
+      </Spacings.Inline>
+    </React.Fragment>
+  );
+};
 FormModalPageExample.displayName = 'FormModalPageExample';
 
 export default FormModalPageExample;
--- packages/application-components/src/components/modal-pages/tabular-modal-page/tabular-modal-page.example.js
+++ packages/application-components/src/components/modal-pages/tabular-modal-page/tabular-modal-page.example.js
@@ -3,103 +3,170 @@ import { TabularModalPage } from '@commercetools-frontend/application-components
 import {
   Text,
   Spacings,
-  SecondaryIconButton,
+  TextInput,
+  ToggleInput,
   PlusThinIcon,
   MinimizeIcon,
+  SecondaryIconButton,
   customProperties,
+  IconButton,
+  SearchIcon,
+  FlameIcon,
+  BinLinearIcon,
 } from '@commercetools-frontend/ui-kit';
 import { css } from '@emotion/core';
 import ExampleWrapper from '../../internals/for-docs/example-wrapper';
 
+const exampleCustomTitleRow = (
+  <Spacings.Inline scale="m">
+    <Spacings.Inline alignItems="center">
+      <label htmlFor="input-1">
+        <Text.Body isBold truncate>
+          Input 1
+        </Text.Body>
+      </label>
+      <TextInput id="input-1" value="" onChange={() => {}} />
+    </Spacings.Inline>
+
+    <Spacings.Inline alignItems="center">
+      <label htmlFor="input-2">
+        <Text.Body isBold truncate>
+          Input 2
+        </Text.Body>
+      </label>
+      <TextInput id="input-2" value="" onChange={() => {}} />
+    </Spacings.Inline>
+  </Spacings.Inline>
+);
+
+const exampleCustomControls = (
+  <Spacings.Inline>
+    <IconButton icon={<SearchIcon />} onClick={() => {}} />
+    <IconButton icon={<FlameIcon />} onClick={() => {}} />
+    <IconButton icon={<BinLinearIcon />} onClick={() => {}} />
+  </Spacings.Inline>
+);
+
 // This component is supposed to be used in the mdx documentation
 const TabularModalPageExample = () => {
   const [numberOfParagraphs, setNumberOfParagraphs] = React.useState(1);
+  const [useCustomTitleRow, setUseCustomTitleRow] = React.useState(false);
+  const [useCustomControls, setUseCustomControls] = React.useState(false);
 
   return (
-    <ExampleWrapper
-      containerId="tabular-modal"
-      containerHeight="600px"
-      controllerTitle="Open the Tabular Modal Page by clicking on the button"
-      controllerButtonLabel="Open Tabular Modal Page"
-    >
-      {({ isOpen, toggle }) => (
-        <TabularModalPage
-          title="Lorem ipsum"
-          isOpen={isOpen}
-          onClose={() => toggle(false)}
-          parentSelector={() => document.querySelector(`#tabular-modal`)}
-          // FIXME: use proper Tab components
-          tabControls={
-            <div
-              css={css`
-                min-height: 30px;
-                display: flex;
-                > * {
-                  min-width: 50px;
-                  cursor: pointer;
-                  &:first-of-type {
-                    border-bottom: 3px solid ${customProperties.colorPrimary};
-                    & > * {
-                      color: ${customProperties.colorPrimary};
-                    }
-                  }
-                }
-                > * + * {
-                  margin-left: 16px;
-                }
-              `}
-            >
-              <span>
-                <Text.Subheadline elementType="h4">Tab One</Text.Subheadline>
-              </span>
-              <span>
-                <Text.Subheadline elementType="h4">Tab Two</Text.Subheadline>
-              </span>
-              <span>
-                <Text.Subheadline elementType="h4">Tab Three</Text.Subheadline>
-              </span>
-            </div>
-          }
-          customControls={<React.Fragment />}
-        >
-          <Spacings.Stack>
-            {Array.from({ length: numberOfParagraphs }).map((_, index) => (
+    <React.Fragment>
+      <ExampleWrapper
+        containerId="tabular-modal"
+        containerHeight="600px"
+        controllerTitle="Open the Tabular Modal Page by clicking on the button"
+        controllerButtonLabel="Open Tabular Modal Page"
+      >
+        {({ isOpen, toggle }) => (
+          <TabularModalPage
+            title="Lorem ipsum"
+            isOpen={isOpen}
+            onClose={() => toggle(false)}
+            parentSelector={() => document.querySelector(`#tabular-modal`)}
+            // FIXME: use proper Tab components
+            tabControls={
               <div
                 css={css`
-                  overflow: auto;
+                  min-height: 30px;
+                  display: flex;
+                  > * {
+                    min-width: 50px;
+                    cursor: pointer;
+                    &:first-of-type {
+                      border-bottom: 3px solid ${customProperties.colorPrimary};
+                      & > * {
+                        color: ${customProperties.colorPrimary};
+                      }
+                    }
+                  }
+                  > * + * {
+                    margin-left: 16px;
+                  }
                 `}
-                key={index}
               >
-                <Spacings.Inset scale="m">
-                  <Text.Body>
-                    {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec turpis in risus elementum fringilla. Vestibulum nec vulputate metus, fringilla luctus nisl. Vestibulum mattis ultricies augue sagittis vestibulum. Nulla facilisi. Quisque tempor pulvinar efficitur. Praesent interdum ultrices leo. Vivamus non ex maximus justo egestas suscipit eget sed purus. Aliquam ut venenatis nulla. Fusce ac ligula viverra, blandit augue eget, congue turpis. Curabitur a sagittis leo. Nunc sed quam dictum, placerat nunc quis, luctus erat.`}
-                  </Text.Body>
-                  <Text.Body>
-                    {`Nam id orci ut risus accumsan pellentesque. Quisque efficitur eu arcu ut tristique. Praesent ornare varius leo, ut consequat lacus rutrum vel. Donec mollis leo id lectus vehicula tempor. Nulla facilisi. Fusce fringilla tellus ac ligula consequat suscipit. Sed consectetur molestie quam eu pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst.`}
-                  </Text.Body>
-                </Spacings.Inset>
+                <span>
+                  <Text.Subheadline elementType="h4">Tab One</Text.Subheadline>
+                </span>
+                <span>
+                  <Text.Subheadline elementType="h4">Tab Two</Text.Subheadline>
+                </span>
+                <span>
+                  <Text.Subheadline elementType="h4">
+                    Tab Three
+                  </Text.Subheadline>
+                </span>
               </div>
-            ))}
-            <Spacings.Inline>
-              <SecondaryIconButton
-                icon={<PlusThinIcon />}
-                label="Add paragraph"
-                onClick={() => setNumberOfParagraphs(numberOfParagraphs + 1)}
-              />
-              <SecondaryIconButton
-                icon={<MinimizeIcon />}
-                label="Remove paragraph"
-                onClick={() =>
-                  setNumberOfParagraphs(
-                    numberOfParagraphs > 0 ? numberOfParagraphs - 1 : 0
-                  )
-                }
-              />
-            </Spacings.Inline>
-          </Spacings.Stack>
-        </TabularModalPage>
-      )}
-    </ExampleWrapper>
+            }
+            customTitleRow={useCustomTitleRow && exampleCustomTitleRow}
+            customControls={
+              useCustomControls ? exampleCustomControls : <React.Fragment />
+            }
+          >
+            <Spacings.Stack>
+              {Array.from({ length: numberOfParagraphs }).map((_, index) => (
+                <div
+                  css={css`
+                    overflow: auto;
+                  `}
+                  key={index}
+                >
+                  <Spacings.Inset scale="m">
+                    <Text.Body>
+                      {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec turpis in risus elementum fringilla. Vestibulum nec vulputate metus, fringilla luctus nisl. Vestibulum mattis ultricies augue sagittis vestibulum. Nulla facilisi. Quisque tempor pulvinar efficitur. Praesent interdum ultrices leo. Vivamus non ex maximus justo egestas suscipit eget sed purus. Aliquam ut venenatis nulla. Fusce ac ligula viverra, blandit augue eget, congue turpis. Curabitur a sagittis leo. Nunc sed quam dictum, placerat nunc quis, luctus erat.`}
+                    </Text.Body>
+                    <Text.Body>
+                      {`Nam id orci ut risus accumsan pellentesque. Quisque efficitur eu arcu ut tristique. Praesent ornare varius leo, ut consequat lacus rutrum vel. Donec mollis leo id lectus vehicula tempor. Nulla facilisi. Fusce fringilla tellus ac ligula consequat suscipit. Sed consectetur molestie quam eu pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst.`}
+                    </Text.Body>
+                  </Spacings.Inset>
+                </div>
+              ))}
+              <Spacings.Inline>
+                <SecondaryIconButton
+                  icon={<PlusThinIcon />}
+                  label="Add paragraph"
+                  onClick={() => setNumberOfParagraphs(numberOfParagraphs + 1)}
+                />
+                <SecondaryIconButton
+                  icon={<MinimizeIcon />}
+                  label="Remove paragraph"
+                  onClick={() =>
+                    setNumberOfParagraphs(
+                      numberOfParagraphs > 0 ? numberOfParagraphs - 1 : 0
+                    )
+                  }
+                />
+              </Spacings.Inline>
+            </Spacings.Stack>
+          </TabularModalPage>
+        )}
+      </ExampleWrapper>
+      <Spacings.Inline>
+        <Spacings.Inline alignItems="center">
+          <ToggleInput
+            name="Use Custom Title Row"
+            isChecked={useCustomTitleRow}
+            onChange={event => {
+              setUseCustomTitleRow(event.target.checked);
+            }}
+          />
+          <Text.Body>Use Custom Title Row</Text.Body>
+        </Spacings.Inline>
+        <Spacings.Inline alignItems="center">
+          <ToggleInput
+            name="Use Custom Controls"
+            isChecked={useCustomControls}
+            onChange={event => {
+              setUseCustomControls(event.target.checked);
+            }}
+          />
+          <Text.Body>Use Custom Controls</Text.Body>
+        </Spacings.Inline>
+      </Spacings.Inline>
+    </React.Fragment>
   );
 };
 TabularModalPageExample.displayName = 'TabularModalPageExample';
