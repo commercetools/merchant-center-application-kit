b73c98398a9d5ed371b6204a6504471cd8534b0f
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-07 13:49:21 +0200

    feat(storage): use typescript

---
+++ packages/storage/index.ts
@@ -0,0 +1,5 @@
+export const put = (key: string, value: string) =>
+  window.localStorage.setItem(key, value);
+export const get = (key: string) => window.localStorage.getItem(key);
+export const remove = (key: string) => window.localStorage.removeItem(key);
+export const clear = () => window.localStorage.clear();
