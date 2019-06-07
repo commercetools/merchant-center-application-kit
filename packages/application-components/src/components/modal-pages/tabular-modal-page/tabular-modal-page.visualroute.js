8fc915e0143c7e1fb40a6b58c4161455019a6c10
Nicola Molinari <nicola.molinari@commercetools.de>
2019-06-07 09:58:41 +0200

    refactor(modal-pages): improve VRTs for tabular modal page

--- packages/application-components/src/components/modal-pages/tabular-modal-page/tabular-modal-page.visualroute.js
+++ packages/application-components/src/components/modal-pages/tabular-modal-page/tabular-modal-page.visualroute.js
@@ -27,6 +27,7 @@ const ModalPageWithPortalParentSelector = ({ portalId, ...props }) => (
     />
     <TabularModalPage
       title="Lorem ipsum"
+      subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
       level={1}
       isOpen={true}
       onClose={() => {}}
@@ -72,18 +73,33 @@ ModalPageWithPortalParentSelector.propTypes = {
   portalId: PropTypes.string.isRequired,
 };
 
+const Content = () => (
+  <Spacings.Stack scale="m">
+    <Text.Body>
+      {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec turpis in risus elementum fringilla. Vestibulum nec vulputate metus, fringilla luctus nisl. Vestibulum mattis ultricies augue sagittis vestibulum. Nulla facilisi. Quisque tempor pulvinar efficitur. Praesent interdum ultrices leo. Vivamus non ex maximus justo egestas suscipit eget sed purus. Aliquam ut venenatis nulla. Fusce ac ligula viverra, blandit augue eget, congue turpis. Curabitur a sagittis leo. Nunc sed quam dictum, placerat nunc quis, luctus erat.`}
+    </Text.Body>
+    <Text.Body>
+      {`Nam id orci ut risus accumsan pellentesque. Quisque efficitur eu arcu ut tristique. Praesent ornare varius leo, ut consequat lacus rutrum vel. Donec mollis leo id lectus vehicula tempor. Nulla facilisi. Fusce fringilla tellus ac ligula consequat suscipit. Sed consectetur molestie quam eu pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst.`}
+    </Text.Body>
+  </Spacings.Stack>
+);
+
 export const component = () => (
   <Suite>
     <Spec label="TabularModalPage" size="xl">
-      <ModalPageWithPortalParentSelector portalId="tabular-modal-one">
-        <Spacings.Inset scale="m">
-          <Text.Body>
-            {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec turpis in risus elementum fringilla. Vestibulum nec vulputate metus, fringilla luctus nisl. Vestibulum mattis ultricies augue sagittis vestibulum. Nulla facilisi. Quisque tempor pulvinar efficitur. Praesent interdum ultrices leo. Vivamus non ex maximus justo egestas suscipit eget sed purus. Aliquam ut venenatis nulla. Fusce ac ligula viverra, blandit augue eget, congue turpis. Curabitur a sagittis leo. Nunc sed quam dictum, placerat nunc quis, luctus erat.`}
-          </Text.Body>
-          <Text.Body>
-            {`Nam id orci ut risus accumsan pellentesque. Quisque efficitur eu arcu ut tristique. Praesent ornare varius leo, ut consequat lacus rutrum vel. Donec mollis leo id lectus vehicula tempor. Nulla facilisi. Fusce fringilla tellus ac ligula consequat suscipit. Sed consectetur molestie quam eu pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst.`}
-          </Text.Body>
-        </Spacings.Inset>
+      <ModalPageWithPortalParentSelector
+        portalId="tabular-modal-page-default"
+        customControls={<React.Fragment />}
+      >
+        <Content />
+      </ModalPageWithPortalParentSelector>
+    </Spec>
+    <Spec
+      label="TabularModalPage - with secondary and primary buttons"
+      size="xl"
+    >
+      <ModalPageWithPortalParentSelector portalId="tabular-modal-page-default-controls">
+        <Content />
       </ModalPageWithPortalParentSelector>
     </Spec>
     <Spec label="TabularModalPage - with Custom Controls" size="xl">
@@ -95,19 +111,15 @@ export const component = () => (
             <IconButton icon={<BinLinearIcon />} onClick={() => {}} />
           </Spacings.Inline>
         }
-        portalId="tabular-modal-custom-controls"
+        portalId="tabular-modal-page-custom-controls"
       >
-        <Spacings.Inset scale="m">
-          <Text.Body>
-            {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec turpis in risus elementum fringilla. Vestibulum nec vulputate metus, fringilla luctus nisl. Vestibulum mattis ultricies augue sagittis vestibulum. Nulla facilisi. Quisque tempor pulvinar efficitur. Praesent interdum ultrices leo. Vivamus non ex maximus justo egestas suscipit eget sed purus. Aliquam ut venenatis nulla. Fusce ac ligula viverra, blandit augue eget, congue turpis. Curabitur a sagittis leo. Nunc sed quam dictum, placerat nunc quis, luctus erat.`}
-          </Text.Body>
-          <Text.Body>
-            {`Nam id orci ut risus accumsan pellentesque. Quisque efficitur eu arcu ut tristique. Praesent ornare varius leo, ut consequat lacus rutrum vel. Donec mollis leo id lectus vehicula tempor. Nulla facilisi. Fusce fringilla tellus ac ligula consequat suscipit. Sed consectetur molestie quam eu pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst.`}
-          </Text.Body>
-        </Spacings.Inset>
+        <Content />
       </ModalPageWithPortalParentSelector>
     </Spec>
-    <Spec label="TabularModalPage - with Custom Title Row" size="xl">
+    <Spec
+      label="TabularModalPage - with Custom Title Row and no controls"
+      size="xl"
+    >
       <ModalPageWithPortalParentSelector
         customTitleRow={
           <Spacings.Inline scale="m">
@@ -129,16 +141,89 @@ export const component = () => (
             </Spacings.Inline>
           </Spacings.Inline>
         }
-        portalId="tabular-modal-custom-title-row"
+        customControls={<React.Fragment />}
+        portalId="tabular-modal-page-custom-title-row-no-controls"
       >
-        <Spacings.Inset scale="m">
-          <Text.Body>
-            {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec turpis in risus elementum fringilla. Vestibulum nec vulputate metus, fringilla luctus nisl. Vestibulum mattis ultricies augue sagittis vestibulum. Nulla facilisi. Quisque tempor pulvinar efficitur. Praesent interdum ultrices leo. Vivamus non ex maximus justo egestas suscipit eget sed purus. Aliquam ut venenatis nulla. Fusce ac ligula viverra, blandit augue eget, congue turpis. Curabitur a sagittis leo. Nunc sed quam dictum, placerat nunc quis, luctus erat.`}
-          </Text.Body>
-          <Text.Body>
-            {`Nam id orci ut risus accumsan pellentesque. Quisque efficitur eu arcu ut tristique. Praesent ornare varius leo, ut consequat lacus rutrum vel. Donec mollis leo id lectus vehicula tempor. Nulla facilisi. Fusce fringilla tellus ac ligula consequat suscipit. Sed consectetur molestie quam eu pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst.`}
-          </Text.Body>
-        </Spacings.Inset>
+        <Content />
+      </ModalPageWithPortalParentSelector>
+    </Spec>
+    <Spec
+      label="TabularModalPage - with Custom Title Row and default controls"
+      size="xl"
+    >
+      <ModalPageWithPortalParentSelector
+        customTitleRow={
+          <Spacings.Inline scale="m">
+            <Spacings.Inline alignItems="center">
+              <label htmlFor="input-1">
+                <Text.Body isBold truncate>
+                  Input 1
+                </Text.Body>
+              </label>
+              <TextInput id="input-1" value="" onChange={() => {}} />
+            </Spacings.Inline>
+            <Spacings.Inline alignItems="center">
+              <label htmlFor="input-2">
+                <Text.Body isBold truncate>
+                  Input 2
+                </Text.Body>
+              </label>
+              <TextInput id="input-2" value="" onChange={() => {}} />
+            </Spacings.Inline>
+          </Spacings.Inline>
+        }
+        portalId="tabular-modal-page-custom-title-row-no-controls"
+      >
+        <Content />
+      </ModalPageWithPortalParentSelector>
+    </Spec>
+    <Spec
+      label="TabularModalPage - with Custom Title Row and custom controls"
+      size="xl"
+    >
+      <ModalPageWithPortalParentSelector
+        customTitleRow={
+          <Spacings.Inline scale="m">
+            <Spacings.Inline alignItems="center">
+              <label htmlFor="input-1">
+                <Text.Body isBold truncate>
+                  Input 1
+                </Text.Body>
+              </label>
+              <TextInput id="input-1" value="" onChange={() => {}} />
+            </Spacings.Inline>
+            <Spacings.Inline alignItems="center">
+              <label htmlFor="input-2">
+                <Text.Body isBold truncate>
+                  Input 2
+                </Text.Body>
+              </label>
+              <TextInput id="input-2" value="" onChange={() => {}} />
+            </Spacings.Inline>
+          </Spacings.Inline>
+        }
+        customControls={
+          <Spacings.Inline>
+            <IconButton icon={<SearchIcon />} onClick={() => {}} />
+            <IconButton icon={<FlameIcon />} onClick={() => {}} />
+            <IconButton icon={<BinLinearIcon />} onClick={() => {}} />
+          </Spacings.Inline>
+        }
+        portalId="tabular-modal-page-custom-title-row-no-controls"
+      >
+        <Content />
+      </ModalPageWithPortalParentSelector>
+    </Spec>
+    <Spec label="TabularModalPage - long content" size="xl">
+      <ModalPageWithPortalParentSelector
+        customControls={<React.Fragment />}
+        portalId="tabular-modal-page-custom-title-row-no-controls"
+      >
+        <Content />
+        <Content />
+        <Content />
+        <Content />
+        <Content />
       </ModalPageWithPortalParentSelector>
     </Spec>
   </Suite>
