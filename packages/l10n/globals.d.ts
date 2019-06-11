548e5c5dacc91aa6792b2aac15d60319ee6b4ef7
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-11 14:11:04 +0200

    chore: fixup

--- packages/l10n/globals.d.ts
+++ packages/l10n/globals.d.ts
@@ -1,14 +1,2 @@
 export {};
 import 'jest-enzyme';
-
-declare module '@commercetools/jest-enzyme-matchers' {
-  interface Matchers<R> {
-    toHaveProp: (htmlElement: string) => object;
-    toHaveState: () => any;
-  }
-
-  interface Expect {
-    toHaveProp: (htmlElement: string) => object;
-    toHaveState: () => any;
-  }
-}
