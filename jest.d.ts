3e150090ae2efe841b456275e572e18bfcedf2cd
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-12 08:52:00 +0200

    chore: pr review

--- jest.d.ts
+++ jest.d.ts
@@ -1,7 +1,8 @@
+// The following matchers are part of https://www.npmjs.com/package/@commercetools/jest-enzyme-matchers
 declare namespace jest {
   interface Matchers<R> {
-    toHaveProp(selector: string): void;
-    toRender(selector: string): void;
-    toHaveState(selector: string, arg2: any): void;
+    toHaveProp(selector: string): R;
+    toRender(selector: string): R;
+    toHaveState(selector: string, expected: unknown): R;
   }
 }
