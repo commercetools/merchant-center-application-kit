b12a4b1cf9fb337b343ab4fd56df46be664c709e
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-24 10:16:48 +0200

    chore: fix prop types

--- application-templates/starter/src/routes.js
+++ application-templates/starter/src/routes.js
@@ -28,6 +28,7 @@ const ApplicationRoutes = ({ match }) => (
 ApplicationRoutes.displayName = 'ApplicationRoutes';
 ApplicationRoutes.propTypes = {
   match: PropTypes.shape({
+    path: PropTyeps.string,
     params: PropTypes.shape({
       projectKey: PropTypes.string.isRequired,
     }).isRequired,
