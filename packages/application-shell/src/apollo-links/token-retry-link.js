5b0de8393cfcf5211bf5c82bcb7d6cab01f33d08
suǝʞǝǝpʇ <tobias.deekens@commercetools.de>
2019-06-27 13:49:22 +0200

    fix(app-shell): to support admin service target (#792)

--- packages/application-shell/src/apollo-links/token-retry-link.js
+++ packages/application-shell/src/apollo-links/token-retry-link.js
@@ -1,11 +1,21 @@
 import { RetryLink } from 'apollo-link-retry';
-import { STATUS_CODES } from '@commercetools-frontend/constants';
+import {
+  STATUS_CODES,
+  GRAPHQL_TARGETS,
+} from '@commercetools-frontend/constants';
 
 // This link retries requests to the CTP API that have been rejected
 // because of an invalid/expired oauth token.
 // To do so, we resend the request with the header "X-Force-Token: true"
 // so that the MC BE can issue a new token.
 // NOTE: the retry is not meant to work for the MC access token.
+
+export const getDoesGraphQLTargetSupportTokenRetry = requestHeaders =>
+  [
+    GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM,
+    GRAPHQL_TARGETS.ADMINISTRATION_SERVICE,
+  ].includes(requestHeaders['X-Graphql-Target']);
+
 const tokenRetryLink = new RetryLink({
   attempts: (count, operation, error) => {
     const { headers: requestHeaders } = operation.getContext();
@@ -13,7 +23,7 @@ const tokenRetryLink = new RetryLink({
       error &&
       error.statusCode &&
       error.statusCode === STATUS_CODES.UNAUTHORIZED &&
-      requestHeaders['X-Graphql-Target'] === 'ctp' &&
+      getDoesGraphQLTargetSupportTokenRetry(requestHeaders) &&
       count === 1
     ) {
       operation.setContext(({ headers }) => ({
