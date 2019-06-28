c84d597c87c731e9a8732cafcab02f88083f03e5
Nicola Molinari <nicola.molinari@commercetools.de>
2019-06-28 13:33:25 +0200

    refactor: react-intl addLocaleData leftovers

--- packages/application-components/test-utils/visual/suite.js
+++ packages/application-components/test-utils/visual/suite.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import de from 'react-intl/locale-data/de';
-import es from 'react-intl/locale-data/es';
-
-addLocaleData(en);
-addLocaleData(de);
-addLocaleData(es);
+import { IntlProvider } from 'react-intl';
 
 const Suite = props => (
   <IntlProvider locale={props.locale}>
