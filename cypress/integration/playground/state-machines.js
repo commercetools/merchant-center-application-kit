d5146d72ef4abcc07ba318fa61291aab4d1222eb
Nicola Molinari <nicola.molinari@commercetools.de>
2019-07-11 14:44:24 +0200

    refactor(cypress): to simplify login setup

--- cypress/integration/playground/state-machines.js
+++ cypress/integration/playground/state-machines.js
@@ -1,12 +1,9 @@
-import { URL_BASE } from '../../support/urls';
+import { URL_STATE_MACHINES } from '../../support/urls';
 
 describe('State machines', () => {
   describe('List view', () => {
     it('should render page', () => {
-      cy.setDesktopViewport();
-      cy.login();
-      cy.visit(`${URL_BASE}/state-machines`);
-
+      cy.login({ redirectToUri: URL_STATE_MACHINES });
       cy.getByText('State Machines').should('exist');
     });
   });
