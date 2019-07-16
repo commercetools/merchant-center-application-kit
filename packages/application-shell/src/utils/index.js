a65e260c445f469ce3afea8bf97d3e8d1bc190c7
tdeekens <tobias.deekens@commercetools.de>
2019-07-16 13:23:09 +0200

    feat(app-shell): team id selector

--- packages/application-shell/src/utils/index.js
+++ packages/application-shell/src/utils/index.js
@@ -2,6 +2,9 @@ export {
   default as selectProjectKeyFromLocalStorage,
 } from './select-project-key-from-local-storage';
 export {
+  default as selectTeamIdFromLocalStorage,
+} from './select-team-id-from-local-storage';
+export {
   default as selectProjectKeyFromUrl,
 } from './select-project-key-from-url';
 export { default as selectUserId } from './select-user-id';
