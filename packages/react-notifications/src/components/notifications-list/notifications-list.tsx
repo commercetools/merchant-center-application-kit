92cb93c36486e2a354c47131eb310afbe5ee4800
Nicola Molinari <nicola.molinari@commercetools.de>
2019-07-29 15:21:50 +0200

    fix: lint rule react hooks

--- packages/react-notifications/src/components/notifications-list/notifications-list.tsx
+++ packages/react-notifications/src/components/notifications-list/notifications-list.tsx
@@ -30,199 +30,209 @@ type Props = {
   domain: TAppNotificationDomain;
 };
 
-const NotificationsList = (props: Props) => {
+const NotificationsListGlobal = (props: Props) => {
   const dispatch = useDispatch();
   const mapCustomNotificationToComponent = useCustomNotificationComponent();
+  const notifications = useSelector<TAppState, TAppNotificationGlobal[]>(
+    selectGlobalNotifications
+  );
+  return (
+    <div css={getStyles(props)}>
+      {notifications.map(notification => {
+        // 1. Check if there is a custom notification component first
+        const CustomNotificationComponent = mapCustomNotificationToComponent(
+          notification
+        ) as Notification;
+        if (CustomNotificationComponent) {
+          return (
+            <CustomNotificationComponent
+              key={notification.id}
+              notification={notification}
+              dismiss={() => {
+                dispatch(removeNotification(notification.id));
+              }}
+            />
+          );
+        }
 
+        switch (notification.kind) {
+          case NOTIFICATION_KINDS_GLOBAL.error:
+          case NOTIFICATION_KINDS_GLOBAL.warning:
+          case NOTIFICATION_KINDS_GLOBAL.info:
+          case NOTIFICATION_KINDS_GLOBAL.success: {
+            const { values, ...genericNotification } = notification;
+            return (
+              <GenericNotification
+                key={notification.id}
+                notification={genericNotification}
+                dismiss={() => {
+                  dispatch(removeNotification(notification.id));
+                }}
+              />
+            );
+          }
+          case NOTIFICATION_KINDS_GLOBAL['unexpected-error']: {
+            const { text, ...errorNotification } = notification;
+            return (
+              <UnexpectedErrorNotification
+                key={notification.id}
+                notification={errorNotification}
+                dismiss={() => {
+                  dispatch(removeNotification(notification.id));
+                }}
+              />
+            );
+          }
+          default: {
+            if (process.env.NODE_ENV !== 'production') {
+              // eslint-disable-next-line no-console
+              reportErrorToSentry(
+                new Error(
+                  `@commercetools-frontend/react-notifications. Invalid prop "kind" supplied: ${notification.kind}.`
+                ),
+                { extra: notification }
+              );
+            }
+            return null;
+          }
+        }
+      })}
+    </div>
+  );
+};
+const NotificationsListPage = (props: Props) => {
+  const dispatch = useDispatch();
+  const mapCustomNotificationToComponent = useCustomNotificationComponent();
+  const notifications = useSelector<TAppState, TAppNotificationPage[]>(
+    selectPageNotifications
+  );
+  return (
+    <div css={getStyles(props)}>
+      {notifications.map(notification => {
+        // 1. Check if there is a custom notification component first
+        const CustomNotificationComponent = mapCustomNotificationToComponent(
+          notification
+        ) as Notification;
+        if (CustomNotificationComponent) {
+          return (
+            <CustomNotificationComponent
+              key={notification.id}
+              notification={notification}
+              dismiss={() => {
+                dispatch(removeNotification(notification.id));
+              }}
+            />
+          );
+        }
+
+        switch (notification.kind) {
+          case NOTIFICATION_KINDS_PAGE.error:
+          case NOTIFICATION_KINDS_PAGE.warning:
+          case NOTIFICATION_KINDS_PAGE.info:
+          case NOTIFICATION_KINDS_PAGE.success: {
+            const { values, ...genericNotification } = notification;
+            return (
+              <GenericNotification
+                key={notification.id}
+                notification={{
+                  ...genericNotification,
+                  kind: notification.kind,
+                }}
+                dismiss={() => {
+                  dispatch(removeNotification(notification.id));
+                }}
+              />
+            );
+          }
+          case NOTIFICATION_KINDS_PAGE['api-error']: {
+            const { text, ...errorNotification } = notification;
+            return (
+              <ApiErrorNotification
+                key={notification.id}
+                notification={errorNotification}
+                dismiss={() => {
+                  dispatch(removeNotification(notification.id));
+                }}
+              />
+            );
+          }
+          case NOTIFICATION_KINDS_PAGE['unexpected-error']: {
+            const { text, ...errorNotification } = notification;
+            return (
+              <UnexpectedErrorNotification
+                key={notification.id}
+                notification={{
+                  ...errorNotification,
+                  values: notification.values as TAppNotificationValuesUnexpectedError,
+                }}
+                dismiss={() => {
+                  dispatch(removeNotification(notification.id));
+                }}
+              />
+            );
+          }
+          default:
+            return null;
+        }
+      })}
+    </div>
+  );
+};
+const NotificationsListSide = (props: Props) => {
+  const dispatch = useDispatch();
+  const mapCustomNotificationToComponent = useCustomNotificationComponent();
+  const notifications = useSelector<TAppState, TAppNotificationSide[]>(
+    selectSideNotifications
+  );
+  return (
+    <div css={getStyles(props)}>
+      {notifications.map(notification => {
+        // 1. Check if there is a custom notification component first
+        const CustomNotificationComponent = mapCustomNotificationToComponent(
+          notification
+        ) as Notification;
+        if (CustomNotificationComponent) {
+          return (
+            <CustomNotificationComponent
+              key={notification.id}
+              notification={notification}
+              dismiss={() => {
+                dispatch(removeNotification(notification.id));
+              }}
+            />
+          );
+        }
+
+        switch (notification.kind) {
+          case NOTIFICATION_KINDS_SIDE.error:
+          case NOTIFICATION_KINDS_SIDE.warning:
+          case NOTIFICATION_KINDS_SIDE.info:
+          case NOTIFICATION_KINDS_SIDE.success:
+            return (
+              <GenericNotification
+                key={notification.id}
+                notification={notification}
+                dismiss={() => {
+                  dispatch(removeNotification(notification.id));
+                }}
+              />
+            );
+          default:
+            return null;
+        }
+      })}
+    </div>
+  );
+};
+
+const NotificationsList = (props: Props) => {
   switch (props.domain) {
-    case NOTIFICATION_DOMAINS.GLOBAL: {
-      const notifications = useSelector<TAppState, TAppNotificationGlobal[]>(
-        selectGlobalNotifications
-      );
-      return (
-        <div css={getStyles(props)}>
-          {notifications.map(notification => {
-            // 1. Check if there is a custom notification component first
-            const CustomNotificationComponent = mapCustomNotificationToComponent(
-              notification
-            ) as Notification;
-            if (CustomNotificationComponent) {
-              return (
-                <CustomNotificationComponent
-                  key={notification.id}
-                  notification={notification}
-                  dismiss={() => {
-                    dispatch(removeNotification(notification.id));
-                  }}
-                />
-              );
-            }
-
-            switch (notification.kind) {
-              case NOTIFICATION_KINDS_GLOBAL.error:
-              case NOTIFICATION_KINDS_GLOBAL.warning:
-              case NOTIFICATION_KINDS_GLOBAL.info:
-              case NOTIFICATION_KINDS_GLOBAL.success: {
-                const { values, ...genericNotification } = notification;
-                return (
-                  <GenericNotification
-                    key={notification.id}
-                    notification={genericNotification}
-                    dismiss={() => {
-                      dispatch(removeNotification(notification.id));
-                    }}
-                  />
-                );
-              }
-              case NOTIFICATION_KINDS_GLOBAL['unexpected-error']: {
-                const { text, ...errorNotification } = notification;
-                return (
-                  <UnexpectedErrorNotification
-                    key={notification.id}
-                    notification={errorNotification}
-                    dismiss={() => {
-                      dispatch(removeNotification(notification.id));
-                    }}
-                  />
-                );
-              }
-              default: {
-                if (process.env.NODE_ENV !== 'production') {
-                  // eslint-disable-next-line no-console
-                  reportErrorToSentry(
-                    new Error(
-                      `@commercetools-frontend/react-notifications. Invalid prop "kind" supplied: ${notification.kind}.`
-                    ),
-                    { extra: notification }
-                  );
-                }
-                return null;
-              }
-            }
-          })}
-        </div>
-      );
-    }
-    case NOTIFICATION_DOMAINS.PAGE: {
-      const notifications = useSelector<TAppState, TAppNotificationPage[]>(
-        selectPageNotifications
-      );
-      return (
-        <div css={getStyles(props)}>
-          {notifications.map(notification => {
-            // 1. Check if there is a custom notification component first
-            const CustomNotificationComponent = mapCustomNotificationToComponent(
-              notification
-            ) as Notification;
-            if (CustomNotificationComponent) {
-              return (
-                <CustomNotificationComponent
-                  key={notification.id}
-                  notification={notification}
-                  dismiss={() => {
-                    dispatch(removeNotification(notification.id));
-                  }}
-                />
-              );
-            }
-
-            switch (notification.kind) {
-              case NOTIFICATION_KINDS_PAGE.error:
-              case NOTIFICATION_KINDS_PAGE.warning:
-              case NOTIFICATION_KINDS_PAGE.info:
-              case NOTIFICATION_KINDS_PAGE.success: {
-                const { values, ...genericNotification } = notification;
-                return (
-                  <GenericNotification
-                    key={notification.id}
-                    notification={{
-                      ...genericNotification,
-                      kind: notification.kind,
-                    }}
-                    dismiss={() => {
-                      dispatch(removeNotification(notification.id));
-                    }}
-                  />
-                );
-              }
-              case NOTIFICATION_KINDS_PAGE['api-error']: {
-                const { text, ...errorNotification } = notification;
-                return (
-                  <ApiErrorNotification
-                    key={notification.id}
-                    notification={errorNotification}
-                    dismiss={() => {
-                      dispatch(removeNotification(notification.id));
-                    }}
-                  />
-                );
-              }
-              case NOTIFICATION_KINDS_PAGE['unexpected-error']: {
-                const { text, ...errorNotification } = notification;
-                return (
-                  <UnexpectedErrorNotification
-                    key={notification.id}
-                    notification={{
-                      ...errorNotification,
-                      values: notification.values as TAppNotificationValuesUnexpectedError,
-                    }}
-                    dismiss={() => {
-                      dispatch(removeNotification(notification.id));
-                    }}
-                  />
-                );
-              }
-              default:
-                return null;
-            }
-          })}
-        </div>
-      );
-    }
-    case NOTIFICATION_DOMAINS.SIDE: {
-      const notifications = useSelector<TAppState, TAppNotificationSide[]>(
-        selectSideNotifications
-      );
-      return (
-        <div css={getStyles(props)}>
-          {notifications.map(notification => {
-            // 1. Check if there is a custom notification component first
-            const CustomNotificationComponent = mapCustomNotificationToComponent(
-              notification
-            ) as Notification;
-            if (CustomNotificationComponent) {
-              return (
-                <CustomNotificationComponent
-                  key={notification.id}
-                  notification={notification}
-                  dismiss={() => {
-                    dispatch(removeNotification(notification.id));
-                  }}
-                />
-              );
-            }
-
-            switch (notification.kind) {
-              case NOTIFICATION_KINDS_SIDE.error:
-              case NOTIFICATION_KINDS_SIDE.warning:
-              case NOTIFICATION_KINDS_SIDE.info:
-              case NOTIFICATION_KINDS_SIDE.success:
-                return (
-                  <GenericNotification
-                    key={notification.id}
-                    notification={notification}
-                    dismiss={() => {
-                      dispatch(removeNotification(notification.id));
-                    }}
-                  />
-                );
-              default:
-                return null;
-            }
-          })}
-        </div>
-      );
-    }
+    case NOTIFICATION_DOMAINS.GLOBAL:
+      return <NotificationsListGlobal {...props} />;
+    case NOTIFICATION_DOMAINS.PAGE:
+      return <NotificationsListPage {...props} />;
+    case NOTIFICATION_DOMAINS.SIDE:
+      return <NotificationsListSide {...props} />;
     default:
       return null;
   }
