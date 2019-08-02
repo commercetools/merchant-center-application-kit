ec9eaaddc5476e74c66bcfe4fbda8b7b56f699c8
Nicola Molinari <nicola.molinari@commercetools.de>
2019-08-02 22:19:30 +0200

    fix(jest): use isolatedModules when running ts-jest

--- packages/jest-preset-mc-app/jest-preset-for-typescript.js
+++ packages/jest-preset-mc-app/jest-preset-for-typescript.js
@@ -8,4 +8,15 @@ module.exports = {
     ...defaultPreset.transform,
     '^.+\\.tsx?$': 'ts-jest',
   },
+  // Without this option, somehow CI fails to run the tests with the following error:
+  //   TypeError: Unable to require `.d.ts` file.
+  //   This is usually the result of a faulty configuration or import. Make sure there is a `.js`, `.json` or another executable extension available alongside `core.ts`.
+  // Fix is based on this comment:
+  // - https://github.com/kulshekhar/ts-jest/issues/805#issuecomment-456055213
+  // - https://github.com/kulshekhar/ts-jest/blob/master/docs/user/config/isolatedModules.md
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
 };
