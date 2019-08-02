16bef8a3ea43f5083ab06ebf1a40fc4908929295
Nicola Molinari <nicola.molinari@commercetools.de>
2019-08-02 18:51:32 +0200

    chore(types/ui-kit): use IconProps type

--- packages/react-notifications/src/components/notification/notification.tsx
+++ packages/react-notifications/src/components/notification/notification.tsx
@@ -7,6 +7,7 @@ import {
   InfoIcon,
   CheckBoldIcon,
   IconButton,
+  IconProps,
 } from '@commercetools-frontend/ui-kit';
 import {
   NOTIFICATION_DOMAINS,
@@ -24,16 +25,7 @@ import {
 
 type PropsIcon = {
   type: TAppNotificationKind;
-  // TODO: get list of supported colors from uikit
-  color:
-    | 'error'
-    | 'warning'
-    | 'info'
-    | 'solid'
-    | 'neutral60'
-    | 'surface'
-    | 'primary'
-    | 'primary40';
+  color: IconProps['color'];
 };
 
 const NotificationIcon = (props: PropsIcon) => {
