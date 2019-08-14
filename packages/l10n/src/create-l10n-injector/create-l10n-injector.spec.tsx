0cbba26179b8d816ab938e7c651a8f664dc1a037
tdeekens <tobias.deekens@commercetools.de>
2019-08-14 11:18:56 +0200

    refactor: to remove path for act

--- packages/l10n/src/create-l10n-injector/create-l10n-injector.spec.tsx
+++ packages/l10n/src/create-l10n-injector/create-l10n-injector.spec.tsx
@@ -30,55 +30,40 @@ const errorLoadingLocalesMock = jest.fn(() =>
   Promise.reject(new Error('Oops'))
 );
 
-// https://github.com/testing-library/react-hooks-testing-library/blob/ebf8460716ac59618cffdc16094c534a3ca59ffe/docs/usage/advanced-hooks.md#act-warning
-const patchActWarningUntilReactSixteenPointNine = async (
-  run: () => Promise<void>
-) => {
-  const originalError = console.error;
-  console.error = jest.fn();
-  try {
-    await run();
-  } finally {
-    console.error = originalError;
-  }
-};
-
 describe('loading data', () => {
-  it('should load data via hook', async () =>
-    patchActWarningUntilReactSixteenPointNine(async () => {
-      const useL10n = createL10NHook<Candies>(loadLocalesMock);
-      const { result, waitForNextUpdate } = renderHook<unknown, Result>(() =>
-        useL10n('en')
-      );
-      expect(result.current.isLoading).toBe(true);
-      expect(result.current.data).toEqual({});
-      expect(result.current.error).not.toBeDefined();
+  it('should load data via hook', async () => {
+    const useL10n = createL10NHook<Candies>(loadLocalesMock);
+    const { result, waitForNextUpdate } = renderHook<unknown, Result>(() =>
+      useL10n('en')
+    );
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.data).toEqual({});
+    expect(result.current.error).not.toBeDefined();
 
-      await waitForNextUpdate();
+    await waitForNextUpdate();
 
-      expect(result.current.isLoading).toBe(false);
-      expect(result.current.data).toEqual(candies.en);
-      expect(result.current.error).not.toBeDefined();
-    }));
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data).toEqual(candies.en);
+    expect(result.current.error).not.toBeDefined();
+  });
 });
 describe('error loading data', () => {
-  it('should return error and report error to sentry', async () =>
-    patchActWarningUntilReactSixteenPointNine(async () => {
-      const useL10n = createL10NHook<Candies>(errorLoadingLocalesMock);
-      const { result, waitForNextUpdate } = renderHook<unknown, Result>(() =>
-        useL10n('en')
-      );
-      expect(result.current.isLoading).toBe(true);
-      expect(result.current.data).toEqual({});
-      expect(result.current.error).not.toBeDefined();
+  it('should return error and report error to sentry', async () => {
+    const useL10n = createL10NHook<Candies>(errorLoadingLocalesMock);
+    const { result, waitForNextUpdate } = renderHook<unknown, Result>(() =>
+      useL10n('en')
+    );
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.data).toEqual({});
+    expect(result.current.error).not.toBeDefined();
 
-      await waitForNextUpdate();
+    await waitForNextUpdate();
 
-      expect(result.current.isLoading).toBe(false);
-      expect(result.current.data).toEqual({});
-      expect(result.current.error).toEqual(
-        expect.objectContaining({ message: 'Oops' })
-      );
-      expect(reportErrorToSentry).toHaveBeenCalledWith(new Error('Oops'));
-    }));
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data).toEqual({});
+    expect(result.current.error).toEqual(
+      expect.objectContaining({ message: 'Oops' })
+    );
+    expect(reportErrorToSentry).toHaveBeenCalledWith(new Error('Oops'));
+  });
 });
