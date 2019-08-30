bc42e6de2a0df853c7d19c54912adb8d5edb7022
tdeekens <tobias.deekens@commercetools.de>
2019-08-30 11:20:37 +0200

    fix(mc-http-server): to add referrer policy

--- packages/mc-http-server/server.js
+++ packages/mc-http-server/server.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 const morgan = require('morgan');
+const hidePoweredBy = require('hide-powered-by');
+const referrerPolicy = require('referrer-policy');
 const { createLightship } = require('lightship');
 const {
   createMiddleware: createPrometheusMetricsMiddleware,
@@ -78,8 +80,10 @@ const startServer = options => {
   const serverIndexMiddleware = createServerIndexMiddleware(options);
 
   // Configure and start the HTTP server.
-  const app = express()
-    .disable('x-powered-by')
+  const app = express();
+  app.use(hidePoweredBy());
+  app
+    .use(referrerPolicy({ policy: 'same-origin' }))
     .get('/version', (request, response) => {
       response.setHeader('Content-Type', 'application/json');
       response.end(
