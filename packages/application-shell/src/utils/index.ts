8f1974360167b7c99b2eeaa848fe003cd35ba0ba
Nicola Molinari <nicola.molinari@commercetools.de>
2019-09-20 12:04:09 +0200

    refactor(app-shell): start converting package to TS  (#1068)
    * refactor(app-shell): start converting package to TS
    
    * test: fix assertions
    
    * refactor(app-shell): convert apollo client setup
    
    * fix(app-shell): to always assign node variable within while loop

--- packages/application-shell/src/utils/index.ts
+++ packages/application-shell/src/utils/index.ts
@@ -1,6 +1,12 @@
-export { default as selectProjectKeyFromLocalStorage } from './select-project-key-from-local-storage';
-export { default as selectTeamIdFromLocalStorage } from './select-team-id-from-local-storage';
-export { default as selectProjectKeyFromUrl } from './select-project-key-from-url';
+export {
+  default as selectProjectKeyFromLocalStorage,
+} from './select-project-key-from-local-storage';
+export {
+  default as selectTeamIdFromLocalStorage,
+} from './select-team-id-from-local-storage';
+export {
+  default as selectProjectKeyFromUrl,
+} from './select-project-key-from-url';
 export { default as selectUserId } from './select-user-id';
 export { default as getCorrelationId } from './get-correlation-id';
 export { default as getPreviousProjectKey } from './get-previous-project-key';
