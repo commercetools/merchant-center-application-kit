b5b7a89bb32ebbb32341707c9ae8ba1c8c8d27af
Amin Benselim <amin.benselim@commercetools.de>
2019-10-07 11:21:18 +0200

    Update test-utils.spec.js

--- packages/application-shell/src/test-utils/test-utils.spec.js
+++ packages/application-shell/src/test-utils/test-utils.spec.js
@@ -143,7 +143,7 @@ describe('ApplicationContext', () => {
           name: 'Test with big data',
           countries: ['de', 'en'],
           currencies: ['EUR', 'GBP'],
-          languages: ['de', 'en-GB'],
+          languages: ['de', 'en-GB', 'en'],
           owner: {
             id: 'project-id-1',
           },
