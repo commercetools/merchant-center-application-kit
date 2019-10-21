558db09e54f5ed56472f4305fe17f144ad9df4b5
Nicola Molinari <nicola.molinari@commercetools.de>
2019-10-21 09:56:16 +0200

    feat(app-connectors): expose mock functions for image-regex settings as test-utils

---
+++ packages/application-shell-connectors/src/test-utils/test-utils.spec.ts
@@ -0,0 +1,50 @@
+import { GRAPHQL_TARGETS } from '@commercetools-frontend/constants';
+import {
+  createFetchProjectExtensionImageRegexMock,
+  createGraphqlResponseForProjectExtensionImageRegexQuery,
+} from './test-utils';
+
+describe('createFetchProjectExtensionImageRegexMock', () => {
+  it('should create mock', () => {
+    const mock = createFetchProjectExtensionImageRegexMock({
+      result: {
+        data: createGraphqlResponseForProjectExtensionImageRegexQuery({
+          projectExtension: {
+            __typename: 'ProjectExtension',
+            id: 'project-extension-id',
+            imageRegex: {
+              __typename: 'ImageRegex',
+              small: {
+                __typename: 'ImageRegexOptions',
+                flag: 'gi',
+                replace: '-small.jpg',
+                search: '.[^.]+$',
+              },
+              thumb: undefined,
+            },
+          },
+        }),
+      },
+    });
+    expect(mock).toEqual(
+      expect.objectContaining({
+        request: expect.objectContaining({
+          variables: expect.objectContaining({
+            target: GRAPHQL_TARGETS.SETTINGS_SERVICE,
+          }),
+        }),
+        result: {
+          data: expect.objectContaining({
+            projectExtension: expect.objectContaining({
+              imageRegex: expect.objectContaining({
+                small: expect.objectContaining({
+                  replace: '-small.jpg',
+                }),
+              }),
+            }),
+          }),
+        },
+      })
+    );
+  });
+});
