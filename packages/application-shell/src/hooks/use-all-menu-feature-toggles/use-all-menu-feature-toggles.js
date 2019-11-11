b03d375a787080adff3df46c0178cfcb55c1ce53
tdeekens <tobias.deekens@commercetools.de>
2019-11-11 11:14:57 +0100

    feat(app-shell): add spec

--- packages/application-shell/src/hooks/use-all-menu-feature-toggles/use-all-menu-feature-toggles.js
+++ packages/application-shell/src/hooks/use-all-menu-feature-toggles/use-all-menu-feature-toggles.js
@@ -1,5 +1,6 @@
 import { useQuery } from 'react-apollo';
 import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
+import { reportErrorToSentry } from '@commercetools-frontend/sentry';
 import { FetchAllMenuFeatureToggles } from './fetch-all-menu-feature-toggles.proxy.graphql';
 
 const useAllMenuFeatureToggles = () => {
@@ -13,6 +14,7 @@ const useAllMenuFeatureToggles = () => {
   const { data, refetch, loading } = useQuery(FetchAllMenuFeatureToggles, {
     fetchPolicy: 'cache-and-network',
     skip: !servedByProxy,
+    onError: reportErrorToSentry,
     context: {
       uri: `${mcProxyApiUrl}/api/graphql`,
     },
---
+++ packages/application-shell/src/hooks/use-all-menu-feature-toggles/use-all-menu-feature-toggles.spec.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { reportErrorToSentry } from '@commercetools-frontend/sentry';
+import { renderApp, waitForElement, wait } from '../../test-utils';
+import FetchAllMenuFeatureToggles from './fetch-all-menu-feature-toggles.proxy.graphql';
+import useAllMenuFeatureToggles from './use-all-menu-feature-toggles';
+
+jest.mock('@commercetools-frontend/sentry');
+
+const TestComponent = () => {
+  const { isLoading, allFeatureToggles } = useAllMenuFeatureToggles();
+
+  if (!isLoading) {
+    return (
+      <>
+        <h3>Number of toggles: {allFeatureToggles.length}</h3>
+        <ul>
+          {allFeatureToggles.map(featureToggle => (
+            <div key={featureToggle}>{`Toggle: ${featureToggle}`}</div>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  return <div>{'Loading'}</div>;
+};
+
+const mockRequests = {
+  withoutError: {
+    request: {
+      query: FetchAllMenuFeatureToggles,
+    },
+    result: {
+      data: {
+        allFeatureToggles: ['flagA', 'flagB'],
+      },
+    },
+  },
+  withError: {
+    request: {
+      query: FetchAllMenuFeatureToggles,
+    },
+    result: {
+      errors: [
+        {
+          message: 'There has been an error.',
+        },
+      ],
+    },
+  },
+};
+
+const render = ({ mocks, environment } = { mocks: [] }) =>
+  renderApp(<TestComponent />, {
+    mocks,
+    environment,
+  });
+
+describe('when served by proxy', () => {
+  describe('without error', () => {
+    it('should be loading and return any feature toggles', async () => {
+      const rendered = render({
+        environment: {
+          servedByProxy: true,
+        },
+        mocks: [mockRequests.withoutError],
+      });
+
+      await waitForElement(() => rendered.getByText('Loading'));
+      await waitForElement(() => [
+        rendered.getByText(/Number of toggles: 2/i),
+        rendered.getByText(/Toggle: flagA/i),
+        rendered.getByText(/Toggle: flagB/i),
+      ]);
+    });
+  });
+
+  describe('with error', () => {
+    it('should be loading and return no feature toggles and report to sentry', async () => {
+      const rendered = render({
+        environment: {
+          servedByProxy: true,
+        },
+        mocks: [mockRequests.withError],
+      });
+
+      await waitForElement(() => rendered.getByText('Loading'));
+      await waitForElement(() => rendered.getByText(/Number of toggles: 0/i));
+
+      await wait(() => {
+        expect(reportErrorToSentry).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('when not served proxy', () => {
+  it('should not be loading and not return any feature toggles', async () => {
+    const rendered = render();
+
+    expect(rendered.queryByText('Loading')).not.toBeInTheDocument();
+
+    await waitForElement(() => rendered.getByText(/Number of toggles: 0/i));
+  });
+});
