46fc8881ad76d01ff6eb9d5daffb9c45100850c5
Nicola Molinari <nicola.molinari@commercetools.de>
2019-12-30 10:37:06 +0100

    fix(app-shell): retry unauthenticated requests for graphql errors

--- packages/application-shell/src/apollo-links/error-link.js
+++ packages/application-shell/src/apollo-links/error-link.js
@@ -7,10 +7,31 @@ import history from '@commercetools-frontend/browser-history';
 
 // Checks response from GraphQL in order to scan 401 errors and redirect the
 // user to the login page resetting the store and showing the proper message
-const errorLink = onError(({ networkError }) => {
-  if (networkError && networkError.statusCode === STATUS_CODES.UNAUTHORIZED) {
-    history.push(`/logout?reason=${LOGOUT_REASONS.UNAUTHORIZED}`);
+const errorLink = onError(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (networkError && networkError.statusCode === STATUS_CODES.UNAUTHORIZED) {
+      history.push(`/logout?reason=${LOGOUT_REASONS.UNAUTHORIZED}`);
+    }
+    // In case of graphql errors, we want to retry unauthenticated requests by
+    // forcing our API to fetch a new token, using the `X-Force-Token` header.
+    // https://www.apollographql.com/docs/link/links/error/#retrying-failed-requests
+    // We need to do this as the `token-retry-link` only works for network errors.
+    // https://www.apollographql.com/docs/link/links/retry/
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        if (err.extensions.code === 'UNAUTHENTICATED') {
+          operation.setContext(({ headers }) => ({
+            headers: {
+              ...headers,
+              'X-Force-Token': true,
+            },
+          }));
+          // retry the request, returning the new observable
+          return forward(operation);
+        }
+      }
+    }
   }
-});
+);
 
 export default errorLink;
