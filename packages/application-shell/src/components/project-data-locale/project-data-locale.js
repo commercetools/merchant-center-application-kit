30f72fd27be4cc6655a71063f8d33624a2ef501a
Nicola Molinari <nicola.molinari@commercetools.de>
2020-01-02 15:34:21 +0100

    refactor(app-shell): rewrite tests for ProjectDataLocale, use functional component

--- packages/application-shell/src/components/project-data-locale/project-data-locale.js
+++ packages/application-shell/src/components/project-data-locale/project-data-locale.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { deepEqual } from 'fast-equals';
 import { STORAGE_KEYS } from '../../constants';
 
-const defaultLocale = 'en';
-
 const getSelectedDataLocaleForProject = projectLocales => {
-  // In case the project is not present (e.g. 404), we just return `en`
-  // as a fallback value but it doesn't really matter at end what the
-  // value is. When the user access a proper project, this value will
-  // be correctly picked.
-  if (!projectLocales) return defaultLocale;
   const cachedDataLocale = window.localStorage.getItem(
     STORAGE_KEYS.SELECTED_DATA_LOCALE
   );
   // Make sure the cached locale is listed in the selected project
   const isCachedDataLocaleIncludedInProjectLanguages = projectLocales.includes(
-    cachedDataLocale
+    cachedDataLocale || ''
   );
-  if (isCachedDataLocaleIncludedInProjectLanguages) return cachedDataLocale;
+  if (cachedDataLocale && isCachedDataLocaleIncludedInProjectLanguages)
+    return cachedDataLocale;
   // Pick the first locale from the list
   const defaultDataLocaleForProject = projectLocales[0];
   // Cache it
@@ -29,37 +22,28 @@ const getSelectedDataLocaleForProject = projectLocales => {
   return defaultDataLocaleForProject;
 };
 
-export default class ProjectDataLocale extends React.PureComponent {
-  static displayName = 'ProjectDataLocale';
-
-  static propTypes = {
-    locales: PropTypes.arrayOf(PropTypes.string.isRequired),
-    children: PropTypes.func.isRequired,
-  };
-
-  state = {
-    locale: getSelectedDataLocaleForProject(this.props.locales),
-  };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (deepEqual(prevState.locales, nextProps.locales)) return null;
-
-    return {
-      locales: nextProps.locales,
-      locale: getSelectedDataLocaleForProject(nextProps.locales),
-    };
-  }
-
-  handleSetProjectDataLocale = locale => {
-    this.setState({ locale });
+const ProjectDataLocale = props => {
+  const [locale, setLocale] = React.useState(
+    getSelectedDataLocaleForProject(props.locales)
+  );
+  const handleSetProjectDataLocale = React.useCallback(locale => {
+    setLocale(locale);
     // Cache it
     window.localStorage.setItem(STORAGE_KEYS.SELECTED_DATA_LOCALE, locale);
-  };
+  }, []);
+  return (
+    <>
+      {props.children({
+        locale,
+        setProjectDataLocale: handleSetProjectDataLocale,
+      })}
+    </>
+  );
+};
+ProjectDataLocale.displayName = 'ProjectDataLocale';
+ProjectDataLocale.propTypes = {
+  locales: PropTypes.arrayOf(PropTypes.string).isRequired,
+  children: PropTypes.func.isRequired,
+};
 
-  render() {
-    return this.props.children({
-      locale: this.state.locale,
-      setProjectDataLocale: this.handleSetProjectDataLocale,
-    });
-  }
-}
+export default ProjectDataLocale;
--- packages/application-shell/src/components/project-data-locale/project-data-locale.spec.js
+++ packages/application-shell/src/components/project-data-locale/project-data-locale.spec.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, waitForElement } from '@testing-library/react';
 import { STORAGE_KEYS } from '../../constants';
 import ProjectDataLocale from './project-data-locale';
-
-const createTestProps = props => ({
-  locales: null,
-  children: jest.fn(() => <div />),
-  ...props,
-});
+import { wait } from '@apollo/react-testing';
 
 beforeEach(() => {
   window.localStorage.setItem.mockClear();
@@ -16,108 +11,51 @@ beforeEach(() => {
 });
 
 describe('rendering', () => {
-  let props;
-  let wrapper;
-
-  describe('when list of locales is not defined', () => {
-    beforeEach(() => {
-      props = createTestProps();
-      wrapper = shallow(<ProjectDataLocale {...props} />);
-    });
-    it('should call render function with default locale "en"', () => {
-      expect(props.children).toHaveBeenCalledWith(
-        expect.objectContaining({ locale: 'en' })
-      );
-    });
-    it('should call render function with setter function', () => {
-      expect(props.children).toHaveBeenCalledWith(
-        expect.objectContaining({
-          setProjectDataLocale: wrapper.instance().handleSetProjectDataLocale,
-        })
-      );
-    });
-  });
-});
-
-describe('lifecycle', () => {
-  describe('getDerivedStateFromProps', () => {
-    let derivedState;
-    let nextProps;
-    describe('when list of locales is defined', () => {
-      describe('when locale is not cached yet', () => {
-        beforeEach(() => {
-          nextProps = { locales: ['it', 'de'] };
-          derivedState = ProjectDataLocale.getDerivedStateFromProps(
-            nextProps,
-            {}
-          );
-        });
-        it('should render derive state with locale "it"', () => {
-          expect(derivedState).toEqual(
-            expect.objectContaining({ locale: 'it' })
-          );
-        });
-        it('should render derive state with locales', () => {
-          expect(derivedState).toEqual(
-            expect.objectContaining({ locales: nextProps.locales })
-          );
-        });
+  describe('when selected locale is cached', () => {
+    describe('when cached locale is a known project locale', () => {
+      it('should render with cached value', async () => {
+        window.localStorage.getItem.mockReturnValue('de');
+        const rendered = render(
+          <ProjectDataLocale locales={['en', 'de']}>
+            {({ locale }) => <span>{`Locale: ${locale}`}</span>}
+          </ProjectDataLocale>
+        );
+        await waitForElement(() => rendered.getByText('Locale: de'));
       });
-      describe('when locale has been already cached', () => {
-        beforeEach(() => {
-          window.localStorage.getItem.mockReturnValue('de');
-          nextProps = { locales: ['it', 'de'] };
-          derivedState = ProjectDataLocale.getDerivedStateFromProps(
-            nextProps,
-            {}
-          );
-        });
-        it('should render derive state with locale "de"', () => {
-          expect(derivedState).toEqual(
-            expect.objectContaining({ locale: 'de' })
-          );
-        });
-        it('should render derive state with locales', () => {
-          expect(derivedState).toEqual(
-            expect.objectContaining({ locales: nextProps.locales })
-          );
-        });
-      });
-      describe('when cached locale is not listed in the project locales', () => {
-        beforeEach(() => {
-          window.localStorage.setItem(STORAGE_KEYS.SELECTED_DATA_LOCALE, 'de');
-          nextProps = { locales: ['it', 'de'] };
-          derivedState = ProjectDataLocale.getDerivedStateFromProps(
-            nextProps,
-            {}
-          );
-        });
-        it('should render derive state with locale "de"', () => {
-          expect(derivedState).toEqual(
-            expect.objectContaining({ locale: 'de' })
-          );
-        });
-        it('should render derive state with locales', () => {
-          expect(derivedState).toEqual(
-            expect.objectContaining({ locales: nextProps.locales })
+    });
+    describe('when cached locale is not in the list of project locales', () => {
+      it('should render with first locale from the project list and cache it', async () => {
+        window.localStorage.getItem.mockReturnValue('it');
+        const rendered = render(
+          <ProjectDataLocale locales={['en', 'de']}>
+            {({ locale }) => <span>{`Locale: ${locale}`}</span>}
+          </ProjectDataLocale>
+        );
+        await waitForElement(() => rendered.getByText('Locale: en'));
+        await wait(() => {
+          expect(window.localStorage.setItem).toHaveBeenCalledWith(
+            STORAGE_KEYS.SELECTED_DATA_LOCALE,
+            'en'
           );
         });
       });
     });
   });
-});
-
-describe('interactions', () => {
-  let props;
-  let wrapper;
-  describe('updating the project data locale', () => {
-    beforeEach(() => {
-      props = createTestProps();
-      wrapper = shallow(<ProjectDataLocale {...props} />);
-      wrapper.instance().handleSetProjectDataLocale('de');
-    });
-    it('should update the state with the new locale', () => {
-      expect(wrapper).toHaveState('locale', 'de');
+  describe('when selected locale is not cached', () => {
+    it('should render with first locale from the project list and cache it', async () => {
+      window.localStorage.getItem.mockReturnValue(undefined);
+      const rendered = render(
+        <ProjectDataLocale locales={['en', 'de']}>
+          {({ locale }) => <span>{`Locale: ${locale}`}</span>}
+        </ProjectDataLocale>
+      );
+      await waitForElement(() => rendered.getByText('Locale: en'));
+      await wait(() => {
+        expect(window.localStorage.setItem).toHaveBeenCalledWith(
+          STORAGE_KEYS.SELECTED_DATA_LOCALE,
+          'en'
+        );
+      });
     });
   });
 });
