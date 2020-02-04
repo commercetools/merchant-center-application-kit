a76218d29e444638e43c51bf83ea0375415d9ae3
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-04 16:17:16 +0100

    chore: remove unnecessary react-router dependency

--- packages/application-shell/src/components/project-suspended/project-suspended.spec.js
+++ packages/application-shell/src/components/project-suspended/project-suspended.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route } from 'react-router-dom';
 import { GRAPHQL_TARGETS } from '@commercetools-frontend/constants';
 import { renderApp, wait } from '../../test-utils';
 import UserQuery from '../fetch-user/fetch-user.mc.graphql';
