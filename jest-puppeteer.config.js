e2b3892ed7f919d7590235dbb63476e443a8b0c1
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-05 14:08:04 +0100

    chore: run CI jobs with github actions (#1309)
    * chore: run CI jobs with github actions
    
    * chore(actions): try running puppeteer with no sandbox
    
    * fix(actions): add missing cypress env vars
    
    * fix(actions): cypress spec path
    
    * fix(playground): start command
    
    * fix(actions): build packages before publishing a release
    
    * refactor(action): include canary release step in main workflow
    
    * docs(actions): wording
    
    Co-Authored-By: Ahmed Mehri <ahmed.mehri@commercetools.de>
    
    * chore: remove circleci config [skip ci]
    
    Co-authored-by: Ahmed Mehri <ahmed.elmehri@gmail.com>

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  launch: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: 'google-chrome-unstable',
+  },
   server: {
     command: 'yarn visual-testing-app:serve',
     port: 3001,
