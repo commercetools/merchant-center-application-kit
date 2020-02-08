a3f7638df6f8009165a8fd3b9f0ebc6b9fd8987f
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-08 17:18:58 +0100

    fix(actions): check out with depth 50 to allow proper canary versions on master branch

--- scripts/release_canary.sh
+++ scripts/release_canary.sh
@@ -9,12 +9,6 @@ COMMIT_MESSAGE=$(git log --format=oneline -n 1 $GITHUB_SHA)
 # Only trigger the canary release when:
 # - the commit message does not contain `[skip publish]`
 if [[ ! "$COMMIT_MESSAGE" =~ \[skip\ publish\] ]]; then
-  echo "pwd"
-  echo $PWD
-
-  echo "ls"
-  ls -lah
-
   echo "Configuring npm for automation bot"
 
   cat > .npmrc << EOF
@@ -22,16 +16,8 @@ email=npmjs@commercetools.com
 //registry.npmjs.org/:_authToken=$NPM_TOKEN
 EOF
 
-  echo "Working tree status"
-  git status
-
-  # Release package only if lerna detected some changes in packages
-  if yarn run lerna changed; then
-    echo "Releasing canary version"
-    yarn release:canary
-  else
-    echo "Skipping release, no changes in packages..."
-  fi
+  echo "Releasing canary version"
+  yarn release:canary
 
 else
   echo "Skipping release due to commit message..."
