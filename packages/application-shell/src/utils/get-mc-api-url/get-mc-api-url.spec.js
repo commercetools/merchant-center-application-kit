c349fc4c9c832f93fddff8f92e987be03418fe0f
tdeekens <tobias.deekens@commercetools.de>
2020-02-10 13:19:34 +0100

    refactor(app-shell): to call back to window origin

--- packages/application-shell/src/utils/get-mc-api-url/get-mc-api-url.spec.js
+++ packages/application-shell/src/utils/get-mc-api-url/get-mc-api-url.spec.js
@@ -1,15 +1,46 @@
 import getMcApiUrl from './get-mc-api-url';
 
 describe('getMcApiUrl', () => {
-  describe('when `mcApiurl` is defined on application environment', () => {
-    it('should return the configured `mcApiUrl`', () => {
-      const applicationEnvironment = {
-        mcApiUrl: 'mc.commercetools.co',
-      };
+  describe('when application is configured not to run behind proxy', () => {
+    describe('when `mcApiurl` is defined on application environment', () => {
+      it('should return the configured `mcApiUrl`', () => {
+        const applicationEnvironment = {
+          mcApiUrl: 'mc.commercetools.co',
+        };
 
-      expect(getMcApiUrl(applicationEnvironment)).toEqual(
-        applicationEnvironment.mcApiUrl
-      );
+        expect(getMcApiUrl(applicationEnvironment)).toEqual(
+          applicationEnvironment.mcApiUrl
+        );
+      });
+    });
+  });
+
+  describe('when application is configured to run behind proxy', () => {
+    beforeEach(() => {
+      global.window.origin = 'mc-api.europe-west1.gcp.commercetools.com';
+    });
+
+    describe('when `mcApiurl` is defined on application environment', () => {
+      it('should not return the configured `mcApiUrl` but the origin of the window', () => {
+        const applicationEnvironment = {
+          mcApiUrl: 'mc.commercetools.co',
+          servedByProxy: true,
+        };
+
+        expect(getMcApiUrl(applicationEnvironment)).toEqual(
+          applicationEnvironment.mcApiUrl
+        );
+      });
+    });
+
+    describe('when `mcApiurl` is not defined on application environment', () => {
+      it('should not return the origin of the window', () => {
+        const applicationEnvironment = {
+          servedByProxy: true,
+        };
+
+        expect(getMcApiUrl(applicationEnvironment)).toEqual(window.origin);
+      });
     });
   });
 });
--- packages/application-shell/src/utils/get-mc-api-url/get-mc-api-url.ts
+++ packages/application-shell/src/utils/get-mc-api-url/get-mc-api-url.ts
@@ -3,5 +3,10 @@ import { ApplicationWindow } from '@commercetools-frontend/constants';
 declare let window: ApplicationWindow;
 
 export default function getMcApiUrl(appEnv = window.app) {
+  const isServedByProxy = appEnv.servedByProxy;
+  const mcApiUrl = appEnv.mcApiUrl;
+
+  if (isServedByProxy && !mcApiUrl) return window.origin;
+
   return appEnv.mcApiUrl;
 }
