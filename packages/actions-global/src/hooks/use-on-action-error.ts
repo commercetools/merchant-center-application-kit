ef2386074771a03ace5c6b014661d6f924b8b59e
Fedor Kulikov <fedor.kulikov@commercetools.de>
2020-02-14 13:27:58 +0100

    fix(actions-global): put DispatchActionError type where it was

--- packages/actions-global/src/hooks/use-on-action-error.ts
+++ packages/actions-global/src/hooks/use-on-action-error.ts
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import handleActionError, { ActionError } from '../actions/handle-action-error';
+import handleActionError, {
+  ActionError,
+  DispatchActionError,
+} from '../actions/handle-action-error';
 
 export default function useOnActionError() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<DispatchActionError>();
+
   return React.useCallback(
     (error: ActionError) => dispatch(handleActionError(error)),
     [dispatch]
