37fd50fd1c979615de19950b5e1a9e9f661f1946
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-17 11:45:44 +0100

    test(back-to-project): do not use beforeEach

---
+++ packages/application-shell/src/components/back-to-project/back-to-project.spec.tsx
@@ -0,0 +1,29 @@
+import { mocked } from 'ts-jest/utils';
+import React from 'react';
+import { renderApp, fireEvent, wait } from '../../test-utils';
+import BackToProject from './back-to-project';
+
+beforeEach(() => {
+  window.location.replace = jest.fn();
+});
+
+describe('with `projectKey`', () => {
+  it('should redirect to the project with the key', async () => {
+    const rendered = renderApp(<BackToProject projectKey="test-project-key" />);
+    fireEvent.click(rendered.getByText('Back to project'));
+    await wait(() => {
+      expect(mocked(window.location.replace)).toHaveBeenCalledWith(
+        '/test-project-key'
+      );
+    });
+  });
+});
+describe('without `projectKey`', () => {
+  it('should redirect to the root', async () => {
+    const rendered = renderApp(<BackToProject />);
+    fireEvent.click(rendered.getByText('Back to project'));
+    await wait(() => {
+      expect(mocked(window.location.replace)).toHaveBeenCalledWith('/');
+    });
+  });
+});
