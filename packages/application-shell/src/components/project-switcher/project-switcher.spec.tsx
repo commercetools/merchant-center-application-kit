01a0787f02eb07be922f70c7548ad0a056081ba0
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-17 11:34:12 +0100

    test(project-switcher): do not use beforeEach

--- packages/application-shell/src/components/project-switcher/project-switcher.spec.tsx
+++ packages/application-shell/src/components/project-switcher/project-switcher.spec.tsx
@@ -1,32 +1,34 @@
+import { mocked } from 'ts-jest/utils';
 import React from 'react';
 import { renderApp, fireEvent, waitForElement, wait } from '../../test-utils';
 import { createGraphqlResponseForProjectsQuery } from './project-switcher-test-utils';
 import ProjectSwitcher from './project-switcher';
 
-const Wrapper = props => (
-  <>
-    <label id="project-switcher">{'Project switcher'}</label>
-    <ProjectSwitcher {...props} />
-  </>
-);
-
-describe('rendering', () => {
-  let mockedRequest;
-  let rendered;
-  beforeEach(() => {
-    mockedRequest = [
-      createGraphqlResponseForProjectsQuery({
-        getIsSuspended: key => key === 'key-2',
-        getIsExpired: key => key === 'key-3',
-      }),
-    ];
-    rendered = renderApp(<Wrapper projectKey="key-0" />, {
+const render = () => {
+  const mockedRequest = [
+    createGraphqlResponseForProjectsQuery({
+      getIsSuspended: key => key === 'key-2',
+      getIsExpired: key => key === 'key-3',
+    }),
+  ];
+  return renderApp(
+    <>
+      <label id="project-switcher">{'Project switcher'}</label>
+      <ProjectSwitcher projectKey="key-0" />
+    </>,
+    {
       addTypename: true,
       mocks: mockedRequest,
-    });
+    }
+  );
+};
+
+describe('rendering', () => {
+  beforeEach(() => {
     window.location.replace = jest.fn();
   });
   it('should search and select a project', async () => {
+    const rendered = render();
     await waitForElement(() => rendered.getByLabelText('Project switcher'));
     const input = rendered.getByLabelText('Project switcher');
     fireEvent.focus(input);
@@ -34,10 +36,11 @@ describe('rendering', () => {
     fireEvent.keyDown(input, { key: 'Enter', keyCode: 13, which: 13 });
 
     await wait(() => {
-      expect(window.location.replace).toHaveBeenCalledWith('/key-1');
+      expect(mocked(window.location.replace)).toHaveBeenCalledWith('/key-1');
     });
   });
   it('should see no results message when search does not match any project', async () => {
+    const rendered = render();
     await waitForElement(() => rendered.getByLabelText('Project switcher'));
     const input = rendered.getByLabelText('Project switcher');
     fireEvent.focus(input);
@@ -50,21 +53,27 @@ describe('rendering', () => {
     );
   });
   it('should prevent clicking on a suspended project', async () => {
+    const rendered = render();
     await waitForElement(() => rendered.getByLabelText('Project switcher'));
     const input = rendered.getByLabelText('Project switcher');
     fireEvent.focus(input);
     fireEvent.keyDown(input, { key: 'ArrowDown' });
     fireEvent.click(rendered.getByText(/Suspended/i));
 
-    await wait(() => expect(window.location.replace).not.toHaveBeenCalled());
+    await wait(() =>
+      expect(mocked(window.location.replace)).not.toHaveBeenCalled()
+    );
   });
   it('should prevent clicking on an expired project', async () => {
+    const rendered = render();
     await waitForElement(() => rendered.getByLabelText('Project switcher'));
     const input = rendered.getByLabelText('Project switcher');
     fireEvent.focus(input);
     fireEvent.keyDown(input, { key: 'ArrowDown' });
     fireEvent.click(rendered.getByText(/Expired/i));
 
-    await wait(() => expect(window.location.replace).not.toHaveBeenCalled());
+    await wait(() =>
+      expect(mocked(window.location.replace)).not.toHaveBeenCalled()
+    );
   });
 });
