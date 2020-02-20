bf1684344ae3eec9d8f9d8e28323b70723c1b60d
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-20 11:26:46 +0100

    fix(create-mc-app): yarn binary check to not throw

--- packages/create-mc-app/src/utils.js
+++ packages/create-mc-app/src/utils.js
@@ -3,8 +3,12 @@ const execa = require('execa');
 const isSemVer = version => /^(v?)([0-9].[0-9].[0-9])+/.test(version);
 
 const shouldUseYarn = () => {
-  const result = execa.sync('yarnpkg', ['--version'], { stdio: 'ignore' });
-  return !result.failed;
+  try {
+    const result = execa.sync('yarn', ['--version'], { stdio: 'ignore' });
+    return !result.failed;
+  } catch (error) {
+    return false;
+  }
 };
 
 const slugify = name => name.toLowerCase().replace(/_/gi, '-');
