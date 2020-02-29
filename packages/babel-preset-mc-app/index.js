e865eaf8f883ac7836739942303b21902342e159
tdeekens <tobias.deekens@commercetools.de>
2020-02-29 16:17:09 +0100

    fix(babel-preset-mc-app): to patch via isMacrosName

--- packages/babel-preset-mc-app/index.js
+++ packages/babel-preset-mc-app/index.js
@@ -1,4 +1,20 @@
 /* eslint-disable global-require */
+
+/**
+ * NOTE:
+ *   `@formatjs/macro` matches the default babel-macros RegExp.
+ *   This prevents updating `react-intl` here and e.g. in create-react-app.
+ *   As neither side `react-intl` nor `babel-plugin-macros` intends to either
+ *   change the package name or RegExp we extend the default RegExp
+ *   and pass it to the plugin as `isMacrosName`.
+ *
+ *   Ref: https://github.com/kentcdodds/babel-plugin-macros/issues/131#issuecomment-563191982
+ */
+const defaultMacrosRegex = /[./]macro(\.js)?$/;
+const isMacrosName = packageName =>
+  defaultMacrosRegex.test(packageName) &&
+  packageName.indexOf('@formatjs/macro') === -1;
+
 module.exports = function getBabePresetConfigForMcApp() {
   // This is similar to how `env` works in Babel:
   // https://babeljs.io/docs/usage/babelrc/#env-option
@@ -74,7 +90,12 @@ module.exports = function getBabePresetConfigForMcApp() {
     plugins: [
       // Experimental macros support. Will be documented after it's had some time
       // in the wild.
-      require('babel-plugin-macros'),
+      [
+        require('babel-plugin-macros'),
+        {
+          isMacrosName,
+        },
+      ],
       // export { default } from './foo'
       require('@babel/plugin-proposal-export-default-from'),
       // export * from './foo'
