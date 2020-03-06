810810c65229fe91671704cf50f254e98f368c31
tdeekens <tobias.deekens@commercetools.de>
2020-03-06 09:58:56 +0100

    feat(mc-html-tmeplate): add env variable placeholders

--- packages/mc-html-template/lib/load-env.js
+++ packages/mc-html-template/lib/load-env.js
@@ -1,15 +1,52 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 const fs = require('fs');
 
 // Keep a reference to the loaded config so that requiring the module
 // again will result in returning the cached value.
 let loadedEnv;
 
-module.exports = configPath => {
-  if (loadedEnv) return loadedEnv;
+const START_OF_ENV_PLACEHODER = 'env:';
+const isEnvVariablesPlaceholder = valueOfEnvConfig =>
+  valueOfEnvConfig && valueOfEnvConfig.startsWith(START_OF_ENV_PLACEHODER);
+const substitutePlaceholder = valueOfEnvConfig => {
+  const nameOfEnvVariable = valueOfEnvConfig.replace(
+    START_OF_ENV_PLACEHODER,
+    ''
+  );
+
+  const valueOfEnvVariable = process.env[nameOfEnvVariable];
+
+  if (!valueOfEnvVariable) {
+    throw new Error(
+      `Missing '${nameOfEnvVariable}' specified in config as 'env:${nameOfEnvVariable}'.`
+    );
+  }
+
+  return valueOfEnvVariable;
+};
+const substituteEnvVariablePlaceholders = config => {
+  const entriesOfEnvConfig = Object.entries(config);
+
+  const replacedEntriesOfEnvConfig = entriesOfEnvConfig.map(
+    ([envConfigKey, envConfigValue]) => [
+      envConfigKey,
+      isEnvVariablesPlaceholder(envConfigValue)
+        ? substitutePlaceholder(envConfigValue)
+        : envConfigValue,
+    ]
+  );
+
+  return Object.fromEntries(replacedEntriesOfEnvConfig);
+};
+
+module.exports = (configPath, { disableCache } = { disableCache: false }) => {
+  if (loadedEnv && !disableCache) return loadedEnv;
 
   // Attempt to read the JSON file provided by each application
   const rawConfig = fs.readFileSync(configPath, { encoding: 'utf8' });
   const config = JSON.parse(rawConfig);
+
+  const substitutedConfig = substituteEnvVariablePlaceholders(config);
 
   // List the required fields in order to validate them
   const requiredKeys = [
@@ -21,11 +58,12 @@ module.exports = configPath => {
     'cdnUrl',
   ];
   requiredKeys.forEach(key => {
-    const hasKey = Object.prototype.hasOwnProperty.call(config, key);
-    if (!hasKey)
+    const hasKey = Object.prototype.hasOwnProperty.call(substitutedConfig, key);
+    if (!hasKey) {
       throw new Error(
         `Missing '${key}' required configuration field. ${rawConfig}`
       );
+    }
   });
 
   // Default configuration containing optional fields
@@ -34,7 +72,9 @@ module.exports = configPath => {
   };
 
   // eslint-disable-next-line
-  const mergedConfig = Object.assign({}, defaultConfig, config);
+  const mergedConfig = Object.assign({}, defaultConfig, substitutedConfig);
+
   loadedEnv = mergedConfig;
+
   return loadedEnv;
 };
