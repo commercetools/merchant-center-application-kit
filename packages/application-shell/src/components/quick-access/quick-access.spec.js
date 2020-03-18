c03ab3e8c68aa4d84164ee8cd57fa294479ffe3b
Vidura Rajapaksa <vidura.rajapaksa@commercetools.de>
2020-03-18 14:30:56 +0100

    fix: tests

--- packages/application-shell/src/components/quick-access/quick-access.spec.js
+++ packages/application-shell/src/components/quick-access/quick-access.spec.js
@@ -679,7 +679,7 @@ describe('QuickAccess', () => {
     fireEvent.keyUp(searchInput, { key: 'Enter' });
     await wait(() => {
       expect(rendered.history.location.pathname).toBe(
-        '/test-with-big-data/products'
+        '/test-with-big-data/products/pim-search'
       );
       expect(rendered.queryByTestId('quick-access')).not.toBeInTheDocument();
     });
