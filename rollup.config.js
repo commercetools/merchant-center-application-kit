f555cbaecef269c848c0ba5a7bf8f218bcc9c41a
tdeekens <tobias.deekens@commercetools.de>
2020-04-28 13:56:04 +0200

    fix: import paths to plugins

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@
 process.env.BUILD_ROLLUP = true;
 
 const fs = require('fs');
-const babel = require('@rollup/plugin-babel');
+const { babel } = require('@rollup/plugin-babel');
 const readPkgUp = require('read-pkg-up');
 const getBabelPreset = require('@commercetools-frontend/babel-preset-mc-app');
 const {
   browserslist,
 } = require('@commercetools-frontend/mc-scripts/package.json');
-const resolve = require('rollup-plugin-node-resolve');
+const resolve = require('@rollup/plugin-node-resolve');
 const json = require('@rollup/plugin-json');
 const commonjs = require('@rollup/plugin-commonjs');
 const postcss = require('rollup-plugin-postcss');
