f5e6311755e828bc6df25943848306a43706ce95
Nicola Molinari <nicola.molinari@commercetools.de>
2020-05-05 09:22:14 +0200

    fix(loggers): silent option

--- packages-backend/loggers/src/middlewares/create-access-logger.ts
+++ packages-backend/loggers/src/middlewares/create-access-logger.ts
@@ -4,7 +4,6 @@ import winston from 'winston';
 import expressWinston from 'express-winston';
 
 const createAccessLoggerMiddleware = (options: TAccessLoggerOptions = {}) => {
-  const shouldLog = Boolean(options.silent);
   const ignoreUrls = options.ignoreUrls ?? [];
   const formatters = winston.format.combine(
     winston.format.timestamp(),
@@ -19,7 +18,8 @@ const createAccessLoggerMiddleware = (options: TAccessLoggerOptions = {}) => {
     meta: true,
     expressFormat: true, // Use default morgan access log formatting
     colorize: !options.json,
-    skip: (req) => !shouldLog || ignoreUrls.includes(req.originalUrl),
+    skip: (req) =>
+      Boolean(options.silent) || ignoreUrls.includes(req.originalUrl),
     dynamicMeta: (req) => ({
       ip: req.ip,
       ips: req.ips,
