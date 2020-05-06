081ab8d03b112fa3f112fc9205c0ab38f148be1e
Nicola Molinari <nicola.molinari@commercetools.de>
2020-05-06 12:03:42 +0200

    refactor(loggers): refine types, add test (#1478)

---
+++ packages-backend/loggers/src/loggers.spec.ts
@@ -0,0 +1,51 @@
+import type { Request } from 'express';
+
+import rewriteFieldsFormatter from './formatters/rewrite-fields';
+import createApplicationLogger from './create-application-logger';
+
+describe('application logger', () => {
+  it('should log and rewrite fields', () => {
+    // @ts-ignore
+    console._stdout.write = jest.fn();
+    const logger = createApplicationLogger({
+      json: true,
+      formatters: [
+        rewriteFieldsFormatter<Request['headers']>({
+          fields: [
+            {
+              from: 'meta.req.headers',
+              to: 'meta.req.headersJsonString',
+              replaceValue: (value) => {
+                const assertHeaders: typeof value = {};
+                // This should not throw any error
+                assertHeaders.accept = 'application/json';
+                return JSON.stringify(value);
+              },
+            },
+          ],
+        }),
+      ],
+    });
+    logger.info('Test log', {
+      meta: { req: { headers: { Accept: 'application/json' } } },
+    });
+    // @ts-ignore
+    expect(console._stdout.write).toHaveBeenCalledWith(
+      expect.stringContaining(`\"level\":\"info\"`)
+    );
+    // @ts-ignore
+    expect(console._stdout.write).toHaveBeenCalledWith(
+      expect.stringContaining(`\"message\":\"Test log\"`)
+    );
+    // @ts-ignore
+    expect(console._stdout.write).toHaveBeenCalledWith(
+      expect.stringContaining(
+        `\"meta\":${JSON.stringify({
+          req: {
+            headersJsonString: JSON.stringify({ Accept: 'application/json' }),
+          },
+        })}`
+      )
+    );
+  });
+});
