7747ddbce21cfffd4bf1a18106f5cb8b57e8c1cb
tdeekens <tobias.deekens@commercetools.com>
2020-06-19 09:16:35 +0200

    fix(app-shell): title message determination

--- packages/application-shell/src/components/project-suspended/project-suspended.tsx
+++ packages/application-shell/src/components/project-suspended/project-suspended.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useIntl, FormattedMessage } from 'react-intl';
+import { useIntl, FormattedMessage, MessageDescriptor } from 'react-intl';
 import ProjectSuspendedSVG from '@commercetools-frontend/assets/images/doors-closed.svg';
 import { MaintenancePageLayout } from '@commercetools-frontend/application-components';
 import ServicePageProjectSwitcher from '../service-page-project-switcher';
@@ -9,22 +9,22 @@ type Props = {
   isTemporary?: boolean;
 };
 
+const getTitleMessage = (props: Props): MessageDescriptor => {
+  /* NOTE: Other suspension reasons are `Payment` and `Other` which are not handled yet */
+  if (props.isTemporary === true)
+    return messages.temporaryMaintenanceSuspensionMessage;
+
+  return messages.defaultSuspensionMessage;
+};
+
 const ProjectSuspended = (props: Props) => {
   const intl = useIntl();
+  const titleMessage = getTitleMessage(props);
 
   <MaintenancePageLayout
     imageSrc={ProjectSuspendedSVG}
-    title={(() => {
-      /* NOTE: Other suspension reasons are `Payment` and `Other` which are not handled yet */
-      if (props.isTemporary === true)
-        return (
-          <FormattedMessage
-            {...messages.temporaryMaintenanceSuspensionMessage}
-          />
-        );
-      return <FormattedMessage {...messages.defaultSuspensionMessage} />;
-    })()}
-    label={intl.formatMessage(messages.title)}
+    title={<FormattedMessage {...titleMessage} />}
+    label={intl.formatMessage(titleMessage)}
     paragraph1={<FormattedMessage {...messages.paragraph1} />}
     bodyContent={<ServicePageProjectSwitcher />}
   />;
