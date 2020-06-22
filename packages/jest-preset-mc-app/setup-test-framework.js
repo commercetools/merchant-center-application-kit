640051f9713815a27997a15849a557b5d5450155
Tobias Deekens <tobias.deekens@commercetools.com>
2020-06-22 09:52:29 +0200

    feat(jest-preset-mc-app): add config for rtl (#1586)

--- packages/jest-preset-mc-app/setup-test-framework.js
+++ packages/jest-preset-mc-app/setup-test-framework.js
@@ -1,14 +1,21 @@
 require('unfetch/polyfill');
 require('jest-enzyme');
 require('@testing-library/jest-dom/extend-expect');
+require('./polyfills/intl');
+
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 const ShallowWrapper = require('enzyme/ShallowWrapper');
+import { configure as configureRtl } from '@testing-library/react';
 const configureEnzymeExtensions = require('@commercetools/enzyme-extensions');
 const commerceToolsEnzymeMatchers = require('@commercetools/jest-enzyme-matchers');
-require('./polyfills/intl');
+const loadConfig = require('./load-config');
+
+const jestConfig = loadConfig();
 
 Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true });
+
+configureRtl(jestConfig.rtlConfig);
 
 expect.extend({
   toBeComponentWithName(received, actual) {
