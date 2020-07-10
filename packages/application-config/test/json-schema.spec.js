450df7fa789ea0dedbf19bf867ef416c0e43ff59
Nicola Molinari <nicola.molinari@commercetools.de>
2020-07-10 11:54:06 +0200

    refactor(config): add more text examples

--- packages/application-config/test/json-schema.spec.js
+++ packages/application-config/test/json-schema.spec.js
@@ -1,18 +1,25 @@
 const JsonSchema = require('@hyperjump/json-schema');
 const schemaJson = require('../schema.json');
 const fixtureConfigSimple = require('./fixtures/config-simple.json');
+const fixtureConfigFull = require('./fixtures/config-full.json');
+const fixtureConfigForDevelopment = require('./fixtures/config-for-development.json');
 
 const schemaId =
   'https://unpkg.com/@commercetools-frontend/application-config/schema.json';
 
 JsonSchema.add(schemaJson, schemaId);
 
-describe('simple config', () => {
-  it('should validate config', async () => {
+describe.each`
+  name                 | config
+  ${'Simple'}          | ${fixtureConfigSimple}
+  ${'Full'}            | ${fixtureConfigFull}
+  ${'For development'} | ${fixtureConfigForDevelopment}
+`('validating config "$name"', ({ config }) => {
+  it('should detect the config as valid', async () => {
     const schema = await JsonSchema.get(schemaId);
     const output = await JsonSchema.validate(
       schema,
-      fixtureConfigSimple,
+      config,
       JsonSchema.DETAILED
     );
     if (!output.valid) {
