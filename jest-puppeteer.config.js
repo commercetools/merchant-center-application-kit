d9c69066dc511c1ab37268d0fcee2e7256a3ac03
Nicola Molinari <nicola.molinari@commercetools.de>
2020-07-10 09:52:04 +0200

    fix: use google chrome stable for puppeteer

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   launch: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: 'google-chrome-unstable',
+    executablePath: 'google-chrome-stable',
   },
   server: {
     command: 'yarn visual-testing-app:serve',
