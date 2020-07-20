77eb38ace68e7f519dea9deda487ed4c612091a5
Nicola Molinari <nicola.molinari@commercetools.de>
2020-07-20 13:55:52 +0200

    fix: unify login/logout dev routes for http servers (#1641)
    * fix: unify login/logout dev routes for http servers
    
    * docs: changeset
    
    * fix(dev-auth): refine boolean check

---
+++ packages/mc-dev-authentication/middlewares/index.js
@@ -0,0 +1,7 @@
+const createLoginMiddleware = require('./create-login-middleware');
+const createLogoutMiddleware = require('./create-logout-middleware');
+
+module.exports = {
+  createLoginMiddleware,
+  createLogoutMiddleware,
+};
--- packages/mc-http-server/routes/login.js
+++
@@ -1,7 +0,0 @@
-module.exports = (env) => (request, response, next) => {
-  if (!env.servedByProxy) {
-    response.render('login', { env });
-  } else {
-    next();
-  }
-};
