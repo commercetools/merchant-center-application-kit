feb49143431f9ee25f69ab79cfbaa664a53984a0
Nicola Molinari <nicola.molinari@commercetools.de>
2020-08-21 09:22:31 +0200

    test: rewrite project-suspended test to use msw

--- packages/application-shell/src/components/project-suspended/project-suspended.spec.js
+++ packages/application-shell/src/components/project-suspended/project-suspended.spec.js
@@ -1,102 +1,40 @@
+import { graphql } from 'msw';
+import { setupServer } from 'msw/node';
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { GRAPHQL_TARGETS } from '@commercetools-frontend/constants';
-import { renderApp, screen } from '../../test-utils';
-import UserQuery from '../fetch-user/fetch-user.mc.graphql';
-import ProjectsQuery from '../project-switcher/project-switcher.mc.graphql';
+import { UserMock, ProjectMock } from '../../../../../graphql-test-utils';
+import { experimentalRenderAppWithRedux, screen } from '../../test-utils';
 import ProjectSuspended from './project-suspended';
+
+const mockServer = setupServer(
+  graphql.query('FetchUserProjects', (req, res, ctx) =>
+    res(
+      ctx.data({
+        user: UserMock.build({
+          projects: {
+            __typename: 'ProjectQueryResult',
+            total: 2,
+            results: ProjectMock.buildList(2),
+          },
+        }),
+      })
+    )
+  )
+);
+afterEach(() => {
+  mockServer.resetHandlers();
+});
+beforeAll(() => mockServer.listen());
+afterAll(() => mockServer.close());
 
 describe('rendering', () => {
   it('when suspension is temporary it should print correct message', async () => {
-    renderApp(
+    experimentalRenderAppWithRedux(
       <Route
         path="/:projectKey"
         render={() => <ProjectSuspended isTemporary={true} />}
       />,
-      {
-        route: '/my-project',
-        addTypename: true,
-        mocks: [
-          {
-            request: {
-              query: UserQuery,
-              variables: { target: GRAPHQL_TARGETS.MERCHANT_CENTER_BACKEND },
-            },
-            result: {
-              data: {
-                user: {
-                  __typename: 'User',
-                  id: 'user-id',
-                  email: 'user-email',
-                  gravatarHash: 'user-gravatarHash',
-                  firstName: 'John',
-                  lastName: 'Snow',
-                  language: 'en',
-                  numberFormat: 'en-US',
-                  timeZone: null,
-                  launchdarklyTrackingId: 'ld-1',
-                  launchdarklyTrackingGroup: 'commercetools',
-                  launchdarklyTrackingSubgroup: 'dev',
-                  launchdarklyTrackingTeam: 'ld-team-1',
-                  launchdarklyTrackingTenant: 'eu',
-                  defaultProjectKey: 'project-key',
-                  projects: {
-                    __typename: 'ProjectQueryResult',
-                    total: 1,
-                    results: [
-                      {
-                        __typename: 'Project',
-                        key: 'key-1',
-                        name: 'name-1',
-                        suspension: {
-                          __typename: 'ProjectSuspension',
-                          isActive: true,
-                        },
-                        expiry: {
-                          __typename: 'ProjectExpiry',
-                          isActive: false,
-                        },
-                      },
-                    ],
-                  },
-                },
-              },
-            },
-          },
-          {
-            request: {
-              query: ProjectsQuery,
-              variables: { target: GRAPHQL_TARGETS.MERCHANT_CENTER_BACKEND },
-            },
-            result: {
-              data: {
-                user: {
-                  __typename: 'User',
-                  id: 'user-id',
-                  projects: {
-                    __typename: 'ProjectQueryResult',
-                    results: [
-                      {
-                        __typename: 'Project',
-                        key: 'key-1',
-                        name: 'name-1',
-                        suspension: {
-                          __typename: 'ProjectSuspension',
-                          isActive: true,
-                        },
-                        expiry: {
-                          __typename: 'ProjectExpiry',
-                          isActive: false,
-                        },
-                      },
-                    ],
-                  },
-                },
-              },
-            },
-          },
-        ],
-      }
+      { route: '/my-project' }
     );
     await screen.findByText(
       /Your Project is temporarily suspended due to maintenance/
