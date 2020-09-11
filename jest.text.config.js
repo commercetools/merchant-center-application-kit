7c7d02212e864a14943cf6c185db99eb5221c6fe
Nicola Molinari <nicola.molinari@commercetools.de>
2020-09-11 09:43:00 +0200

    chore: do not lint changelog for writing styles

--- jest.text.config.js
+++ jest.text.config.js
@@ -2,7 +2,7 @@ module.exports = {
   runner: 'jest-runner-executor',
   displayName: 'vale',
   moduleFileExtensions: ['md', 'mdx'],
-  modulePathIgnorePatterns: ['build', 'dist', 'public'],
+  modulePathIgnorePatterns: ['build', 'dist', 'public', 'CHANGELOG.md'],
   testMatch: ['<rootDir>/website/**/*.md', '<rootDir>/website/**/*.mdx'],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-master'],
 };
