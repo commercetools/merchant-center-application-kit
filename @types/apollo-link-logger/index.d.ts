c10ffc6224c17bfbf9744f9924bbccfc07fbeafe
Nicola Molinari <nicola.molinari@commercetools.de>
2020-09-30 10:53:20 +0200

    refactor: use apollo-link-logger package

---
+++ @types/apollo-link-logger/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'apollo-link-logger' {
+  import { ApolloLink } from '@apollo/client';
+  const apolloLinkLogger: ApolloLink;
+  export default apolloLinkLogger;
+}
--- packages/application-shell/src/apollo-links/index.ts
+++ packages/application-shell/src/apollo-links/index.ts
@@ -1,4 +1,5 @@
+export { default as loggerLink } from 'apollo-link-logger';
+
 export { default as errorLink } from './error-link';
 export { default as headerLink } from './header-link';
-export { default as loggerLink } from './logger-link';
 export { default as tokenRetryLink } from './token-retry-link';
