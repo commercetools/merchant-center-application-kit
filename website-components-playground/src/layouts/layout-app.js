7d828d35ed07eb2874b7d4d7d0d28b58d2730dc3
Nicola Molinari <nicola.molinari@commercetools.de>
2020-11-18 09:36:42 +0100

    refactor(website): use fontsource package instead of gatsby fonts plugin (#1883)

--- website-components-playground/src/layouts/layout-app.js
+++ website-components-playground/src/layouts/layout-app.js
@@ -1,3 +1,7 @@
+import 'fontsource-open-sans/latin-300.css';
+import 'fontsource-open-sans/latin-400.css';
+import 'fontsource-open-sans/latin-600.css';
+import 'fontsource-open-sans/latin-700.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css, Global } from '@emotion/core';
