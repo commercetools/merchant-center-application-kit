48b330e9432b75d1416138a54c0c1bad57d59347
Nicola Molinari <nicola.molinari@commercetools.de>
2020-11-18 09:10:34 +0100

    refactor(website): use fontsource package instead of gatsby fonts plugin

--- website-components-playground/gatsby-config.js
+++ website-components-playground/gatsby-config.js
@@ -9,27 +9,5 @@ module.exports = {
     repositoryUrl:
       'https://github.com/commercetools/merchant-center-application-kit',
   },
-  plugins: [
-    `gatsby-plugin-emotion`,
-    {
-      resolve: 'gatsby-plugin-prefetch-google-fonts',
-      options: {
-        fonts: [
-          {
-            family: 'Open Sans',
-            variants: [
-              '300',
-              '300i',
-              '400',
-              '400i',
-              '600',
-              '600i',
-              '700',
-              '700i',
-            ],
-          },
-        ],
-      },
-    },
-  ],
+  plugins: [`gatsby-plugin-emotion`],
 };
