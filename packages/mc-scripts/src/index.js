70ac94d8e9417c6bc34bed2ec37fa8521af1910f
Nicola Molinari <nicola.molinari@commercetools.de>
2020-11-25 22:27:45 +0100

    fix: wrong export

--- packages/mc-scripts/src/index.js
+++ packages/mc-scripts/src/index.js
@@ -1,11 +1,9 @@
 const createWebpackConfigForDevelopment = require('./config/create-webpack-config-for-development');
 const createWebpackConfigForProduction = require('./config/create-webpack-config-for-production');
-const createDevServerConfig = require('./config/webpack-dev-server.config');
 const vendorsToCompile = require('./config/vendors-to-transpile');
 
 module.exports = {
   createWebpackConfigForDevelopment,
   createWebpackConfigForProduction,
-  createDevServerConfig,
   vendorsToCompile,
 };
