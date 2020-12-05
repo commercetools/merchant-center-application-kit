746da5d54266ac811b2ef693829bbcd3641fc1e1
Nicola Molinari <nicola.molinari@commercetools.de>
2020-12-05 17:49:30 +0100

    fix(babel): regression, pass ROLLUP_BUILD env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BUILD_ROLLUP = true;
+
 module.exports = {
   presets: ['@commercetools-frontend/babel-preset-mc-app'],
   plugins: [
