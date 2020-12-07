02d4a5fbf607f162aee64607f829af229f9af50d
Adnan Asani <adnasa@users.noreply.github.com>
2020-12-07 12:37:16 +0100

    refactor(product-picker/messages): update messages

--- packages/application-components/src/experimental/components/product-picker-input/messages.ts
+++ packages/application-components/src/experimental/components/product-picker-input/messages.ts
@@ -2,17 +2,17 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   noProductsFound: {
-    id: 'AppKit.ProductPickerInput.noProductsFound',
+    id: 'Components.ProductPickerInput.noProductsFound',
     description: 'The message to display when no products were found',
     defaultMessage: 'No case sensitive matches found',
   },
   placeholder: {
-    id: 'AppKit.ProductPickerInput.placeholder',
+    id: 'Components.ProductPickerInput.placeholder',
     description: 'The placeholder of the input',
     defaultMessage: 'Search by name or key (case sensitive)',
   },
   onResourceLoadError: {
-    id: 'AppKit.ProductPickerInput.onResourceLoadError',
+    id: 'Components.ProductPickerInput.onResourceLoadError',
     description: 'Message to show when fetching the initial value failed.',
     defaultMessage:
       'Unable to load products. Check the connection and try again.',
