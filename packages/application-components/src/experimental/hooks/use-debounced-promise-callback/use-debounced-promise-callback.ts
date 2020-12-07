ab194aaa21aef9db1e666f74fbf0c62a7d7f1021
Adnan Asani <adnasa@users.noreply.github.com>
2020-12-07 16:32:05 +0100

    refactor(hooks/use-debounce): update signature type

--- packages/application-components/src/experimental/hooks/use-debounced-promise-callback/use-debounced-promise-callback.ts
+++ packages/application-components/src/experimental/hooks/use-debounced-promise-callback/use-debounced-promise-callback.ts
@@ -1,9 +1,15 @@
 import debounce from 'debounce-promise';
 import { useCallback } from 'react';
 
-const useDebouncedPromiseCallback = <T extends (...args: any[]) => any>(
-  promiseCallback: T,
-  wait: number
-) => useCallback(debounce(promiseCallback, wait), [promiseCallback]);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type DebouncedCallback<T> = (...args: any[]) => Promise<T>;
+
+const useDebouncedPromiseCallback = <T>(
+  promiseCallback: DebouncedCallback<T>,
+  delayedTimeMS: number
+): DebouncedCallback<T> => {
+  const debounced = debounce(promiseCallback, delayedTimeMS);
+  return useCallback(() => debounced(), [debounced]);
+};
 
 export default useDebouncedPromiseCallback;
