7e359413d5db377ba91f02e3a7f5b9cf76790e96
Timon Turak <timon.turak@commercetools.de>
2020-12-16 15:43:18 +0100

    refactor: remove spacing and border from override

---
+++ .changeset/gentle-mayflies-heal.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-website/custom-applications': patch
+---
+
+The second left border of the Github icon in the right column was removed.
--- website/src/@commercetools-docs/gatsby-theme-docs/overrides/page-header-side.js
+++ website/src/@commercetools-docs/gatsby-theme-docs/overrides/page-header-side.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import { css } from '@emotion/react';
 import { useSiteData, Link } from '@commercetools-docs/gatsby-theme-docs';
-import { designSystem } from '@commercetools-docs/ui-kit';
-import SpacingsInline from '@commercetools-uikit/spacings-inline';
 import { GithubSvgIcon } from '../../../icons';
 
 const RepositoryLinks = () => {
   const siteData = useSiteData();
   return (
-    <div
-      css={css`
-        margin: ${designSystem.dimensions.spacings.m} 0;
-        padding: 0 ${designSystem.dimensions.spacings.m};
-        border-left: 1px solid ${designSystem.colors.light.borderPrimary};
-      `}
+    <Link
+      href={siteData.siteMetadata.repositoryUrl}
+      title="Application Kit repository"
     >
-      <SpacingsInline scale="l" alignItems="center" justifyContent="flex-start">
-        <Link
-          href={siteData.siteMetadata.repositoryUrl}
-          title="Application Kit repository"
-        >
-          <GithubSvgIcon />
-        </Link>
-      </SpacingsInline>
-    </div>
+      <GithubSvgIcon />
+    </Link>
   );
 };
 
