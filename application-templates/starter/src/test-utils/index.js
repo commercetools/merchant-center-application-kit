6dd87d24970a9ec5c321dc4e7b6d8b7e51693d45
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-08 13:22:51 +0100

    refactor(starter): avoid using async routes in tests (#1960)

--- application-templates/starter/src/test-utils/index.js
+++ application-templates/starter/src/test-utils/index.js
@@ -3,7 +3,7 @@ import {
   renderApp,
   renderAppWithRedux,
 } from '@commercetools-frontend/application-shell/test-utils';
-import { AsyncApplicationRoutes } from '../components/entry-point';
+import ApplicationRoutes from '../routes';
 
 const entryPointUriPath = 'examples-starter';
 
@@ -18,13 +18,13 @@ const mergeWithDefaultOptions = (options = {}) => ({
 
 const renderApplication = (ui, options) =>
   renderApp(
-    <AsyncApplicationRoutes>{ui}</AsyncApplicationRoutes>,
+    <ApplicationRoutes>{ui}</ApplicationRoutes>,
     mergeWithDefaultOptions(options)
   );
 
 const renderApplicationWithRedux = (ui, options = {}) =>
   renderAppWithRedux(
-    <AsyncApplicationRoutes>{ui}</AsyncApplicationRoutes>,
+    <ApplicationRoutes>{ui}</ApplicationRoutes>,
     mergeWithDefaultOptions(options)
   );
 
