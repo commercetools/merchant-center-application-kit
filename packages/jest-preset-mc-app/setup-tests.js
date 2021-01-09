4fa8eb37d3fdf23464778e6849dd20a704d4f938
tdeekens <tobias.deekens@commercetools.com>
2021-01-10 00:46:39 +0100

    refactor(jest-preset): to remove unhandled promise lockfile

--- packages/jest-preset-mc-app/setup-tests.js
+++ packages/jest-preset-mc-app/setup-tests.js
@@ -67,30 +67,3 @@ const logError = console.error;
 global.console.error = (...messages) => {
   logOrThrow(logError, 'error', messages);
 };
-
-// Avoid unhandled promise rejections from going unnoticed
-// https://github.com/facebook/jest/issues/3251#issuecomment-299183885
-// In Node v7 unhandled promise rejections will terminate the process
-if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
-  process.on('unhandledRejection', (reason) => {
-    logMessage('UNHANDLED REJECTION', reason);
-
-    // We create a file in case there is an unhandled rejection
-    // We later check for the existence of this file to fail CI
-    if (process.env.CI && !process.env.HAS_CREATED_UNHANDLED_REJECTION_FILE) {
-      const rootDir = pkgDir.sync(__dirname);
-
-      fs.writeFileSync(
-        path.join(
-          rootDir,
-          'fail-tests-because-there-was-an-unhandled-rejection.lock'
-        ),
-        '',
-        { encoding: 'utf8' }
-      );
-      process.env.HAS_CREATED_UNHANDLED_REJECTION_FILE = true;
-    }
-  });
-  // Avoid memory leak by adding too many listeners
-  process.env.LISTENING_TO_UNHANDLED_REJECTION = true;
-}
