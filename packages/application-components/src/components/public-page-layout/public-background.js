b9eb29d2988e5d26ca6cc0496b8fa2b92d58ef95
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-16 15:58:27 +0100

    fix(components): base64 conversion

--- packages/application-components/src/components/public-page-layout/public-background.js
+++ packages/application-components/src/components/public-page-layout/public-background.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 const pngPath = path.join(__dirname, 'public-background.png');
-const svgString = fs.readFileSync(pngPath, 'utf8');
-const base64String = Buffer.from(svgString).toString('base64');
+const base64String = fs.readFileSync(pngPath).toString('base64');
 
 module.exports = base64String;
