28588f4ca8db5efa85788c2473e9ab8f4c3f31a0
Nicola Molinari <nicola.molinari@commercetools.de>
2021-05-06 13:15:32 +0200

    fix(mc-scripts): loading and parsing of dotenv files (#2195)
    * fix(mc-scripts): loading and parsing of dotenv files
    
    * docs: changeset

---
+++ .changeset/rotten-foxes-bake.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-frontend/mc-scripts': patch
+---
+
+Fix loading and evaluation of custom dotenv files
--- packages/mc-scripts/src/commands/serve.js
+++ packages/mc-scripts/src/commands/serve.js
@@ -1,15 +1,4 @@
 /* eslint-disable no-console,global-require,import/no-dynamic-require */
-
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.NODE_ENV = 'production';
-
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', (err) => {
-  throw err;
-});
-
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
