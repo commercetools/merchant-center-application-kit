99ea52d354a9ad61187388553e1490964347550f
Tobias Deekens <tobias.deekens@commercetools.com>
2021-05-11 12:25:13 +0200

    fix(babel-preset-mc-app): to add loose private methods (#2203)
    * fix(babel-preset-mc-app): to add loose private methods
    
    * Create tiny-kids-wait.md
    
    * fix(babel-preset-mc-app): to add anoher loose option
    
    * feat(babel-preset-mc-app): to allow configurating loose option
    
    * fix: disable loose mode for cypress
    
    * Apply suggestions from code review
    
    Co-authored-by: Nicola Molinari <nicola.molinari@commercetools.de>
    
    * fix: boolean condition
    
    Co-authored-by: Nicola Molinari <nicola.molinari@commercetools.de>

---
+++ .changeset/tiny-kids-wait.md
@@ -0,0 +1,5 @@
+---
+"@commercetools-frontend/babel-preset-mc-app": patch
+---
+
+Fixes `babel-preset-mc-app` warnings about `loose` mode. You can now pass a `disableLooseMode` option to this preset to turn it off (defaults to `true`).
--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
         // TODO: change this to `automatic` when using the new runtime
         runtime: 'classic',
         keepPropTypes: true,
+        disableLooseMode: process.env.CYPRESS_CI === 'true',
       },
     ],
   ],
