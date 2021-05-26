be3eef98997085874073ebdbd9fabdae4212dfbc
tdeekens <tobias.deekens@commercetools.com>
2021-05-26 14:52:39 +0200

    refactor: to update to jest v27

--- packages/jest-preset-mc-app/transform-babel-jest.js
+++ packages/jest-preset-mc-app/transform-babel-jest.js
@@ -1,6 +1,6 @@
 const getBabePresetConfigForMcApp = require('@commercetools-frontend/babel-preset-mc-app');
 const getJestBabelPreset = require('babel-preset-jest');
-const babelJest = require('babel-jest');
+const { default: babelJest } = require('babel-jest');
 const hasJsxRuntime = require('./has-jsx-runtime');
 
 const mcAppBabelConfig = getBabePresetConfigForMcApp(null, {
