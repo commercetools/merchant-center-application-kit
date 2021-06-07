84dfcb52167ac71f026a410eeb39acad55296ec5
tdeekens <tobias.deekens@commercetools.com>
2021-06-07 08:25:01 +0200

    fix(jest-preset-mc-app): add backwards compatible check

--- packages/jest-preset-mc-app/transform-babel-jest.js
+++ packages/jest-preset-mc-app/transform-babel-jest.js
@@ -1,6 +1,6 @@
 const getBabePresetConfigForMcApp = require('@commercetools-frontend/babel-preset-mc-app');
 const getJestBabelPreset = require('babel-preset-jest');
-const { default: babelJest } = require('babel-jest');
+const babelJest = require('babel-jest');
 const hasJsxRuntime = require('./has-jsx-runtime');
 
 const mcAppBabelConfig = getBabePresetConfigForMcApp(null, {
@@ -14,6 +14,22 @@ const jestBabelConfig = {
   plugins: [...mcAppBabelConfig.plugins, ...getJestBabelPreset().plugins],
 };
 
-const transformer = babelJest.createTransformer(jestBabelConfig);
+/**
+ * NOTE:
+ *  In `babel-jest` v27 the export has changed
+ *  from: `babelJest.createTransformer`
+ *  to `babelJest.default.createTransformer`.
+ *  In order to remain backwards compatible the
+ *  type of the export is checked.
+ *
+ *  TODO: v21: Remove check and release `jest-preset-mc-app`
+ *  as a breaking release.
+ */
+const createTransformer =
+  typeof babelJest.createTransformer === 'function'
+    ? babelJest.createTransformer
+    : babelJest.default.createTransformer;
+
+const transformer = createTransformer(jestBabelConfig);
 
 module.exports = transformer;
