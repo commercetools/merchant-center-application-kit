fd03bfdb882b5090b7880e5e4d657cef6bbc9128
Nicola Molinari <nicola.molinari@commercetools.com>
2021-07-30 22:11:35 +0200

    chore: try disabling custom loader

--- packages/application-config/src/load-config.ts
+++ packages/application-config/src/load-config.ts
@@ -1,20 +1,20 @@
 import type { JSONSchemaForCustomApplicationConfigurationFiles } from './schema';
-import type { LoaderSync } from 'cosmiconfig';
+// import type { LoaderSync } from 'cosmiconfig';
 
 import { cosmiconfigSync, defaultLoaders } from 'cosmiconfig';
-import get from 'lodash/get';
+// import get from 'lodash/get';
 
-const loadJsModule: LoaderSync = (filePath) => {
-  // Load JS modules using Babel, as we need to load
-  // the config synchronously with `require`, no `await import`.
-  require('@babel/register')({
-    babelrc: false,
-    extensions: ['.js', '.cjs', '.mjs', '.ts'],
-    presets: ['@commercetools-frontend/babel-preset-mc-app'],
-  });
-  const result = require(filePath);
-  return get(result, 'default', result);
-};
+// const loadJsModule: LoaderSync = (filePath) => {
+//   // Load JS modules using Babel, as we need to load
+//   // the config synchronously with `require`, no `await import`.
+//   require('@babel/register')({
+//     babelrc: false,
+//     extensions: ['.js', '.cjs', '.mjs', '.ts'],
+//     presets: ['@commercetools-frontend/babel-preset-mc-app'],
+//   });
+//   const result = require(filePath);
+//   return get(result, 'default', result);
+// };
 
 const moduleName = 'custom-application-config';
 const explorer = cosmiconfigSync(moduleName, {
@@ -34,10 +34,10 @@ const explorer = cosmiconfigSync(moduleName, {
   ],
   loaders: {
     noExt: defaultLoaders['.json'],
-    '.js': loadJsModule,
-    '.cjs': loadJsModule,
-    '.mjs': loadJsModule,
-    '.ts': loadJsModule,
+    // '.js': loadJsModule,
+    // '.cjs': loadJsModule,
+    // '.mjs': loadJsModule,
+    // '.ts': loadJsModule,
   },
 });
 
