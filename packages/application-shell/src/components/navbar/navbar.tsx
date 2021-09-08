090dfd55e26429592cc1c249a74c24417910889c
Byron Wall <byron.wall@commercetools.com>
2021-09-08 08:53:34 -0400

    feat(navbar-expansion-behavior): set flyout to open if screen width is less than 1024px

--- packages/application-shell/src/components/navbar/navbar.tsx
+++ packages/application-shell/src/components/navbar/navbar.tsx
@@ -431,8 +431,9 @@ const ApplicationMenu = (props: ApplicationMenuProps) => {
   );
 
   useEffect(() => {
-    const canExpandMenu = window.innerWidth > 918;
-    if (canExpandMenu && isMainMenuRouteActive(props.menu.uriPath)) {
+    // const canExpandMenu = window.innerWidth > 918;
+    const shouldOpen = window.innerWidth > 1024;
+    if (shouldOpen && isMainMenuRouteActive(props.menu.uriPath)) {
       props.handleToggleItem();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
