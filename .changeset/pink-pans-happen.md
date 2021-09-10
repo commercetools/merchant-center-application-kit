932e2440dcc05e865e0175589d35aaad07cb84fb
Byron Wall <byron.wall@commercetools.com>
2021-09-10 10:54:08 -0400

    feat(navbar-expansion-behavior): add changeset, update playground permissions to match main, ensure cypress viewport for appkit starter is 1024px

---
+++ .changeset/pink-pans-happen.md
@@ -0,0 +1,6 @@
+---
+'@commercetools-frontend/application-shell': patch
+'playground': patch
+---
+
+    feat(navbar-expansion-behavior): toggle menu and submenu for the currently loaded application on initial mount in navbar whne screen width is greater than 1024px"
--- cypress/integration/template-starter/channels.js
+++ cypress/integration/template-starter/channels.js
@@ -2,6 +2,7 @@ import { URL_EXAMPLES_STARTER_CHANNELS } from '../../support/urls';
 
 describe('Channels', () => {
   it('should render page', () => {
+    cy.viewport(1024, 1024);
     cy.login({ redirectToUri: URL_EXAMPLES_STARTER_CHANNELS });
     cy.findByText('Channels list').should('exist');
     cy.findByText('Store Berlin').should('exist');
