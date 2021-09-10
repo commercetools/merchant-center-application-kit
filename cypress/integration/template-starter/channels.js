54eb73517123685d84ffa1189646034e42a7f7a0
Byron Wall <byron.wall@commercetools.com>
2021-09-10 11:38:08 -0400

    feat(navbar-expansion-behavior): enlarge viewport on channels test

--- cypress/integration/template-starter/channels.js
+++ cypress/integration/template-starter/channels.js
@@ -3,9 +3,10 @@ import { URL_EXAMPLES_STARTER_CHANNELS } from '../../support/urls';
 describe('Channels', () => {
   it('should render page', () => {
     cy.login({ redirectToUri: URL_EXAMPLES_STARTER_CHANNELS });
+    cy.viewport(1030, 2200);
     cy.findByText('Channels list').should('exist');
     cy.findByText('Store Berlin').should('exist');
     cy.findByText('Store Munich').should('exist');
-    cy.percySnapshot(null, { 'enable-javascript': true });
+    cy.percySnapshot();
   });
 });
