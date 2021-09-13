f5df1f5a82301c236e078f9050ce27d2ba1c352c
Byron Wall <byron.wall@commercetools.com>
2021-09-13 12:39:26 -0400

    feat(navbar-expansion-behavior): revert permissions in app-config and router to match main, remove changes to e2e tests made for debugging purposes

--- cypress/integration/template-starter/channels.js
+++ cypress/integration/template-starter/channels.js
@@ -4,7 +4,6 @@ describe('Channels', () => {
   it('should render page', () => {
     cy.login({
       redirectToUri: URL_EXAMPLES_STARTER_CHANNELS,
-      isForcedMenuOpen: false,
     });
     cy.findByText('Channels list').should('exist');
     cy.findByText('Store Berlin').should('exist');
