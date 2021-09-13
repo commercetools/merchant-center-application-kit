0d8efe9d6e79678e73aca001c0ca20b1365edb3f
Byron Wall <byron.wall@commercetools.com>
2021-09-13 11:35:39 -0400

    feat(navbar-expansion-behavior): update channels and welcome e2e tests to use 'setDesktopViewport' method before login

--- cypress/integration/template-starter/channels.js
+++ cypress/integration/template-starter/channels.js
@@ -2,8 +2,9 @@ import { URL_EXAMPLES_STARTER_CHANNELS } from '../../support/urls';
 
 describe('Channels', () => {
   it('should render page', () => {
-    cy.login({ redirectToUri: URL_EXAMPLES_STARTER_CHANNELS });
     cy.setDesktopViewport();
+    cy.login({ redirectToUri: URL_EXAMPLES_STARTER_CHANNELS });
+
     cy.findByText('Channels list').should('exist');
     cy.findByText('Store Berlin').should('exist');
     cy.findByText('Store Munich').should('exist');
--- cypress/integration/template-starter/welcome.js
+++ cypress/integration/template-starter/welcome.js
@@ -2,8 +2,8 @@ import { URL_EXAMPLES_STARTER } from '../../support/urls';
 
 describe('Welcome', () => {
   it('should render page', () => {
-    cy.login({ redirectToUri: URL_EXAMPLES_STARTER });
     cy.setDesktopViewport();
+    cy.login({ redirectToUri: URL_EXAMPLES_STARTER });
     cy.findByText('Develop applications for the Merchant Center').should(
       'exist'
     );
