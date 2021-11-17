b5c90df8c9f6420a874d79bab5a32ebf59b03fa8
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-17 21:27:08 +0100

    chore: use npm run run vercel deploy command for playground apps

--- scripts/deploy_playground.sh
+++ scripts/deploy_playground.sh
@@ -8,10 +8,14 @@ echo "Deploying Playground for production GCP-EU"
 yarn --cwd playground compile-html:gcp-eu
 rm -rf playground/vercel-deployments/state-machines-gcp-eu/public
 cp -R playground/public playground/vercel-deployments/state-machines-gcp-eu
-yarn --cwd playground/vercel-deployments/state-machines-gcp-eu run deploy --token="$VERCEL_TOKEN_PLAYGROUND"
+pushd playground/vercel-deployments/state-machines-gcp-eu
+npm run deploy -- --token="$VERCEL_TOKEN_PLAYGROUND"
+popd
 
 echo "Deploying Playground for production GCP-US"
 yarn --cwd playground compile-html:gcp-us
 rm -rf playground/vercel-deployments/state-machines-gcp-us/public
 cp -R playground/public playground/vercel-deployments/state-machines-gcp-us
-yarn --cwd playground/vercel-deployments/state-machines-gcp-us run deploy --token="$VERCEL_TOKEN_PLAYGROUND"
+pushd playground/vercel-deployments/state-machines-gcp-us
+npm run deploy -- --token="$VERCEL_TOKEN_PLAYGROUND"
+popd
