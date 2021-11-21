4755f4e8d4cc9ed6d005298512e6d62b95336270
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-22 00:40:37 +0100

    fix(vercel): run preconstruct dev

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,14 @@ set -e
 
 
 if [ "$VERCEL_PROJECT" = "app-kit-playground" ]; then
-  echo "Deploying Vercel project $VERCEL_PROJECT."
+  echo "Deploying Vercel project $VERCEL_PROJECT..."
 
   yarn build
   yarn workspace playground run build
   yarn workspace playground run compile-html
 
 else
-  echo "Deploying Custom Applications documentation."
+  echo "Deploying Custom Applications documentation..."
 
   yarn build:website
   rm -rf public
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-if [ -n "$VERCEL" ]; then
-  echo "Running on Vercel, skipping development setup."
+echo "Preparing development setup..."
 
-else
-  echo "Preparing development setup."
-
-  yarn husky install
-  yarn manypkg check
-  yarn preconstruct dev
-  # We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
-  yarn prettier --write --parser json '**/package.json' &>/dev/null
-fi
+yarn husky install
+yarn manypkg check
+yarn preconstruct dev
+# We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
+yarn prettier --write --parser json '**/package.json' &>/dev/null
 
 yarn patch-package
 yarn compile-css-modules
