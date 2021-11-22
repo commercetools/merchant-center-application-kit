811c6b3ad2d02c36461751df2a73cbdaba141cf8
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-22 16:45:57 +0100

    refactor: to skip postinstall on Vercel

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-echo "Preparing development setup..."
+if [ -n "$VERCEL" ]; then
+  echo "Running on Vercel, skipping development setup."
 
-yarn husky install
-yarn manypkg check
-yarn preconstruct dev
-# We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
-yarn prettier --write --parser json '**/package.json' &>/dev/null
+else
+  echo "Preparing development setup."
+
+  yarn husky install
+  yarn manypkg check
+  yarn preconstruct dev
+  # We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
+  yarn prettier --write --parser json '**/package.json' &>/dev/null
+fi
 
 yarn patch-package
 yarn compile-css-modules
