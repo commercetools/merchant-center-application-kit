59b72adff45d631d751a32541fa23d2d5d795b3f
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2021-11-23 12:19:39 +0100

    feat(application-config): Allow "frame-src" directive in the Custom Application Config (#2403)
    * feat(schema): introduce frame-src
    
    * feat(schema): add changeset
    
    * Update .changeset/kind-poets-agree.md
    
    Co-authored-by: Nicola Molinari <nicola.molinari@commercetools.com>
    
    Co-authored-by: Nicola Molinari <nicola.molinari@commercetools.com>

---
+++ .changeset/kind-poets-agree.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-frontend/application-config': minor
+---
+
+Allow to use the "frame-src" directive for `headers.csp` in the Custom Application config.
--- packages/application-config/src/schema.ts
+++ packages/application-config/src/schema.ts
@@ -89,6 +89,7 @@ export interface JSONSchemaForCustomApplicationConfigurationFiles {
       'img-src'?: CspDirective;
       'script-src'?: CspDirective;
       'style-src'?: CspDirective;
+      'frame-src'?: CspDirective;
     };
     /**
      * Configuration for the HTTP Feature-Policy header (https://developer.mozilla.org/en-US/docs/Web/HTTP/Feature_Policy/Using_Feature_Policy)
