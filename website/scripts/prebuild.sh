562bcbc053526e836e1096ea7c41fea2300b12f1
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-24 16:33:07 +0100

    Update vercel.json to use non-deprecated fields (#2405)
    * refactor(vercel.json): do not use deprecated fields
    
    * chore(website): missing dep
    
    * refactor(website): use vercel rewrites, do not move website into subfolder
    
    * chore: no need to build packages when building docs
    
    * refactor: dev setup on postinstall also on vercel
    
    * fix(website): to check links against a running gatsby server
    
    * fix: compile intl before building websites
    
    * fix: build everything
    
    * fix: website playground setup

--- website/scripts/prebuild.sh
+++ website/scripts/prebuild.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-rm -rf public/custom-applications
 # Copy the custom application config `schema.json` to the website static assets.
 cp ../packages/application-config/schema.json static/
