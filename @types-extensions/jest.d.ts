114154401e32b1adf7b0a51e1c773f7eec7bfaf2
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-25 12:35:50 +0100

    fix: patch global NodeJS type due to Jest conflicts

--- @types-extensions/jest.d.ts
+++ @types-extensions/jest.d.ts
@@ -3,3 +3,10 @@ declare namespace jest {
     toHaveTextContent(selector: string): R;
   }
 }
+
+// Patch for Jest types using a depreacted type declaration for NodeJS.Global,
+// as it's been removed from Node > v16.
+// https://github.com/facebook/jest/issues/11640#issuecomment-893867514
+declare module NodeJS {
+  interface Global {}
+}
