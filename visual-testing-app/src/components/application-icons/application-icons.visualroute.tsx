f549637b4a235ecc768ca77710784ddf63d1889b
Nicola Molinari <nicola.molinari@commercetools.com>
2021-12-01 16:57:15 +0100

    feat(assets): add new Custom Apps icons and test variations with VRTs (#2416)

---
+++ visual-testing-app/src/components/application-icons/application-icons.visualroute.tsx
@@ -0,0 +1,81 @@
+/// <reference types="@emotion/react/types/css-prop" />
+
+import { css } from '@emotion/react';
+import InlineSvg from '@commercetools-uikit/icons/inline-svg';
+import AccessibleButton from '@commercetools-uikit/accessible-button';
+import Grid from '@commercetools-uikit/grid';
+import { customProperties } from '@commercetools-uikit/design-system';
+import { Suite, Spec } from '../../test-utils';
+
+// Cache object to store references so SVG icons.
+const svgIcons: Record<string, { default: string }> = {};
+const importAllSvgs = (requireContext: ReturnType<typeof require.context>) =>
+  requireContext.keys().forEach((filePath) => {
+    const name = filePath.replace(/\.\/(.*)\.svg$/, '$1') || filePath;
+    svgIcons[name] = requireContext(filePath);
+  });
+importAllSvgs(
+  require.context(
+    '!!raw-loader!@commercetools-frontend/assets/application-icons',
+    false,
+    /.svg$/
+  )
+);
+
+const IconsGrid = (props: {
+  color:
+    | 'solid'
+    | 'neutral60'
+    | 'surface'
+    | 'info'
+    | 'primary'
+    | 'primary40'
+    | 'warning'
+    | 'error';
+}) => (
+  <Grid
+    gridGap={customProperties.spacingS}
+    gridAutoColumns="1fr"
+    gridTemplateColumns={`repeat(auto-fill, minmax(calc(${customProperties.spacingXl} * 2), 1fr))`}
+  >
+    {Object.keys(svgIcons).map((iconName, index) => {
+      const data = svgIcons[iconName].default;
+      return (
+        <Grid.Item key={iconName} justifySelf="center">
+          <AccessibleButton
+            label={iconName}
+            buttonAttributes={{
+              'data-testid': `icon-svg-${index}`,
+              tabIndex: 0,
+              role: 'button',
+            }}
+            css={css`
+              padding: ${customProperties.spacingS};
+              width: calc(${customProperties.spacingXl} * 2);
+              height: calc(${customProperties.spacingXl} * 2);
+            `}
+            onClick={() => {}}
+          >
+            <InlineSvg data={data} size="scale" color={props.color} />
+          </AccessibleButton>
+        </Grid.Item>
+      );
+    })}
+  </Grid>
+);
+
+export const routePath = '/application-icons';
+
+export const Component = () => (
+  <Suite>
+    <Spec label="Application icons (solid)">
+      <IconsGrid color="solid" />
+    </Spec>
+    <Spec label="Application icons (primary)">
+      <IconsGrid color="primary" />
+    </Spec>
+    <Spec label="Application icons (warning)">
+      <IconsGrid color="warning" />
+    </Spec>
+  </Suite>
+);
