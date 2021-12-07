6c144673cdafd29fcdcd44302e7867906623a255
Nicola Molinari <nicola.molinari@commercetools.com>
2021-12-07 20:17:10 +0100

    fix: canary builds

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,13 +2,24 @@
 
 set -e
 
-echo "Preparing development setup."
+if [ -n "$SKIP_POSTINSTALL_DEV_SETUP" ]; then
+  echo "Skipping development setup."
 
-yarn husky install
-yarn manypkg check
-yarn preconstruct dev
+else
+  echo "Preparing development setup."
+  yarn husky install
+  yarn manypkg check
+  yarn preconstruct dev
+
+fi
+
+echo "Running prettier on package.json files"
 # We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
 yarn prettier --write --parser json '**/package.json' &>/dev/null
 
+
+echo "Patching packages"
 yarn patch-package
+
+echo "Compiling CSS modules"
 yarn compile-css-modules
