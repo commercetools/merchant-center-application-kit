ff44b3ba8072931c578cfe9ce3d17668df59af82
Nicola Molinari <nicola.molinari@commercetools.com>
2021-12-16 21:11:26 +0100

    fix: expose helpers from new ssr entry point

--- application-templates/starter/src/constants.js
+++ application-templates/starter/src/constants.js
@@ -1,4 +1,5 @@
-import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-config';
+// Make sure to import the helper functions from the `ssr` entry point.
+import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
 export const entryPointUriPath = 'template-starter';
 
