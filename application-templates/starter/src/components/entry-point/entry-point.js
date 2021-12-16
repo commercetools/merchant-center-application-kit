398606999fac16dff159253505924e095d568e6f
Nicola Molinari <nicola.molinari@commercetools.com>
2021-12-16 17:16:46 +0100

    fix: prettier

--- application-templates/starter/src/components/entry-point/entry-point.js
+++ application-templates/starter/src/components/entry-point/entry-point.js
@@ -10,8 +10,8 @@ import loadMessages from '../../load-messages';
 // Here we split up the main (app) bundle with the actual application business logic.
 // Splitting by route is usually recommended and you can potentially have a splitting
 // point for each route. More info at https://reactjs.org/docs/code-splitting.html
-const AsyncApplicationRoutes = React.lazy(
-  () => import('../../routes' /* webpackChunkName: "routes" */)
+const AsyncApplicationRoutes = React.lazy(() =>
+  import('../../routes' /* webpackChunkName: "routes" */)
 );
 
 // Ensure to setup the global error listener before any React component renders
