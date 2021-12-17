e10ff3fbbbf491f6432aa7f0f2d80734f851e3cb
Nicola Molinari <nicola.molinari@commercetools.com>
2021-12-17 12:40:05 +0100

    fix: placeholder url

--- application-templates/starter/custom-application-config.mjs
+++ application-templates/starter/custom-application-config.mjs
@@ -13,7 +13,7 @@ const config = {
     },
     production: {
       applicationId: 'TODO',
-      url: 'https://<your_app_hostname>.com',
+      url: 'https://your_app_hostname.com',
     },
   },
   oAuthScopes: {
