dcacef5222a1376d3a88993f4ffef6d4de36171b
Kacper Krzywiec <krzywiec@gmail.com>
2022-03-10 13:21:49 +0100

    test: try to fix vrt #2

--- visual-testing-app/src/components/tabular-modal-page/tabular-modal-page.visualspec.ts
+++ visual-testing-app/src/components/tabular-modal-page/tabular-modal-page.visualspec.ts
@@ -3,12 +3,11 @@ import { HOST } from '../../constants';
 
 describe('TabularModalPage', () => {
   beforeAll(async () => {
-    await page.goto(`${HOST}/tabular-modal-page`);
+    await page.goto(`${HOST}/tabular-modal-page`, { timeout: 200 });
   });
 
   it('Default', async () => {
     await expect(page).toMatch('TabularModalPage');
-    await new Promise((resolve) => setTimeout(resolve, 200));
     await percySnapshot(page, 'TabularModalPage');
   });
 });
