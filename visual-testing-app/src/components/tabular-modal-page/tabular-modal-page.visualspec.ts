600b1ddd755e29f8191813c46df9b3192d3f050d
Kacper Krzywiec <krzywiec@gmail.com>
2022-03-10 14:31:11 +0100

    test: try to fix vrt #6

--- visual-testing-app/src/components/tabular-modal-page/tabular-modal-page.visualspec.ts
+++ visual-testing-app/src/components/tabular-modal-page/tabular-modal-page.visualspec.ts
@@ -1,10 +1,12 @@
 import percySnapshot from '@percy/puppeteer';
 import { HOST } from '../../constants';
 
+jest.setTimeout(1000);
+
 describe('TabularModalPage', () => {
   beforeAll(async () => {
     await page.goto(`${HOST}/tabular-modal-page`);
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 300));
   });
 
   it('Default', async () => {
