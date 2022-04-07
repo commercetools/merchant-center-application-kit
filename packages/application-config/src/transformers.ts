e853df750b36ac0d329d2845f60b051f6352f81e
Nicola Molinari <nicola.molinari@commercetools.com>
2022-04-07 14:53:56 +0200

    fix(config): compute submenu link uriPath when parsing local config

--- packages/application-config/src/transformers.ts
+++ packages/application-config/src/transformers.ts
@@ -2,6 +2,21 @@ import type { JSONSchemaForCustomApplicationConfigurationFiles } from './schema'
 import type { CustomApplicationData } from './types';
 import { entryPointUriPathToResourceAccesses } from './formatters';
 
+// The `uriPath` of each submenu link is supposed to be defined relative
+// to the `entryPointUriPath`. Computing the full path is done internally to keep
+// the configuration simple.
+const computeUriPath = (uriPath: string, entryPointUriPath: string) => {
+  // In case the `uriPath` is only `/`, it means that the link is supposed to be
+  // treated the same as the main application path. In this case, the return value
+  // should not contain any unnecessary trailing slash and therefore we use the `entryPointUriPath`.
+  if (uriPath === '/') return entryPointUriPath;
+  // In case the `uriPath` is already configured including the `entryPointUriPath`,
+  // we return the `uriPath` as-is.
+  if (uriPath.startsWith(`${entryPointUriPath}/`)) return uriPath;
+  // Return the full path including the `entryPointUriPath` as a prefix.
+  return `${entryPointUriPath}/${uriPath}`;
+};
+
 function transformCustomApplicationConfigToData(
   appConfig: JSONSchemaForCustomApplicationConfigurationFiles
 ): CustomApplicationData {
@@ -27,7 +42,10 @@ function transformCustomApplicationConfigToData(
     ],
     icon: appConfig.icon,
     mainMenuLink: appConfig.mainMenuLink,
-    submenuLinks: appConfig.submenuLinks,
+    submenuLinks: appConfig.submenuLinks.map((submenuLink) => ({
+      ...submenuLink,
+      uriPath: computeUriPath(submenuLink.uriPath, appConfig.entryPointUriPath),
+    })),
   };
 }
 
