08a689bb1861d41b2a6f573df91940d50aa28372
Nicola Molinari <nicola.molinari@commercetools.com>
2022-04-11 09:20:09 +0200

    fix(cli): do not use : char in command filenames (#2544)

---
+++ .changeset/nasty-keys-act.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-frontend/mc-scripts': patch
+---
+
+Fix problem with file name in Windows when name contains characters like `:`.
--- packages/mc-scripts/src/bin/cli.js
+++ packages/mc-scripts/src/bin/cli.js
@@ -119,7 +119,12 @@ const applicationDirectory = fs.realpathSync(process.cwd());
 
         // Get specific flag for this command.
         const commandArgs = getArgsForCommand(['dry-run']);
-        proxyCommand(command, { commandArgs });
+        proxyCommand(command, {
+          commandArgs,
+          // File names with `:` cause issues in Windows, therefore the file name is
+          // different from the command name.
+          fileName: 'config-sync',
+        });
         break;
       }
       default:
@@ -145,7 +150,7 @@ function getArgsForCommand(allowedFlags = []) {
   }, []);
 }
 
-function proxyCommand(fileName, { commandArgs, noExit } = {}) {
+function proxyCommand(commandName, { commandArgs, fileName, noExit } = {}) {
   // Load dotenv files into the process environment.
   // This is essentially what `dotenv-cli` does, but it's now built into this CLI.
   loadDotEnvFiles(flags);
@@ -153,7 +158,9 @@ function proxyCommand(fileName, { commandArgs, noExit } = {}) {
   // Spawn the actual command.
   const result = spawn.sync(
     'node',
-    [require.resolve(`../commands/${fileName}`)].concat(commandArgs),
+    [require.resolve(`../commands/${fileName || commandName}`)].concat(
+      commandArgs
+    ),
     { stdio: 'inherit' }
   );
 
@@ -162,13 +169,13 @@ function proxyCommand(fileName, { commandArgs, noExit } = {}) {
     switch (result.signal) {
       case 'SIGKILL': {
         console.log(
-          `The command ${fileName} failed because the process exited too early. This probably means the system ran out of memory or someone called "kill -9" on the process.`
+          `The command ${commandName} failed because the process exited too early. This probably means the system ran out of memory or someone called "kill -9" on the process.`
         );
         break;
       }
       case 'SIGTERM': {
         console.log(
-          `The command ${fileName} failed because the process exited too early. Someone might have called "kill" or "killall", or the system could be shutting down.`
+          `The command ${commandName} failed because the process exited too early. Someone might have called "kill" or "killall", or the system could be shutting down.`
         );
         break;
       }
