2b154fe66977c906a2b20de67c0a569b53e77b4f
Kacper Krzywiec <krzywiec@gmail.com>
2022-04-14 11:55:10 +0200

    refactor: delete unused components

--- application-templates/starter/src/components/read-only-message/messages.js
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  readOnly: {
-    id: 'Messages.readOnly',
-    description: 'The title for read only access',
-    defaultMessage: 'View access only',
-  },
-});
