5d3b02a688c22335691a3d6cf76a221b52f42890
Nicola Molinari <nicola.molinari@commercetools.com>
2022-04-25 11:54:40 +0200

    fix: use preval macro for preloading static file on build

--- packages/mc-html-template/src/load-html-scripts.js
+++ packages/mc-html-template/src/load-html-scripts.js
@@ -1,4 +1,10 @@
-/* eslint-disable no-console */
+// @preval
+/**
+ * https://github.com/kentcdodds/babel-plugin-preval#preval-file-comment--preval
+ * NOTE: This file is pre-evaluated during build time, using `babel-plugin-preval`.
+ * This is ok as the loaded files are static anyway and it prevents possible
+ * loading issues when files are required through Webpack own context.
+ */
 const fs = require('fs');
 const path = require('path');
 const uglify = require('uglify-js');
