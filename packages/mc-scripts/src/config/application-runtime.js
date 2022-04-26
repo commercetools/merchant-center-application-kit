cb0bd5ffceb767320dc0c3ba7ec338dab7ec3021
Nicola Molinari <nicola.molinari@commercetools.com>
2022-04-26 17:24:11 +0200

    fix(mc-scripts): revert unnecessary change

--- packages/mc-scripts/src/config/application-runtime.js
+++ packages/mc-scripts/src/config/application-runtime.js
@@ -2,17 +2,5 @@
 // Dynamically inject webpack `publicPath`, for resolving assets locations.
 // https://webpack.js.org/guides/public-path/#on-the-fly
 // https://webpack.js.org/configuration/output/#output-publicpath
-if (process.env.NODE_ENV !== 'test') {
-  const basePath = `${window.app.cdnUrl.replace(/\/$/, '')}/`;
-
-  // Webpack
-  __webpack_public_path__ = basePath;
-
-  // Vite
-  window.__dynamicImportHandler__ = function (importer) {
-    return basePath + importer;
-  };
-  window.__dynamicImportPreload__ = function (preloads) {
-    return preloads.map((preload) => basePath + preload);
-  };
-}
+if (process.env.NODE_ENV !== 'test')
+  __webpack_public_path__ = `${window.app.cdnUrl.replace(/\/$/, '')}/`;
