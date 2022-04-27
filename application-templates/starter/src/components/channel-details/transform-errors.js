0d4e192954fd49aa6a5476f30fe9b11caca77b43
Kacper Krzywiec <krzywiec@gmail.com>
2022-04-27 08:12:26 +0200

    refactor: transformErrors function

--- application-templates/starter/src/components/channel-details/transform-errors.js
+++ application-templates/starter/src/components/channel-details/transform-errors.js
@@ -2,14 +2,6 @@ import omitEmpty from 'omit-empty-es';
 
 const DUPLICATE_FIELD_ERROR_CODE = 'DuplicateField';
 
-const setFieldValidationError = ({
-  formErrors,
-  fieldName,
-  validationError,
-}) => {
-  formErrors[fieldName] = { [validationError]: true };
-};
-
 /**
  * TransformedErrors: { unmappedErrors: [], formErrors: {} }
  *
@@ -22,23 +14,23 @@ export const transformErrors = (graphQlErrors) => {
     ? graphQlErrors
     : [graphQlErrors];
 
-  const formErrors = {}; // will be mappped to form field error messages
-  const unmappedErrors = []; // will result in dispatching `showApiErrorNotification`
+  const { formErrors, unmappedErrors } = errorsToMap.reduce(
+    (transformedErrors, graphQlError) => {
+      const errorCode = graphQlError?.extensions?.code ?? graphQlError.code;
+      const fieldName = graphQlError?.extensions?.field ?? graphQlError.field;
 
-  errorsToMap.forEach((graphQlError) => {
-    const errorCode = graphQlError?.extensions?.code ?? graphQlError.code;
-    const fieldName = graphQlError?.extensions?.field ?? graphQlError.field;
-
-    if (errorCode === DUPLICATE_FIELD_ERROR_CODE) {
-      setFieldValidationError({
-        formErrors,
-        fieldName,
-        validationError: 'duplicate',
-      });
-    } else {
-      unmappedErrors.push(graphQlError);
+      if (errorCode === DUPLICATE_FIELD_ERROR_CODE) {
+        transformedErrors.formErrors[fieldName] = { duplicate: true };
+      } else {
+        transformedErrors.unmappedErrors.push(graphQlError);
+      }
+      return transformedErrors;
+    },
+    {
+      formErrors: {}, // will be mappped to form field error messages
+      unmappedErrors: [], // will result in dispatching `showApiErrorNotification`
     }
-  });
+  );
 
   return {
     formErrors: omitEmpty(formErrors),
