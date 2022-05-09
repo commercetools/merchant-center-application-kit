e8d03c77d9b35fa4aa1d86272d0dccfca686ce14
Nicola Molinari <nicola.molinari@commercetools.com>
2022-05-09 22:05:58 +0200

    refactor(config): load the config as a JS module instead of parsing it as JSON

---
+++ .changeset/calm-apricots-carry.md
@@ -0,0 +1,6 @@
+---
+'@commercetools-frontend/application-config': patch
+---
+
+Internally load the Custom Application config as a JS module instead of parsing it as JSON.
+This is to unlock some future work.
--- packages/application-config/src/load-config.ts
+++ packages/application-config/src/load-config.ts
@@ -1,10 +1,7 @@
-import type { JSONSchemaForCustomApplicationConfigurationFiles } from './schema';
-import type { LoaderSync } from 'cosmiconfig';
-
 import fs from 'fs';
 import path from 'path';
-import { execFileSync } from 'child_process';
-import { cosmiconfigSync, defaultLoaders } from 'cosmiconfig';
+import { cosmiconfigSync, defaultLoaders, type LoaderSync } from 'cosmiconfig';
+import type { JSONSchemaForCustomApplicationConfigurationFiles } from './schema';
 import { MissingOrInvalidConfigError } from './errors';
 
 // Helper function to find the package root path from the current location,
@@ -23,18 +20,15 @@ const loadJsModule: LoaderSync = (filePath) => {
     // Start from the parent folder
     path.join(__dirname, '..')
   );
-  // Load the JS module using a child process. This is primarly to avoid
+  // Load the JS module using a separate module loader. This is primarly to avoid
   // unwanted behaviors using `@babel/register` in the main process.
   // The loader script does the actual `require` of the given `filePath`
   // and uses `@babel/register` to correctly parse and execute the file.
-  // The "required module output" is then written into `stdout` and parsed
-  // as JSON.
-  const output = execFileSync(
-    'node',
-    [path.join(packageRootPath, 'scripts/load-js-module.js'), filePath],
-    { encoding: 'utf8' }
-  );
-  return JSON.parse(output);
+  const requireModule = require(path.join(
+    packageRootPath,
+    'loaders/load-js-module'
+  ));
+  return requireModule(filePath);
 };
 
 const moduleName = 'custom-application-config';
