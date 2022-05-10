59929db56c05f216d20b42a3e80ee6e7c902f0a6
Nicola Molinari <nicola.molinari@commercetools.com>
2022-05-10 10:13:40 +0200

    test: try fixing error on CI

--- packages/application-config/test/load-config.spec.js
+++ packages/application-config/test/load-config.spec.js
@@ -2,6 +2,10 @@ import path from 'path';
 import loadConfig from '../src/load-config';
 import { validateConfig } from '../src/validations';
 
+// Try to fix an issue on CI
+// ReferenceError: You are trying to `import` a file after the Jest environment has been torn down. From packages/application-config/test/load-config.spec.js
+jest.useFakeTimers();
+
 describe.each`
   extension | fixtureApp
   ${'.js'}  | ${'app-js'}
