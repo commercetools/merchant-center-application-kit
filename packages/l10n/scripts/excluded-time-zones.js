396eb460598cdd1a8712135a944a7eaa0b91587c
Byron Wall <byron.wall@commercetools.com>
2022-06-14 14:51:35 -0400

    feat(timezone list update): remove excluded timezones in favor of fallback map

--- packages/l10n/scripts/excluded-time-zones.js
+++ packages/l10n/scripts/excluded-time-zones.js
@@ -1,12 +1,2 @@
-const EXCLUDED_TIME_ZONES = [
-  'America/Coral_Harbour',
-  'Antarctica/Casey',
-  'CST6CDT',
-  'EET',
-  'Etc/GMT-3',
-  'Etc/GMT+12',
-  'MET',
-  'MST7MDT',
-  'PST8PDT',
-];
+const EXCLUDED_TIME_ZONES = [];
 module.exports = EXCLUDED_TIME_ZONES;
