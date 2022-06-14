d54159348226b081650ed5c565c8db417791865c
Byron Wall <byron.wall@commercetools.com>
2022-06-14 07:51:21 -0400

    feat(timezone list update): initial fallback map complete

--- packages/l10n/scripts/excluded-time-zones.js
+++ packages/l10n/scripts/excluded-time-zones.js
@@ -1,13 +1,6 @@
 const EXCLUDED_TIME_ZONES = [
-  'Africa/Addis_Ababa',
-  'Africa/Dar_es_Salaam',
-  'Africa/Djibouti',
-  'Africa/Kampala',
-  'Africa/Mogadishu',
-  'Africa/Nairobi',
   'America/Coral_Harbour',
   'Antarctica/Casey',
-  'Antarctica/Macquarie',
   'CST6CDT',
   'EET',
   'Etc/GMT-3',
