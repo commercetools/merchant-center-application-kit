2143907cffbd7213728a613b09de6caba8c98507
Nicola Molinari <nicola.molinari@commercetools.com>
2022-07-13 22:29:40 +0200

    refactor(config): simplify schema and validate entry point separately

--- packages/application-config/src/validations.ts
+++ packages/application-config/src/validations.ts
@@ -1,12 +1,14 @@
-import type { JSONSchemaForCustomApplicationConfigurationFiles } from './schema';
 import Ajv, { type ErrorObject } from 'ajv';
 import schemaJson from '../schema.json';
+import type { JSONSchemaForCustomApplicationConfigurationFiles } from './schema';
 
 type ErrorAdditionalProperty = ErrorObject<
   'additionalProperty',
   { additionalProperty: string }
 >;
 type ErrorEnum = ErrorObject<'enum', { allowedValues: string[] }>;
+
+const entryPointUriPathRegex = /^[^-_#]([0-9a-z]|[-_](?![-_])){0,62}[^-_#]$/g;
 
 const ajv = new Ajv({ strict: true, useDefaults: true });
 
@@ -43,6 +45,16 @@ export const validateConfig = (
   const valid = validate(config);
   if (!valid) {
     throw new Error(printErrors(validate.errors));
+  }
+};
+
+export const validateEntryPointUriPath = (
+  config: JSONSchemaForCustomApplicationConfigurationFiles
+) => {
+  if (!config.entryPointUriPath.match(entryPointUriPathRegex)) {
+    throw new Error(
+      'Invalid "entryPointUriPath". The value may be between 2 and 64 characters and only contain alphanumeric lowercase characters, non-consecutive underscores and hyphens. Leading and trailing underscore and hyphens are also not allowed.'
+    );
   }
 };
 
