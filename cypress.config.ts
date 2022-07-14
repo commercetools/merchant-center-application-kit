c4c8b77181f287881d150b8b4e95c9fa09c5a331
Nicola Molinari <nicola.molinari@commercetools.com>
2022-07-14 21:24:55 +0200

    chore: update cypress to v10

---
+++ cypress.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    retries: 1,
+    supportFile: 'cypress/support/e2e.ts',
+    async setupNodeEvents(on, cypressConfig) {
+      // Load the config
+      if (!process.env.CI) {
+        const envPath = path.join(__dirname, 'cypress/.env');
+        console.log('Loading environment variables from', envPath);
+        const dotenv = await import('dotenv');
+        dotenv.config({ path: envPath });
+      }
+
+      const { customApplicationConfig } = await import(
+        '@commercetools-frontend/cypress/task'
+      );
+
+      on('task', {
+        customApplicationConfig,
+      });
+
+      return Object.assign({}, cypressConfig, {
+        env: Object.assign({}, cypressConfig.env, {
+          LOGIN_USER: process.env.CYPRESS_LOGIN_USER,
+          LOGIN_PASSWORD: process.env.CYPRESS_LOGIN_PASSWORD,
+          PROJECT_KEY: process.env.CYPRESS_PROJECT_KEY,
+        }),
+      });
+    },
+  },
+});
