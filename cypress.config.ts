d0a488e8cc2ba865426d571c20b33fe0109a47ad
Carlos Martinez <carlos.martines@commercetools.com>
2022-08-18 10:13:28 +0200

    fix(cypress): update cypress config (remove unnecessary file)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
@@ -6,9 +7,30 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      // eslint-disable-next-line import/extensions
-      return require('./cypress/plugins/index.js')(on, config);
+    async setupNodeEvents(on, cypressConfig) {
+      // Load the config
+      if (!process.env.CI) {
+        const envPath = path.join(__dirname, 'cypress/.env');
+        console.log('Loading environment variables from', envPath);
+        const dotenv = await import('dotenv');
+        dotenv.config({ path: envPath });
+      }
+
+      const { customApplicationConfig } = await import(
+        '@commercetools-frontend/cypress/task'
+      );
+
+      on('task', {
+        customApplicationConfig,
+      });
+
+      return Object.assign({}, cypressConfig, {
+        env: Object.assign({}, cypressConfig.env, {
+          LOGIN_USER: process.env.CYPRESS_LOGIN_USER,
+          LOGIN_PASSWORD: process.env.CYPRESS_LOGIN_PASSWORD,
+          PROJECT_KEY: process.env.CYPRESS_PROJECT_KEY,
+        }),
+      });
     },
     baseUrl: 'http://localhost:3001',
   },
