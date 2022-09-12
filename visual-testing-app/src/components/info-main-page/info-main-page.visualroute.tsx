b803b587565a89bccbe2aa4ed73833555556f2cc
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2022-09-12 12:31:39 +0200

    refactor(main-page): update vrt

--- visual-testing-app/src/components/info-main-page/info-main-page.visualroute.tsx
+++ visual-testing-app/src/components/info-main-page/info-main-page.visualroute.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from 'react';
 import { InfoMainPage } from '@commercetools-frontend/application-components';
 import TextField from '@commercetools-uikit/text-field';
 import Spacings from '@commercetools-uikit/spacings';
@@ -5,31 +6,37 @@ import { Suite, Spec } from '../../test-utils';
 
 export const routePath = '/info-main-page';
 
-const Content = () => (
-  <Spacings.Stack scale="l">
-    <Spacings.Inline scale="l">
-      <TextField title="First Name" value="foo" onChange={() => {}} />
-      <TextField title="Last Name" value="foo" onChange={() => {}} />
-    </Spacings.Inline>
-    <TextField title="Email Address" value="foo" onChange={() => {}} />
-    <TextField title="Business Role" value="foo" onChange={() => {}} />
-  </Spacings.Stack>
+type InfoMainPageContainerProps = {
+  customTitleRow?: ReactNode;
+};
+
+const InfoMainPageContainer = (props: InfoMainPageContainerProps) => (
+  <div style={{ height: '750px' }}>
+    <InfoMainPage
+      title="Lorem Ipsum"
+      subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      {...props}
+    >
+      <Spacings.Stack scale="l">
+        <Spacings.Inline scale="l">
+          <TextField title="First Name" value="foo" onChange={() => {}} />
+          <TextField title="Last Name" value="foo" onChange={() => {}} />
+        </Spacings.Inline>
+        <TextField title="Email Address" value="foo" onChange={() => {}} />
+        <TextField title="Business Role" value="foo" onChange={() => {}} />
+      </Spacings.Stack>
+    </InfoMainPage>
+  </div>
 );
+InfoMainPageContainer.displayName = 'InfoMainPageContainer';
 
 export const Component = () => (
   <Suite>
     <Spec label="InfoMainPage">
-      <InfoMainPage
-        title="Lorem Ipsum"
-        subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      >
-        <Content />
-      </InfoMainPage>
+      <InfoMainPageContainer />
     </Spec>
     <Spec label="InfoMainPage with customTitleRow">
-      <InfoMainPage customTitleRow={<div>John Doe</div>}>
-        <Content />
-      </InfoMainPage>
+      <InfoMainPageContainer customTitleRow={<h2>John Doe</h2>} />
     </Spec>
   </Suite>
 );
