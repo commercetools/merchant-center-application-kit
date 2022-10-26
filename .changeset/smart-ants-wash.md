836c86aa187125e0670b2248619a1cbaa094bec2
Nicola Molinari <nicola.molinari@commercetools.com>
2022-10-26 15:38:51 +0200

    refactor(config): expose `sanitizeSvg` function, fix diffing logic (#2862)
    * refactor(config): expose sanitizeSvg function, fix diffing logic
    
    * fix: tests
    
    * fix: export

---
+++ .changeset/smart-ants-wash.md
@@ -0,0 +1,6 @@
+---
+'@commercetools-frontend/application-config': patch
+'@commercetools-frontend/mc-scripts': patch
+---
+
+Sanitize SVG icon only when comparing diffs, as the icon is sanitized in the API. Expose the `sanitizeSvg` logic from the `@commercetools-frontend/application-config`.
--- packages/application-config/src/sanitize-svg.ts
+++ packages/application-config/src/sanitize-svg.ts
@@ -8,9 +8,10 @@ const DOMPurify = createDOMPurify(jsdom.window);
 export default function sanitizeSvg(data: string) {
   return DOMPurify.sanitize(data, {
     USE_PROFILES: { svg: true },
+    RETURN_DOM: true,
     FORBID_ATTR: [
       // To avoid injection by using `style="filter:url(\"data:image/svg+xml,<svg`
       'style',
     ],
-  });
+  }).innerHTML;
 }
