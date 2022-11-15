89bbf3f4470b6c3cf11bec5fc430f711a1bcffe9
Nicola Molinari <nicola.molinari@commercetools.com>
2022-11-15 13:10:09 +0100

    fix(components): code split public-page-layout

--- packages/application-components/src/components/public-page-layout/public-background.js
+++ packages/application-components/src/components/public-page-layout/public-background.js
@@ -5,4 +5,3 @@ const pngPath = path.join(__dirname, 'public-background.png');
 const base64String = fs.readFileSync(pngPath).toString('base64');
 
 module.exports = base64String;
-exports.default = base64String;
