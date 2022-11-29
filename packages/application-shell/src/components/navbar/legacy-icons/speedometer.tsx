51b966b8b03568f822b3fe31347bb93a1a98bbf5
Carlos Martinez <carlos.martines@commercetools.com>
2022-11-29 15:30:49 +0100

    refactor(application-shell): get legacy app icons back

---
+++ packages/application-shell/src/components/navbar/legacy-icons/speedometer.tsx
@@ -0,0 +1,6 @@
+import { SpeedometerIcon } from '@commercetools-uikit/icons';
+
+// @ts-ignore
+const Icon = (props) => <SpeedometerIcon {...props} />;
+
+export default Icon;
---
+++ packages/application-shell/src/components/navbar/legacy-icons/tag-multi.tsx
@@ -0,0 +1,6 @@
+import { TagMultiIcon } from '@commercetools-uikit/icons';
+
+// @ts-ignore
+const Icon = (props) => <TagMultiIcon {...props} />;
+
+export default Icon;
