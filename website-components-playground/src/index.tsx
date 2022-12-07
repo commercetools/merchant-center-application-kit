c87e17626208baf40637be2a71d32cf43e6e1129
Nicola Molinari <nicola.molinari@commercetools.com>
2022-12-07 19:31:38 +0100

    fix(vrt): theme setup

--- website-components-playground/src/index.tsx
+++ website-components-playground/src/index.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from '@commercetools-uikit/design-system';
+import { themesOverrides } from '@commercetools-frontend/application-components';
 import Application from './application';
+
+const theme = process.env.NODE_ENV === 'production' ? 'default' : 'test';
 
 ReactDOM.render(
   <Suspense fallback={'Loading...'}>
     <>
-      <ThemeProvider />
+      <ThemeProvider theme={theme} themeOverrides={themesOverrides[theme]} />
       <Application />
     </>
   </Suspense>,
