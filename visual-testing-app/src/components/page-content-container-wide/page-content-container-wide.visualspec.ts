1406a35202d1cad118fc97c976aeb377c9d09c2b
Carlos Martinez <carlos.martines@commercetools.com>
2023-03-08 11:23:00 +0100

    fix(visual-testing-app): fix page-content-container-wide component tests

--- visual-testing-app/src/components/page-content-container-wide/page-content-container-wide.visualspec.ts
+++ visual-testing-app/src/components/page-content-container-wide/page-content-container-wide.visualspec.ts
@@ -17,39 +17,39 @@ describe('PageContentContainerWide', () => {
       'PageContentContainerWide_singleColumnSeveralChildren'
     );
   });
-  it('Two columns half small gap', async () => {
+  it('Two columns half big gap', async () => {
     await page.goto(`${HOST}/page-content-container-wide/two-columns-half`);
+    await expect(page).toMatch('Page content container wide');
+    await percySnapshot(page, 'PageContentContainerWide_twoColumnsHalfBigGap');
+  });
+  it('Two columns half small gap', async () => {
+    await page.goto(
+      `${HOST}/page-content-container-wide/two-columns-half-small-gap`
+    );
     await expect(page).toMatch('Page content container wide');
     await percySnapshot(
       page,
       'PageContentContainerWide_twoColumnsHalfSmallGap'
     );
   });
-  it('Two columns half big gap', async () => {
-    await page.goto(
-      `${HOST}/page-content-container-wide/two-columns-half-big-gap`
-    );
-    await expect(page).toMatch('Page content container wide');
-    await percySnapshot(page, 'PageContentContainerWide_twoColumnsHalfBigGap');
-  });
-  it('Two columns two thirds small gap', async () => {
+  it('Two columns two thirds big gap', async () => {
     await page.goto(
       `${HOST}/page-content-container-wide/two-columns-two-thirds`
     );
     await expect(page).toMatch('Page content container wide');
     await percySnapshot(
       page,
-      'PageContentContainerWide_twoColumnsTwoThirdsSmallGap'
+      'PageContentContainerWide_twoColumnsTwoThirdsBigGap'
     );
   });
-  it('Two columns two thirds big gap', async () => {
+  it('Two columns two thirds small gap', async () => {
     await page.goto(
-      `${HOST}/page-content-container-wide/two-columns-two-thirds-big-gap`
+      `${HOST}/page-content-container-wide/two-columns-two-thirds-small-gap`
     );
     await expect(page).toMatch('Page content container wide');
     await percySnapshot(
       page,
-      'PageContentContainerWide_twoColumnsTwoThirdsBigGap'
+      'PageContentContainerWide_twoColumnsTwoThirdsSmallGap'
     );
   });
 });
