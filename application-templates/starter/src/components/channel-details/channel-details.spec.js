0dfca6d1ee339d4c920ee2ef2c0a4a5ee6085cc5
Nicola Molinari <nicola.molinari@commercetools.com>
2023-03-08 09:15:47 +0100

    refactor(templates): refine required dependencies (#2980)
    * refactor(templates): refine required dependencies
    
    * fix(templates): mocks
    
    * chore(template): ignore public folder

--- application-templates/starter/src/components/channel-details/channel-details.spec.js
+++ application-templates/starter/src/components/channel-details/channel-details.spec.js
@@ -7,14 +7,11 @@ import {
   within,
   mapResourceAccessToAppliedPermissions,
 } from '@commercetools-frontend/application-shell/test-utils';
-import { reportErrorToSentry } from '@commercetools-frontend/sentry';
 import { buildGraphqlList } from '@commercetools-test-data/core';
 import * as Channel from '@commercetools-test-data/channel';
 import { LocalizedString } from '@commercetools-test-data/commons';
 import { renderApplicationWithRoutesAndRedux } from '../../test-utils';
 import { entryPointUriPath, PERMISSIONS } from '../../constants';
-
-jest.mock('@commercetools-frontend/sentry');
 
 const mockServer = setupServer();
 afterEach(() => mockServer.resetHandlers());
@@ -287,6 +284,9 @@ describe('notifications', () => {
     );
   });
   it('should display an error notification if an update resulted in an unmapped error', async () => {
+    // Mock error log
+    jest.spyOn(console, 'error').mockImplementation();
+
     useMockServerHandlers(
       fetchChannelDetailsQueryHandler,
       updateChannelDetailsHandlerWithARandomError
@@ -306,7 +306,5 @@ describe('notifications', () => {
 
     const notification = await screen.findByRole('alertdialog');
     within(notification).getByText(/some fake error message/i);
-
-    expect(reportErrorToSentry).toHaveBeenCalled();
   });
 });
