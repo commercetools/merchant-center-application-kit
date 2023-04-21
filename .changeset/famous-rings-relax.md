2426a3849ac9f3e581e0d3e06df672391c5ce56e
Nicola Molinari <nicola.molinari@commercetools.com>
2023-04-21 11:14:25 +0200

    [Breaking] Upgrade `react-intl` to `v6` (#3029)
    * chore: upgrade react-intl to v6
    
    * chore: refresh deps and fix type errors
    
    * chore: update uikit to v16
    
    * docs: changeset

---
+++ .changeset/famous-rings-relax.md
@@ -0,0 +1,16 @@
+---
+'@commercetools-applications/merchant-center-template-starter-typescript': minor
+'@commercetools-frontend/application-components': minor
+'@commercetools-applications/merchant-center-template-starter': minor
+'@commercetools-website/components-playground': minor
+'@commercetools-frontend/react-notifications': minor
+'@commercetools-frontend/jest-preset-mc-app': minor
+'@commercetools-frontend/application-shell': minor
+'@commercetools-frontend/mc-scripts': minor
+'@commercetools-local/visual-testing-app': minor
+'@commercetools-frontend/i18n': minor
+'@commercetools-local/playground': minor
+'@commercetools-website/custom-applications': minor
+---
+
+Update UI Kit dependencies to v16
--- packages/application-components/src/components/page-not-found/page-not-found.tsx
+++ packages/application-components/src/components/page-not-found/page-not-found.tsx
@@ -1,4 +1,3 @@
-import { ReactChildren } from 'react';
 import { useIntl, FormattedMessage } from 'react-intl';
 import PageNotFoundIllustration from '@commercetools-frontend/assets/images/page-application-disabled.svg';
 import { SUPPORT_PORTAL_URL } from '@commercetools-frontend/constants';
@@ -6,23 +5,27 @@ import MaintenancePageLayout from '../maintenance-page-layout';
 import messages from './messages';
 
 // eslint-disable-next-line react/display-name
-const getLink = (msg: ReactChildren) => (
+const getLink = (msg: string) => (
   <a href={SUPPORT_PORTAL_URL} target="_blank" rel="noopener noreferrer">
     {msg}
   </a>
 );
 const PageNotFound = () => {
   const intl = useIntl();
-  const paragraph1Message = intl.formatMessage(messages.paragraph1, {
-    a: getLink,
-  });
 
   return (
     <MaintenancePageLayout
       imageSrc={PageNotFoundIllustration}
       title={<FormattedMessage {...messages.title} />}
       label={intl.formatMessage(messages.title)}
-      paragraph1={paragraph1Message}
+      paragraph1={
+        <FormattedMessage
+          {...messages.paragraph1}
+          values={{
+            a: getLink,
+          }}
+        />
+      }
     />
   );
 };
