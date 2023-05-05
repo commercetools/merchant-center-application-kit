b1c0d729bcd4e7951eccc8779d0ad9ec8ce891f3
tdeekens <tobias.deekens@commercetools.com>
2023-05-05 16:56:12 +0200

    refactor: add ability to silence redesign announcement

--- packages/application-shell/src/components/theme-switcher/theme-switcher.tsx
+++ packages/application-shell/src/components/theme-switcher/theme-switcher.tsx
@@ -1,6 +1,7 @@
 import { useState, lazy } from 'react';
 import { useFeatureToggle } from '@flopflip/react-broadcast';
 import { themesOverrides } from '@commercetools-frontend/application-components';
+import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
 import { ThemeProvider } from '@commercetools-uikit/design-system';
 import { STORAGE_KEYS } from '../../constants';
 import { UI_REDESIGN } from '../../feature-toggles';
@@ -8,8 +9,21 @@ import { UI_REDESIGN } from '../../feature-toggles';
 const NewDesignReleaseInfoDialog = lazy(
   () => import('./new-design-release-info-dialog')
 );
+
+type TAdditionalEnvironmentValues = { disableRedesignAnnouncement: boolean };
+type TSelectedEnvironmentValues = boolean | undefined;
+function useDisableRedesignAnnouncement() {
+  const disableRedesignAnnouncement = useApplicationContext<
+    TSelectedEnvironmentValues,
+    TAdditionalEnvironmentValues
+  >((context) => context.environment.disableRedesignAnnouncement);
+
+  return Boolean(disableRedesignAnnouncement);
+}
+
 const ThemeSwitcher = () => {
   const isNewThemeEnabled = useFeatureToggle(UI_REDESIGN);
+  const isRedesignAnnouncementDisabled = useDisableRedesignAnnouncement();
   const theme = isNewThemeEnabled ? 'test' : 'default';
   const [
     hasUserSeenNewDesignReleaseNotificationDialog,
@@ -31,7 +45,9 @@ const ThemeSwitcher = () => {
   return (
     <>
       <ThemeProvider theme={theme} themeOverrides={themesOverrides[theme]} />
-      {!hasUserSeenNewDesignReleaseNotificationDialog && isNewThemeEnabled ? (
+      {!isRedesignAnnouncementDisabled &&
+      !hasUserSeenNewDesignReleaseNotificationDialog &&
+      isNewThemeEnabled ? (
         <NewDesignReleaseInfoDialog onClose={handleCloseDialog} />
       ) : null}
     </>
