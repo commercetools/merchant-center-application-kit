1daa41402f71e2ca34de6bd656776686044b3b80
Nicola Molinari <nicola.molinari@commercetools.com>
2023-05-12 14:07:06 +0200

    fix: export EnhancedHistory type

--- packages/application-shell/src/components/application-shell-provider/utils.ts
+++ packages/application-shell/src/components/application-shell-provider/utils.ts
@@ -1,7 +1,9 @@
-import history from '@commercetools-frontend/browser-history';
+import history, {
+  type EnhancedHistory,
+} from '@commercetools-frontend/browser-history';
 import { getSupportedLocale } from '@commercetools-frontend/l10n';
 
-export const getBrowserHistory = () => history;
+export const getBrowserHistory = (): EnhancedHistory => history;
 
 export const getBrowserLocale = (win: Window) => {
   const browserLocale = win && win.navigator && win.navigator.language;
