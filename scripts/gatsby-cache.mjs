2c14d43bbfb196bbaae82f2df55c6bc6b8cf39f0
Yahia El Tai <yahiaeltai022@gmail.com>
2023-05-19 15:52:48 +0200

    chore: use cac instead of mri in gatsby-cache cli (#3090)
    * chore: use cac instead of mri in gatsby-cache cli
    
    * chore: run pnpm install

--- scripts/gatsby-cache.mjs
+++ scripts/gatsby-cache.mjs
@@ -18,35 +18,46 @@ import fs from 'fs';
 import path from 'path';
 import { findRootSync } from '@manypkg/find-root';
 import { getPackages } from '@manypkg/get-packages';
-import mri from 'mri';
+import { cac } from 'cac';
 
-const flags = mri(process.argv.slice(2), { alias: { help: ['h'] } });
-const commands = flags._;
+const cli = cac('gatsby-cache');
 
-if (commands.length === 0 || (flags.help && commands.length === 0)) {
-  console.log(`
-  Usage: node ./scripts/gatsby-cache.mjs [command]
+const commands = [
+  {
+    name: 'pre',
+    description:
+      "Extract each Gatsby website's specific {.cache,public} folders from the root directory to each workspace.",
+    action: async ({
+      globalWebsiteCachePath,
+      localWebsiteCachePath,
+      globalWebsitePublicPath,
+      localWebsitePublicPath,
+    }) => {
+      // Move global website cache to local cache folder
+      await moveFolderFromTo(globalWebsiteCachePath, localWebsiteCachePath);
 
-  Displays help information.
+      // Move global website public to local public folder
+      await moveFolderFromTo(globalWebsitePublicPath, localWebsitePublicPath);
+    },
+  },
+  {
+    name: 'post',
+    description:
+      "Extract each Gatsby website's specific {.cache,public} folders from each workspace to the root directory.",
+    action: async ({
+      globalWebsiteCachePath,
+      localWebsiteCachePath,
+      globalWebsitePublicPath,
+      localWebsitePublicPath,
+    }) => {
+      // Move local website cache to global cache folder
+      await moveFolderFromTo(localWebsiteCachePath, globalWebsiteCachePath);
 
-  Commands:
-
-  pre     Extract each Gatsby website's specific {.cache,public} folders from the root directory to each workspace.
-  post    Extract each Gatsby website's specific {.cache,public} folders from each workspace to the root directory.
-  `);
-  process.exit(0);
-}
-
-const [command] = commands;
-switch (command) {
-  case 'pre':
-  case 'post':
-    break;
-  default:
-    throw new Error(
-      `Missing or unsupported command "${command}". Supported commands are "pre" or "post".`
-    );
-}
+      // Move local website public to global public folder
+      await moveFolderFromTo(localWebsitePublicPath, globalWebsitePublicPath);
+    },
+  },
+];
 
 const workspaceRoot = findRootSync(process.cwd());
 const globalPublicPath = path.join(workspaceRoot.rootDir, 'public');
@@ -74,57 +85,53 @@ const run = async () => {
   await fs.promises.mkdir(globalPublicPath, { recursive: true });
   await fs.promises.mkdir(globalCachePath, { recursive: true });
 
-  await Promise.all(
-    packages
-      .filter((workspace) =>
-        gatsbyWebsites.includes(workspace.packageJson.name)
-      )
-      .map(async (workspace) => {
-        const namespaceKey = path.basename(workspace.dir);
+  // Default command
+  cli
+    .command('')
+    .usage("\n\n  Manage Gatsby website's specific {.cache,public} folders.")
+    .action(cli.outputHelp);
 
-        const globalWebsiteCachePath = path.join(globalCachePath, namespaceKey);
-        const localWebsiteCachePath = path.join(workspace.dir, '.cache');
+  commands.forEach((command) => {
+    cli
+      .command(command.name, command.description)
+      .usage(`${command.name} \n\n ${command.description}`)
+      .action(async () => {
+        await Promise.all(
+          packages
+            .filter((workspace) =>
+              gatsbyWebsites.includes(workspace.packageJson.name)
+            )
+            .map(async (workspace) => {
+              const namespaceKey = path.basename(workspace.dir);
 
-        const globalWebsitePublicPath = path.join(
-          globalPublicPath,
-          namespaceKey
+              const globalWebsiteCachePath = path.join(
+                globalCachePath,
+                namespaceKey
+              );
+              const localWebsiteCachePath = path.join(workspace.dir, '.cache');
+
+              const globalWebsitePublicPath = path.join(
+                globalPublicPath,
+                namespaceKey
+              );
+              const localWebsitePublicPath = path.join(workspace.dir, 'public');
+
+              command.action({
+                globalWebsiteCachePath,
+                localWebsiteCachePath,
+                globalWebsitePublicPath,
+                localWebsitePublicPath,
+              });
+            })
         );
-        const localWebsitePublicPath = path.join(workspace.dir, 'public');
+      });
+  });
 
-        switch (command) {
-          case 'pre': {
-            // Move global website cache to local cache folder
-            await moveFolderFromTo(
-              globalWebsiteCachePath,
-              localWebsiteCachePath
-            );
+  cli.help();
 
-            // Move global website public to local public folder
-            await moveFolderFromTo(
-              globalWebsitePublicPath,
-              localWebsitePublicPath
-            );
-            break;
-          }
-          case 'post': {
-            // Move local website cache to global cache folder
-            await moveFolderFromTo(
-              localWebsiteCachePath,
-              globalWebsiteCachePath
-            );
+  cli.parse(process.argv, { run: false });
 
-            // Move local website public to global public folder
-            await moveFolderFromTo(
-              localWebsitePublicPath,
-              globalWebsitePublicPath
-            );
-            break;
-          }
-          default:
-            break;
-        }
-      })
-  );
+  await cli.runMatchedCommand();
 };
 
 run().catch((error) => {
