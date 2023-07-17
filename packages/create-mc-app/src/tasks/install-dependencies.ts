5f4c47f8f98e798ed24ce33b66130a273fa2e25a
Nicola Molinari <nicola.molinari@commercetools.com>
2023-07-17 17:44:49 +0200

    fix(create-mc-app): run npm install cmd with --legacy-peer-deps option (#3138)

--- packages/create-mc-app/src/tasks/install-dependencies.ts
+++ packages/create-mc-app/src/tasks/install-dependencies.ts
@@ -1,17 +1,17 @@
 import execa from 'execa';
 import type { ListrTask } from 'listr2';
 import type { TCliTaskOptions } from '../types';
-import { getPreferredPackageManager } from '../utils';
+import { getInstallCommand } from '../utils';
 
 function installDependencies(options: TCliTaskOptions): ListrTask {
   return {
     title: 'Installing dependencies (this might take a while)',
     task: () => {
-      const packageManager = getPreferredPackageManager(options);
+      const installCommand = getInstallCommand(options);
 
       // TODO: we could check for min yarn/npm versions
       // See https://github.com/facebook/create-react-app/blob/0f4781e8507249ce29a9ac1409fece67c1a53c38/packages/create-react-app/createReactApp.js#L225-L254
-      return execa(packageManager, ['install'], {
+      return execa.command(installCommand, {
         cwd: options.projectDirectoryPath,
         encoding: 'utf-8',
       });
