62e97acac5fafddc1383e7b8d6e35e9931d46eb0
Kacper Krzywiec <krzywiec@gmail.com>
2023-07-18 13:22:42 +0200

    fix(app-shell): send teamId header

---
+++ .changeset/tiny-eels-tan.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-frontend/application-shell': patch
+---
+
+fix: send `teamId` as GraphQL request header in Custom Application local development
--- packages/application-shell/src/utils/select-team-id-from-storage/select-team-id-from-storage.ts
+++ packages/application-shell/src/utils/select-team-id-from-storage/select-team-id-from-storage.ts
@@ -1,8 +1,12 @@
+import type { ApplicationWindow } from '@commercetools-frontend/constants';
 import { STORAGE_KEYS } from '../../constants';
+
+declare let window: ApplicationWindow;
 
 // Attempt to load the `teamId` from sessionStorage
 export default function selectTeamIdFromStorage() {
   return (
-    window.sessionStorage.getItem(STORAGE_KEYS.ACTIVE_TEAM_ID) || undefined
+    window.sessionStorage.getItem(STORAGE_KEYS.ACTIVE_TEAM_ID) ||
+    window.app.__DEVELOPMENT__?.oidc?.teamId
   );
 }
