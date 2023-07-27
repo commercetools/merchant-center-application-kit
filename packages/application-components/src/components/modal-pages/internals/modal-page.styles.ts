0c90dcb4f3b671e59b3cddd4fd3545ef6a0a62cc
Carlos Martinez <carlos.martines@commercetools.com>
2023-07-27 09:56:45 +0200

    refactor(application-components): adjust custom-panel small size width

--- packages/application-components/src/components/modal-pages/internals/modal-page.styles.ts
+++ packages/application-components/src/components/modal-pages/internals/modal-page.styles.ts
@@ -13,7 +13,7 @@ export const getContainerStyles = (_props: StyleProps): SerializedStyles => css`
   top: 0;
   right: 0;
   height: 100%;
-  width: ${_props.size === 'small' ? '562px !important' : '100%'};
+  width: ${_props.size === 'small' ? '600px !important' : '100%'};
   display: flex;
   flex-direction: column;
   background-color: ${customProperties.colorSurface};
