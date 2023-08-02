ff7cd58f13b489953cc319b5bf8d8e8956d363a9
Kacper Krzywiec <krzywiec@gmail.com>
2023-08-02 15:59:02 +0200

    chore: downgrade vite

--- .changeset/popular-trainers-sparkle.md
+++ .changeset/popular-trainers-sparkle.md
@@ -1,5 +1,7 @@
 ---
+'@commercetools-website/components-playground': patch
 '@commercetools-frontend/mc-scripts': patch
+'@commercetools-local/visual-testing-app': patch
 ---
 
-Fix playground in dev mode
+Fix playground in dev mode by downgrading `vite` to `v4.1.4`
--- packages/mc-scripts/src/commands/start-vite.ts
+++ packages/mc-scripts/src/commands/start-vite.ts
@@ -46,9 +46,6 @@ async function run() {
       pluginSvgr(),
       pluginCustomApplication(applicationConfig),
     ],
-    optimizeDeps: {
-      force: true, // based on https://github.com/storybookjs/storybook/issues/22253#issuecomment-1661092137 TODO: remove when the issue with vite is resolved
-    },
   });
   await server.listen();
 
