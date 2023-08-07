9721f4f70f99513fc8e3f37d594f936104c3d2ae
Carlos Martinez <carlos.martines@commercetools.com>
2023-08-07 11:56:22 +0200

    refactor(playground): load an actual demo custom view

--- playground/custom-application-config.mjs
+++ playground/custom-application-config.mjs
@@ -31,6 +31,7 @@ const config = {
   headers: {
     csp: {
       'connect-src': ['${env:HOST_GCP_STAGING}'],
+      'frame-src': ['${env:LOCAL_HOST}'],
     },
   },
   icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
