eaa39524d4678f0c563a135048502b03851f6c46
Kacper Krzywiec <krzywiec@gmail.com>
2023-08-10 14:57:41 +0200

    refactor: revert isloading

--- packages/application-shell/src/components/navbar/navbar.tsx
+++ packages/application-shell/src/components/navbar/navbar.tsx
@@ -267,16 +267,15 @@ const NavBar = (props: TNavbarProps) => {
     [props.project]
   );
 
-  // TODO: uncomment before merging
-  // if (props.isLoading) {
-  // Render the loading navbar as long as all the data
-  // hasn't been loaded, or if the project does not exist.
-  return props.isNewNavigationEnabled ? (
-    <NavBarSkeleton isExpanded={isMenuOpen} />
-  ) : (
-    <LoadingNavBar />
-  );
-  // }
+  if (props.isLoading) {
+    // Render the loading navbar as long as all the data
+    // hasn't been loaded, or if the project does not exist.
+    return props.isNewNavigationEnabled ? (
+      <NavBarSkeleton isExpanded={isMenuOpen} />
+    ) : (
+      <LoadingNavBar />
+    );
+  }
 
   return (
     <NavBarLayout ref={navBarNode}>
