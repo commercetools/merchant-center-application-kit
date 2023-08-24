9c89929eb81cc1e5f912c79df34b79d5501a1c77
Carlos Martinez <carlos.martines@commercetools.com>
2023-08-24 16:44:18 +0200

    fix(website): update prebuild script

--- website/scripts/prebuild.sh
+++ website/scripts/prebuild.sh
@@ -4,4 +4,4 @@ set -e
 
 # Copy the custom application config `schema.json` to the website static assets.
 echo "Copying JSON schema for Custom Applications"
-cp ../packages/application-config/schema.json static/
+cp ../packages/application-config/custom-application.schema.json static/
