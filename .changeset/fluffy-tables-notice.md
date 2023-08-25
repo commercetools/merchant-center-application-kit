a6ebc4d1ba265bf21a1ee33f0311a3eb6fca20c0
Kacper Krzywiec <49066275+kark@users.noreply.github.com>
2023-08-25 15:47:09 +0200

    fix: styling issues with MainPageContent and Navbar (#3184)
    * revert: main page content styles
    
    * fix: navbars z-index
    
    * chore: add comment

---
+++ .changeset/fluffy-tables-notice.md
@@ -0,0 +1,7 @@
+---
+'@commercetools-frontend/application-components': patch
+'@commercetools-frontend/application-shell': patch
+---
+
+Reverting MainPageContent style change due to breaking sticky DataTable styles.
+Fix for NavBar submenu title's `z-index`.
--- packages/application-components/src/components/main-pages/internals/main-page.styles.ts
+++ packages/application-components/src/components/main-pages/internals/main-page.styles.ts
@@ -15,5 +15,6 @@ export const MainPageContent = styled.div`
   flex: 1;
   flex-basis: 0;
   overflow: auto;
-  padding: ${appKitDesignTokens.marginForPageContent};
+  // NOTE: do not change to "padding" as this breaks sticky DataTable styles
+  margin: ${appKitDesignTokens.marginForPageContent};
 `;
