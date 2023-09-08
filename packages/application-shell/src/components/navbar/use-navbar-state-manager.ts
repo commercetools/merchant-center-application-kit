c566aecfdc01a7d9d64a8cb5298298922f7d9a58
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2023-09-08 10:15:28 +0200

    chore: removed transitions and unnecessary margins

--- packages/application-shell/src/components/navbar/use-navbar-state-manager.ts
+++ packages/application-shell/src/components/navbar/use-navbar-state-manager.ts
@@ -247,7 +247,26 @@ const useNavbarStateManager = (props: HookProps) => {
     );
   }, [state.activeItemIndex, state.isMenuOpen]);
 
-  const allInternalApplicationsNavbarMenu = applicationsNavBarMenu || [];
+  // const allInternalApplicationsNavbarMenu = applicationsNavBarMenu || [];
+
+  const allInternalApplicationsNavbarMenu = [];
+
+  if (applicationsNavBarMenu) {
+    for (let i = 15; i > 0; i--) {
+      allInternalApplicationsNavbarMenu.push({
+        ...applicationsNavBarMenu[0],
+        key: applicationsNavBarMenu[0].key + i,
+        uriPath: applicationsNavBarMenu[0].uriPath + i,
+        submenu: applicationsNavBarMenu[0].submenu.map((submenu) => {
+          return {
+            ...submenu,
+            key: submenu.key + i,
+            uriPath: submenu.uriPath + i,
+          };
+        }),
+      });
+    }
+  }
 
   return {
     ...state,
