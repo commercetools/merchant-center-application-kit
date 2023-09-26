9fcbb833fe66acb573f4fa95868f59afa0f6928b
Ddouglasz <douglas.egiemeh@gmail.com>
2023-09-26 14:39:35 +0200

    refactor(left nav transition): remove unused transition

--- packages/application-shell/src/constants.ts
+++ packages/application-shell/src/constants.ts
@@ -6,7 +6,6 @@ export const DIMENSIONS = {
   navMenuItemHeight: '48px',
   newNavMenu: '80px',
   newNavMenuExpanded: '256px',
-  subMenuItemVerticalOffset: 48,
 } as const;
 
 export const WINDOW_SIZES = {
