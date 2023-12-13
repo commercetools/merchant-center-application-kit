5314f169e8af0ffcc820b57b818ac5495b92c45a
Byron Wall <87667330+ByronDWall@users.noreply.github.com>
2023-12-13 04:34:16 -0500

    fix(svgo/r config): update handling of prefixIds plugin to prefix svg ID's with a string hash of their filename (#3342)
    * fix(svgo/r config): update handling of prefixIds plugin to prefix svg ID's with a string hash of their filename in order to provide usable ID prefixes for SVG's in dev/prod webpack configs and vite-plugin-svgr in mc-scripts
    
    * fix(svgo/r config): remove unused 'svgoPrefixIdsCount' variable from prod webpack config and vite-plugin-svgr
    
    * fix(svgo/r config): remove string-hash dep in favor of nodejs builtin crypto library, use shake256 hash in order to limit the output length of the hash

--- packages/mc-scripts/src/config/create-webpack-config-for-production.ts
+++ packages/mc-scripts/src/config/create-webpack-config-for-production.ts
@@ -1,8 +1,10 @@
 // @ts-ignore
+import crypto from 'crypto';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import MomentLocalesPlugin from 'moment-locales-webpack-plugin';
+import type { XastElement, PluginInfo } from 'svgo';
 import TerserPlugin from 'terser-webpack-plugin';
 import webpack, {
   type WebpackPluginInstance,
@@ -20,8 +22,6 @@ import hasJsxRuntime from './has-jsx-runtime';
 import momentLocalesToKeep from /* preval */ './moment-locales';
 import paths from './paths';
 import vendorsToTranspile from './vendors-to-transpile';
-
-let svgoPrefixIdsCount = 0;
 
 const defaultToggleFlags: TWebpackConfigToggleFlagsForProduction = {
   // Allow to disable CSS extraction in case it's not necessary (e.g. for Storybook)
@@ -273,18 +273,38 @@ function createWebpackConfigForProduction(
                       },
                     },
                     // Avoid collisions with ids in other SVGs,
-                    // which was causing incorrect gradient directions
-                    // https://github.com/svg/svgo/issues/1746#issuecomment-1803600573
-                    //
-                    // Previously, this was a problem where unique ids
-                    // could not be generated since svgo@3
+                    // which was causing incorrect masking, gradient directions, etc
+                    // this is an ongoing issue with both SVGR and SVGO,
+                    // https://github.com/svg/svgo/issues/913#issuecomment-369373572
+                    // see SVGR issues:
+                    // https://github.com/gregberge/svgr/issues/322
+                    // https://github.com/gregberge/svgr/issues/210
+                    // see SVGO issues:
                     // https://github.com/svg/svgo/issues/674
                     // https://github.com/svg/svgo/issues/1746
+                    //
+                    // Initially, a naive counter was implemented based on this github comment:
+                    // https://github.com/svg/svgo/issues/1746#issuecomment-1803600573
+                    // But while that implementation insured id’s that are unique,
+                    // it did not work in cases where the id is both declared and referenced in the same file,
+                    // because the refernce gets a separate unique ID (a different number from the counter).
+                    //
+                    // The current implementation is based on this github comment:
+                    // https://github.com/svg/svgo/issues/913#issuecomment-369373572
+                    // Generates a hash of the filepath of the svg file, resulting in a prefix which is:
+                    // - Short,
+                    // - With characters valid for IDs,
+                    // - The same within a file,
+                    // - And different in different files.
                     {
                       name: 'prefixIds',
                       params: {
                         delim: '',
-                        prefix: () => svgoPrefixIdsCount++,
+                        prefix: (_: XastElement, info: PluginInfo) =>
+                          `svg${crypto
+                            .createHash('shake256', { outputLength: 6 })
+                            .update(info.path || '')
+                            .digest('hex')}`,
                       },
                     },
                   ],
