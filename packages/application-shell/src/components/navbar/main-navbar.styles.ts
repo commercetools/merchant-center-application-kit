76aa898acfb14f9ce6127dc5e837adf9390e3272
Kacper Krzywiec <krzywiec@gmail.com>
2024-02-12 15:01:20 +0100

    fix: correct width

--- packages/application-shell/src/components/navbar/main-navbar.styles.ts
+++ packages/application-shell/src/components/navbar/main-navbar.styles.ts
@@ -87,8 +87,7 @@ const leftNavigationOpenStyles = css`
     height: auto;
     min-height: ${NAVBAR.itemSize};
     width: calc(
-      ${NAVBAR.sublistIndentationWhenExpanded} - 2 *
-        ${uiKitDesignTokens.spacing30}
+      ${NAVBAR.sublistIndentationWhenExpanded} - ${uiKitDesignTokens.spacing30}
     );
 
     &.active {
