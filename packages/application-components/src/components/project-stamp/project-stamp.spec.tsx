a82d4048ee20cdd826e360c6536e7a605016932a
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2024-02-21 11:05:03 +0100

    chore: updated test

--- packages/application-components/src/components/project-stamp/project-stamp.spec.tsx
+++ packages/application-components/src/components/project-stamp/project-stamp.spec.tsx
@@ -12,7 +12,17 @@ describe('rendering', () => {
 
     expect(screen.getByText('Trial expired')).toBeInTheDocument();
   });
-  it('should render ProjectStamp - WillExpire with correct days left', () => {
+  it('should render ProjectStamp - WillExpire with 0 days left', () => {
+    renderComponent(<ProjectStamp.WillExpire daysLeft={0} />);
+
+    expect(screen.getByText('Trial ends today')).toBeInTheDocument();
+  });
+  it('should render ProjectStamp - WillExpire with 1 days left', () => {
+    renderComponent(<ProjectStamp.WillExpire daysLeft={1} />);
+
+    expect(screen.getByText('Trial ends in 1 day')).toBeInTheDocument();
+  });
+  it('should render ProjectStamp - WillExpire with 4 days left', () => {
     renderComponent(<ProjectStamp.WillExpire daysLeft={4} />);
 
     expect(screen.getByText('Trial ends in 4 days')).toBeInTheDocument();
