adddcb2efd3211c0637a91b96b14c424a3106c4d
Nicola Molinari <nicola.molinari@commercetools.com>
2024-03-05 12:48:46 +0100

    refactor(i18n): change transifex file format to structured json

--- packages/i18n/scripts/compile_data.sh
+++ packages/i18n/scripts/compile_data.sh
@@ -5,7 +5,7 @@ set -e
 export NODE_ENV="production"
 
 echo "Compiling i18n messages data."
-pnpm formatjs compile-folder --format=./transifex-transformer.js --ast data compiled-data
+pnpm formatjs compile-folder --format=./transifex-compiler.js --ast data compiled-data
 
 echo "Generating moment locale imports."
 node scripts/generate-moment-locale-imports.js
