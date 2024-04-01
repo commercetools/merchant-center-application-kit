603e9d5e4380993e8a2304d2976fa7dfd935284b
Carlos Cortizas <carlos.martines@commercetools.com>
2024-04-01 18:17:08 +0200

    refactor(jest-preset-mc-app): make new msw version play nice with jsdom

--- packages/jest-preset-mc-app/module-exports-resolver.js
+++ packages/jest-preset-mc-app/module-exports-resolver.js
@@ -18,6 +18,18 @@ module.exports = (path, options) => {
         delete pkg['exports'];
         delete pkg['module'];
       }
+
+      // Latest version of MSW uses ESM exports, but does not play well with JSDOM.
+      // Basically we need to hack it here so tests use the Node version of MSW instead
+      // of the browser one, which is what happens by default because of JSDOM.
+      // More info here: https://github.com/mswjs/msw/issues/1786
+      if (pkg.name === 'msw') {
+        delete pkg.exports['./node'].browser;
+      }
+      if (pkg.name === '@mswjs/interceptors') {
+        delete pkg.exports;
+      }
+
       return pkg;
     },
   });
