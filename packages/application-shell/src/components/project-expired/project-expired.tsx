2531ac36f80b28616955c90dbae68d275c5249e2
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2024-04-19 12:15:33 +0200

    chore: project-expired illustration

--- packages/application-shell/src/components/project-expired/project-expired.tsx
+++ packages/application-shell/src/components/project-expired/project-expired.tsx
@@ -1,8 +1,6 @@
 import { useIntl, FormattedMessage } from 'react-intl';
 import { MaintenancePageLayout } from '@commercetools-frontend/application-components';
-import ProjectExpiredIllustrationRebranding from '@commercetools-frontend/assets/images/hourglass-rebranding.svg';
 import ProjectExpiredIllustration from '@commercetools-frontend/assets/images/hourglass.svg';
-import { useTheme } from '@commercetools-uikit/design-system';
 import ServicePageProjectSwitcher from '../service-page-project-switcher';
 import messages from './messages';
 
@@ -19,14 +17,10 @@ EmailLink.displayName = 'EmailLink';
 
 const ProjectExpired = () => {
   const intl = useIntl();
-  const { themedValue } = useTheme();
 
   return (
     <MaintenancePageLayout
-      imageSrc={themedValue(
-        ProjectExpiredIllustration,
-        ProjectExpiredIllustrationRebranding
-      )}
+      imageSrc={ProjectExpiredIllustration}
       title={<FormattedMessage {...messages.title} />}
       label={intl.formatMessage(messages.title)}
       paragraph1={
