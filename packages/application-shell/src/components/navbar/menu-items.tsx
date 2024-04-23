383678613d5e58977b6316dc95c0a3c63355c839
Douglas Egiemeh <douglasegiemeh@gmail.com>
2024-04-23 12:36:41 +0200

    feat(navbar keyboard navigation): optionally navigate to submenu items using the `Tab` and `Enter` keys. (#3482)
    * feat(navbar keyboard navigation): add arrow keys navigation to navbar
    
    * feat(navbar keyboard navigation): add arrow keys navigation to navbar
    
    * feat(navbar keyboard navigation): add event types
    
    * feat(navbar keyboard navigation): cleanup unused code
    
    * feat(navbar keyboard navigation): make Tab and Enter key primarily for keyboard navigation
    
    * feat(navbar keyboard navigation): make Tab and Enter key primarily for keyboard navigation
    
    * feat(navbar keyboard navigation): tabIndex to focus submenu container
    
    * feat(navbar keyboard navigation): update changeset
    
    * feat(navbar keyboard navigation): rename state variable
    
    * feat(navbar keyboard navigation): prevent default enter behaviour
    
    * feat(navbar keyboard navigation): handle submenu state after onBlur
    
    * feat(keyboard navigation): add submenu preview on navigation
    
    * feat(keyboard navigation): remove unused variables
    
    * feat(accessibility): remove unused hook
    
    * feat(main nav accessibility): update focus when navigating to a new submenu
    
    ---------
    
    Co-authored-by: Ddouglasz <douglas.egiemeh@gmail.com>

--- packages/application-shell/src/components/navbar/menu-items.tsx
+++ packages/application-shell/src/components/navbar/menu-items.tsx
@@ -173,6 +173,7 @@ export type MenuGroupProps = {
   children?: ReactNode;
   submenuVerticalPosition?: number;
   isSubmenuAboveMenuItem?: boolean;
+  handleKeyDown?: React.KeyboardEventHandler<HTMLUListElement>;
 };
 
 const MenuGroup = forwardRef<HTMLUListElement, MenuGroupProps>((props, ref) => {
@@ -189,6 +190,7 @@ const MenuGroup = forwardRef<HTMLUListElement, MenuGroupProps>((props, ref) => {
   const isSublistActiveWhileIsMenuCollapsed = Boolean(
     props.level === 2 && props.isActive && !props.isExpanded
   );
+
   return (
     <MenuList
       ref={ref && props.level === 2 ? ref : null}
@@ -200,6 +202,7 @@ const MenuGroup = forwardRef<HTMLUListElement, MenuGroupProps>((props, ref) => {
         isSublistActiveWhileIsMenuExpanded ||
         isSublistActiveWhileIsMenuCollapsed
       }
+      onKeyDown={props.handleKeyDown}
       className={classnames(
         {
           'sublist-expanded__active': isSublistActiveWhileIsMenuExpanded,
@@ -249,6 +252,7 @@ type MenuItemProps = {
     | FocusEventHandler<HTMLElement>;
   children: ReactNode;
   identifier?: string;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLLIElement>) => void;
 };
 const MenuItem = (props: MenuItemProps) => {
   return (
@@ -259,6 +263,7 @@ const MenuItem = (props: MenuItemProps) => {
       onMouseLeave={props.onMouseLeave as MouseEventHandler<HTMLElement>}
       onFocus={props.onMouseEnter as FocusEventHandler<HTMLElement>}
       onBlur={props.onMouseLeave as FocusEventHandler<HTMLElement>}
+      onKeyDown={props.onKeyDown}
       data-menuitem={props.identifier}
       className={classnames({
         active: props.isActive,
@@ -281,6 +286,7 @@ export type MenuItemLinkProps = {
   onClick?: (event: SyntheticEvent<HTMLAnchorElement>) => void;
   useFullRedirectsForLinks?: boolean;
   isSubmenuLink?: boolean;
+  isSubmenuFocused?: boolean;
 };
 const menuItemLinkDefaultProps: Pick<MenuItemLinkProps, 'exactMatch'> = {
   exactMatch: false,
@@ -306,6 +312,7 @@ const MenuItemLink = (props: MenuItemLinkProps) => {
           activeClassName="highlighted"
           data-link-level={linkLevel}
           css={getMenuItemLinkStyles(Boolean(props.isSubmenuLink))}
+          tabIndex={props.isSubmenuLink && !props.isSubmenuFocused ? -1 : 0}
           onClick={(event) => {
             if (props.linkTo && props.useFullRedirectsForLinks) {
               event.preventDefault();
