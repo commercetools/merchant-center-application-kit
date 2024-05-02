4b5b6806b3e710ad2aa71c499d22216210ae1e49
Doğa Yüksel <doga.yuksel@commercetools.de>
2024-05-02 12:00:59 +0300

    feat(message-loader): compile i18n messages during webpack build (#3496)
    * feat(message-loader): introduce a simple loader for webpack
    
    * feat(message-loader): add content sniffing and a simple check
    
    * test(message-loader): add check for loading an empty object
    
    * fix(message-loader): improve naming and directory structure
    
    * test(i18n-message-compilation): use inlineSnapshots for assertions
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Tobias Deekens <tobias.deekens@commercetools.com>

---
+++ packages/mc-scripts/src/webpack-loaders/i18n-message-compilation-loader.ts
@@ -0,0 +1,18 @@
+import { compile } from '@formatjs/cli-lib';
+import type { LoaderDefinitionFunction } from 'webpack';
+
+const i18nMessageCompilationLoader: LoaderDefinitionFunction = function (
+  source
+) {
+  const callback = this.async();
+
+  const messageContents = JSON.parse(source);
+  const firstValue = Object.values(messageContents)[0];
+  const format = typeof firstValue === 'string' ? 'simple' : 'transifex';
+
+  compile([this.resourcePath], { ast: true, format }).then((result) => {
+    callback(null, result);
+  });
+};
+
+export default i18nMessageCompilationLoader;
