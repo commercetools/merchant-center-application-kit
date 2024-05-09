179da9a4fdc73ade7717b50bc1e5c106382bf9eb
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2024-05-09 09:18:35 +0200

    Remove rebranding modal pop-up to announce visual changes (#3516)
    * chore: removing rebranding modal
    
    * chore: remove local storage value for rebranding modal

--- packages/application-shell/src/components/theme-switcher/theme-switcher.tsx
+++ packages/application-shell/src/components/theme-switcher/theme-switcher.tsx
@@ -1,17 +1,11 @@
-import { lazy } from 'react';
 import { themesOverrides } from '@commercetools-frontend/application-components';
 import { ThemeProvider } from '@commercetools-uikit/design-system';
-
-const NewColoursSchemaAnnouncementDialog = lazy(
-  () => import('./new-colors-schema-announcement-dialog')
-);
 
 const ThemeSwitcher = () => {
   const theme = 'default';
   return (
     <>
       <ThemeProvider theme={theme} themeOverrides={themesOverrides[theme]} />
-      <NewColoursSchemaAnnouncementDialog />
     </>
   );
 };
