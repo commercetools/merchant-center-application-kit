80e8d4bdfa3cf72a78019b57a90d254b3297e4e0
Tyler Ford <tyler.ford@commercetools.com>
2024-10-09 13:23:00 -0600

    chore(app-kit): remove workspaces button and gradient background

---
+++ .changeset/nervous-kangaroos-rush.md
@@ -0,0 +1,7 @@
+---
+'@commercetools-frontend/application-components': minor
+'@commercetools-frontend/application-shell': minor
+'@commercetools-frontend/constants': minor
+---
+
+Removes the workspaces button from app-bar, and the workspaces gradient background from public page layout.
--- packages/application-shell/src/components/workspaces-navigation-button/messages.ts
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  tooltip: {
-    id: 'WorkspacesNavigationButton.tooltip',
-    description: 'The tooltip for the Workspaces navigation button',
-    defaultMessage: 'Switch between commercetools products',
-  },
-});
