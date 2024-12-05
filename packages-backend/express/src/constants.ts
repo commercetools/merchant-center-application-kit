57ef01b81e27f1ae5e5a1f2c18ed27f1eb9cb1be
Nicola Molinari <nicola.molinari@commercetools.com>
2024-12-05 17:08:21 +0100

    refactor: remove unsupported cloud identifiers (#3666)

--- packages-backend/express/src/constants.ts
+++ packages-backend/express/src/constants.ts
@@ -12,34 +12,28 @@ export const CLOUD_IDENTIFIERS = {
    * @deprecated: use `AWS_US` instead
    */
   AWS_OHIO: 'aws-ohio',
-  /**
-   * @deprecated: not supported anymore
-   */
-  AWS_CN: 'aws-cn',
-  AZURE_US: 'azure-us',
-  AZURE_EU: 'azure-eu',
 } as const;
 
 export const MC_API_URLS = {
-  GCP_AU: 'https://mc-api.australia-southeast1.gcp.commercetools.com',
-  GCP_EU: 'https://mc-api.europe-west1.gcp.commercetools.com',
-  GCP_US: 'https://mc-api.us-central1.gcp.commercetools.com',
-  AWS_EU: 'https://mc-api.eu-central-1.aws.commercetools.com',
-  AWS_US: 'https://mc-api.us-east-2.aws.commercetools.com',
+  [CLOUD_IDENTIFIERS.GCP_AU]:
+    'https://mc-api.australia-southeast1.gcp.commercetools.com',
+  [CLOUD_IDENTIFIERS.GCP_EU]:
+    'https://mc-api.europe-west1.gcp.commercetools.com',
+  [CLOUD_IDENTIFIERS.GCP_US]:
+    'https://mc-api.us-central1.gcp.commercetools.com',
+  [CLOUD_IDENTIFIERS.AWS_EU]:
+    'https://mc-api.eu-central-1.aws.commercetools.com',
+  [CLOUD_IDENTIFIERS.AWS_US]: 'https://mc-api.us-east-2.aws.commercetools.com',
   /**
    * @deprecated: use `AWS_EU` instead
    */
-  AWS_FRA: 'https://mc-api.eu-central-1.aws.commercetools.com',
+  [CLOUD_IDENTIFIERS.AWS_FRA]:
+    'https://mc-api.eu-central-1.aws.commercetools.com',
   /**
    * @deprecated: use `AWS_US` instead
    */
-  AWS_OHIO: 'https://mc-api.us-east-2.aws.commercetools.com',
-  /**
-   * @deprecated: not supported anymore
-   */
-  AWS_CN: 'https://mc-api.cn-northwest-1.aws.commercetools.cn',
-  AZURE_US: 'https://mc-api.eastus.azure.commercetools.com',
-  AZURE_EU: 'https://mc-api.germanywestcentral.azure.commercetools.com',
+  [CLOUD_IDENTIFIERS.AWS_OHIO]:
+    'https://mc-api.us-east-2.aws.commercetools.com',
 } as const;
 
 export const MC_API_PROXY_HEADERS = {
