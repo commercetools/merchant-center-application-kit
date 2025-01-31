c9a5fce64a2b8df6461ba4747046fd2610698724
Carlos Cortizas <carlos.martines@commercetools.com>
2025-01-31 14:38:25 +0100

    chore: added code comments

--- packages/jest-preset-mc-app/setup-tests.js
+++ packages/jest-preset-mc-app/setup-tests.js
@@ -12,6 +12,8 @@ global.Headers = global.Headers || require('node-fetch').Headers;
 global.Request = global.Request || require('node-fetch').Request;
 global.Response = global.Response || require('node-fetch').Response;
 
+// The jsdom version we're using does not support the crypto.randomUUID function, so we need to mock it.
+// We need to wait to use Jest v30, which uses a more recent version of jsdom, to remove this mock.
 global.crypto.randomUUID = () => uuid.v4();
 
 // Fix missing globals when `jsdom` is used in a test environment.
