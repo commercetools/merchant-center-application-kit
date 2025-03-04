bf5940519752869572bd613cfc167ccd07614408
Nicola Molinari <nicola.molinari@commercetools.com>
2025-03-04 20:51:05 +0100

    chore: upgrade dompurify to v3

---
+++ .changeset/loud-beans-drum.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-frontend/application-config': patch
+---
+
+Upgrade `dompurify` to v3
--- packages/application-config/src/sanitize-svg.ts
+++ packages/application-config/src/sanitize-svg.ts
@@ -2,16 +2,14 @@ import createDOMPurify from 'dompurify';
 import { JSDOM } from 'jsdom';
 
 const jsdom = new JSDOM('');
-// @ts-expect-error: jsdom returns DOMWindow, which doesn't match Window dompurify expects
 const DOMPurify = createDOMPurify(jsdom.window);
 
 export default function sanitizeSvg(data: string) {
   return DOMPurify.sanitize(data, {
     USE_PROFILES: { svg: true },
-    RETURN_DOM: true,
     FORBID_ATTR: [
       // To avoid injection by using `style="filter:url(\"data:image/svg+xml,<svg`
       'style',
     ],
-  }).innerHTML;
+  });
 }
