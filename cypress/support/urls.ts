a862d7d2f2513f309a693486f184cf32d2609b56
Muhammad Mustafa Asif <mustafa.asif@commercetools.com>
2025-05-02 12:50:34 +0200

    chore: reverted state machine id

--- cypress/support/urls.ts
+++ cypress/support/urls.ts
@@ -5,7 +5,7 @@ export const URL_BASE = `/${projectKey}`;
 export const ENTRY_POINT_APP_KIT_PLAYGROUND = 'app-kit-playground';
 export const URL_APP_KIT_PLAYGROUND = `${URL_BASE}/${ENTRY_POINT_APP_KIT_PLAYGROUND}`;
 export const URL_APP_KIT_PLAYGROUND_NOTIFICATIONS = `${URL_APP_KIT_PLAYGROUND}/notifications`;
-export const URL_APP_KIT_PLAYGROUND_STATE_MACHINES_ID = `${URL_APP_KIT_PLAYGROUND}/3309e9d0-fb15-4056-afa7-dc12f31ea0ad`;
+export const URL_APP_KIT_PLAYGROUND_STATE_MACHINES_ID = `${URL_APP_KIT_PLAYGROUND}/12ad40eb-b33f-4e0e-ae91-bca373ccfd58`;
 export const URL_APP_KIT_PLAYGROUND_DATE_FORMATTERS = `${URL_APP_KIT_PLAYGROUND}/formatters`;
 
 export const ENTRY_POINT_TEMPLATE_STARTER = 'template-starter';
