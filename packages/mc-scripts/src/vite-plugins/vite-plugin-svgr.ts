d4cad023bdf589bf43aadb6d4f61fcf8724b7093
Tyler Ford <tyler.ford@commercetools.com>
2025-05-13 17:08:50 -0600

    fix(svgr): add separate function for optimization

--- packages/mc-scripts/src/vite-plugins/vite-plugin-svgr.ts
+++ packages/mc-scripts/src/vite-plugins/vite-plugin-svgr.ts
@@ -13,11 +13,76 @@ function vitePluginSvgr(): Plugin {
     name: 'vite-plugin-svgr',
     async transform(_code, id) {
       if (filter(id)) {
+        console.log(`Transforming ${id}...`);
         const { transform } = await import('@svgr/core');
         const svgCode = await fs.promises.readFile(id, 'utf8');
 
+        const optimizeEmbeddedPngs = async (code: string) => {
+          const pngRegex =
+            /xlink:href="data:image\/png;base64,([a-zA-Z0-9+/]+=*)"/gs;
+          let modifiedCode = code; // Create a mutable copy of the code
+
+          const matchesIterator = code.matchAll(pngRegex);
+
+          for (const match of matchesIterator) {
+            const originalBase64 = match[1]; // Access the captured Base64 string (group 1)
+
+            if (originalBase64) {
+              try {
+                console.log(
+                  'originalPngString length: ',
+                  originalBase64.length
+                );
+
+                const pngBuffer = Buffer.from(originalBase64, 'base64');
+                const sharp = (await import('sharp')).default;
+
+                const optimizedBuffer = await sharp(pngBuffer)
+                  .png({
+                    quality: 80,
+                    compressionLevel: 9,
+                  })
+                  .toBuffer();
+                const optimizedPngBase64 = optimizedBuffer.toString('base64');
+                console.log(
+                  'optimizedPngString length: ',
+                  optimizedPngBase64.length
+                );
+
+                // Construct the new xlink:href value
+                const originalMatch = match[0];
+                const optimizedXlinkHref = originalMatch.replace(
+                  originalBase64,
+                  optimizedPngBase64
+                );
+
+                // Replace the original xlink:href with the optimized one
+                modifiedCode = modifiedCode.replace(
+                  originalMatch,
+                  optimizedXlinkHref
+                );
+              } catch (error) {
+                console.error(
+                  'Error processing embedded PNG:',
+                  error,
+                  'Original Base64:',
+                  originalBase64
+                );
+              }
+            }
+            console.log('------');
+            console.log('code length: ', code.length);
+            console.log('modifiedCode length: ', modifiedCode.length);
+            console.log('------');
+          }
+          return modifiedCode; // Return the modified code
+        };
+
+        const optimizedSVGCode = await optimizeEmbeddedPngs(svgCode);
+        // console.log('optimizedSVGCode: ', optimizedSVGCode);
+        console.log('optimizedSVGCode length: ', optimizedSVGCode.length);
         const componentCode = await transform(
-          svgCode,
+          optimizedSVGCode,
           {
             icon: false,
             svgoConfig: {
@@ -34,6 +99,8 @@ function vitePluginSvgr(): Plugin {
                         );
                         // potentially large string embedded in the SVG
                         const originalPngString = valueString.split(',')[1];
+                        // delete the original embedded image
+                        delete ast.attributes['xlink:href'];
                         const pngBuffer = Buffer.from(
                           originalPngString,
                           'base64'
@@ -43,7 +110,7 @@ function vitePluginSvgr(): Plugin {
 
                         const optimizedBuffer = await sharp(pngBuffer)
                           .png({
-                            quality: 70, // Adjust quality (0-100)
+                            quality: 80, // Adjust quality (0-100)
                             compressionLevel: 9, // Adjust compression (0-9, 9 is best)
                           })
                           .toBuffer();
@@ -51,9 +118,8 @@ function vitePluginSvgr(): Plugin {
                         const optimizedPngString =
                           optimizedBuffer.toString('base64');
 
-                        delete ast.attributes['xlink:href'];
                         ast.attributes['xlink:href'] =
-                          'data:image/png;base64, ' + optimizedPngString;
+                          'data:image/png;base64,' + optimizedPngString;
                       }
                       // TODO: understand why the build includes the original png
                     }
@@ -114,6 +180,7 @@ function vitePluginSvgr(): Plugin {
             },
           }
         );
+        console.log('componentCode length: ', componentCode.length);
 
         const res = await transformWithEsbuild(componentCode, id, {
           loader: 'jsx',
