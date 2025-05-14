df3363b3912cfc94f07c3491687895d2dbb39bd3
Tyler Ford <tyler.ford@commercetools.com>
2025-05-14 17:53:14 -0600

    fix(svgr): remove debug logs and add cleanbuild plugin

--- packages/mc-scripts/src/commands/build-vite.ts
+++ packages/mc-scripts/src/commands/build-vite.ts
@@ -15,6 +15,7 @@ import pluginI18nMessageCompilation from '../vite-plugins/vite-plugin-i18n-messa
 import pluginSvgr from '../vite-plugins/vite-plugin-svgr';
 
 async function run() {
+  const CleanBuild = (await import('vite-plugin-clean-build')).default;
   const DEFAULT_PORT = parseInt(String(process.env.HTTP_PORT), 10) || 3001;
 
   // Ensure the `/public` folder exists.
@@ -104,6 +105,13 @@ async function run() {
               : undefined,
           ].filter(nonNullable),
         },
+      }),
+      // cleanbuild is needed to remove large svg assets
+      // that are not used after the build.
+      CleanBuild({
+        outputDir: paths.appBuild,
+        patterns: ['*.react-*.svg'],
+        verbose: true, // logs the files that are removed
       }),
       pluginDynamicBaseAssetsGlobals(),
       pluginI18nMessageCompilation(),
