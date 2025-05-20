1018a287ca41dda2d1013c0d1e392006ec2f7bdb
Tyler Ford <tyler.ford@commercetools.com>
2025-05-20 17:28:04 -0600

    fix(svgr): check env to enable svg optimization

--- packages/mc-scripts/src/vite-plugins/vite-plugin-svgr.ts
+++ packages/mc-scripts/src/vite-plugins/vite-plugin-svgr.ts
@@ -6,6 +6,7 @@ import fs from 'fs';
 import { createFilter } from '@rollup/pluginutils';
 import type { XastElement, PluginInfo } from 'svgo';
 import { transformWithEsbuild, type Plugin } from 'vite';
+import optimizeEmbeddedPngs from '../utils/optimize-embedded-pngs';
 
 function vitePluginSvgr(): Plugin {
   const filter = createFilter('**/*.react.svg');
@@ -16,59 +17,17 @@ function vitePluginSvgr(): Plugin {
         const { transform } = await import('@svgr/core');
         const svgCode = await fs.promises.readFile(id, 'utf8');
 
-        const optimizeEmbeddedPngs = async (code: string) => {
-          const pngRegex = /href="data:image\/png;base64,([a-zA-Z0-9+/]+=*)"/gs;
-          let modifiedCode = code;
-          const matchesIterator = code.matchAll(pngRegex);
+        let transformableSVGCode;
+        if (process.env.ENABLE_EXPERIMENTAL_SVG_PNG_OPTIMIZATION) {
+          // This is a temporary fix for the issue with large embedded PNGs
+          // in SVGs. It will be removed once we have a better solution.
+          transformableSVGCode = await optimizeEmbeddedPngs(svgCode, id);
+        } else {
+          transformableSVGCode = svgCode;
+        }
 
-          for (const match of matchesIterator) {
-            const originalBase64 = match[1];
-
-            if (originalBase64) {
-              // This length would indicate  that the match is greater than ~1mb
-              if (originalBase64.length > 1000000) {
-                const fileName = id.split('/').pop();
-                console.warn(
-                  `\n🚨 You have a large png embedded in ${fileName} - consider using an image tag instead 🚨`
-                );
-                try {
-                  const pngBuffer = Buffer.from(originalBase64, 'base64');
-                  const sharp = (await import('sharp')).default;
-                  const optimizedBuffer = await sharp(pngBuffer)
-                    .png({
-                      quality: 10,
-                      compressionLevel: 9,
-                    })
-                    .toBuffer();
-                  const optimizedPngBase64 = optimizedBuffer.toString('base64');
-                  // Construct the new href value
-                  const originalMatch = match[0];
-                  const optimizedXlinkHref = originalMatch.replace(
-                    originalBase64,
-                    optimizedPngBase64
-                  );
-                  // Replace the original href with the optimized one
-                  modifiedCode = modifiedCode.replace(
-                    originalMatch,
-                    optimizedXlinkHref
-                  );
-                } catch (error) {
-                  console.error(
-                    'Error processing embedded PNG:',
-                    error,
-                    'Original Base64:',
-                    originalBase64
-                  );
-                }
-              }
-            }
-          }
-          return modifiedCode;
-        };
-
-        const optimizedSVGCode = await optimizeEmbeddedPngs(svgCode);
         const componentCode = await transform(
-          optimizedSVGCode,
+          transformableSVGCode,
           {
             icon: false,
             svgoConfig: {
