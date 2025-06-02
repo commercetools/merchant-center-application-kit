cd5f3d3b85ddd060b4c3fd62a341749dd7ac2c0e
Carlos Cortizas <carlos.martines@commercetools.com>
2025-06-02 20:20:54 +0200

    refactor(application-components): ignore log errors in test

--- packages/application-components/src/components/custom-views/custom-view-loader/custom-view-loader.spec.tsx
+++ packages/application-components/src/components/custom-views/custom-view-loader/custom-view-loader.spec.tsx
@@ -18,6 +18,7 @@ jest.mock('@commercetools-frontend/sentry');
 jest.mock('@commercetools-frontend/actions-global', () => ({
   useShowNotification: () => mockShowNotification,
 }));
+jest.spyOn(console, 'log').mockImplementation(() => {});
 
 // TODO: We must add this entity to the test data repository
 const TEST_CUSTOM_VIEW: CustomViewData = {
