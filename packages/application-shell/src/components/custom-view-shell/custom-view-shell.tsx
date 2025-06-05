056a45f1f10029998bde7d29a2c70026ec1dd807
Tobias Deekens <tobias.deekens@commercetools.com>
2025-06-05 09:37:42 +0200

    docs: improve wording on comment
    Co-authored-by: Kacper Krzywiec <49066275+kark@users.noreply.github.com>

--- packages/application-shell/src/components/custom-view-shell/custom-view-shell.tsx
+++ packages/application-shell/src/components/custom-view-shell/custom-view-shell.tsx
@@ -203,7 +203,7 @@ function CustomViewShell(props: TCustomViewShellProps) {
     window.addEventListener('message', bootstrapMessageHandler);
 
     // This notifies the custom view loader that the iframe is ready.
-    // This is only supported by custom view loader in the host application older than v24.x
+    // This is only supported by custom view loader in the host application from v24.x onwards
     window.parent.postMessage({
       origin: window.location.origin,
       eventName: CUSTOM_VIEWS_EVENTS_NAMES.CUSTOM_VIEW_READY,
