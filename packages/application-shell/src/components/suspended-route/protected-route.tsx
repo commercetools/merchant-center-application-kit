ef412d32566babaf33450eed2064cc21af75fc0d
Tobias Deekens <tobias.deekens@commercetools.com>
2025-06-18 17:38:55 +0200

    Refactor `SuspendedProctedRoute` to support loader and fallback component (#3800)

--- packages/application-shell/src/components/suspended-route/protected-route.tsx
+++ packages/application-shell/src/components/suspended-route/protected-route.tsx
@@ -3,18 +3,16 @@ import { PageUnauthorized } from '@commercetools-frontend/application-components
 
 type TProtectedRouteProps = {
   condition: boolean;
+  fallback?: React.ReactNode;
 } & RouteProps;
 
 const ProtectedRoute: React.FC<TProtectedRouteProps> = ({
   condition,
+  fallback = <PageUnauthorized />,
   children,
   ...routeProps
 }) => {
-  return condition ? (
-    <Route {...routeProps}>{children}</Route>
-  ) : (
-    <PageUnauthorized />
-  );
+  return condition ? <Route {...routeProps}>{children}</Route> : <>fallback</>;
 };
 
 export { ProtectedRoute, type TProtectedRouteProps };
--- packages/application-shell/src/components/suspended-route/suspended-protected-route.tsx
+++ packages/application-shell/src/components/suspended-route/suspended-protected-route.tsx
@@ -1,18 +1,21 @@
 import { Suspense } from 'react';
+import { PageUnauthorized } from '@commercetools-frontend/application-components';
 import LoadingSpinner from '@commercetools-uikit/loading-spinner';
 import { ProtectedRoute, type TProtectedRouteProps } from './protected-route';
 
 type TSuspendedProtectedRouteProps = TProtectedRouteProps & {
+  loading?: React.ReactNode;
   fallback?: React.ReactNode;
 };
 
 const SuspendedProtectedRoute: React.FC<TSuspendedProtectedRouteProps> = ({
-  fallback = <LoadingSpinner />,
+  loading = <LoadingSpinner />,
+  fallback = <PageUnauthorized />,
   ...routeProps
 }) => {
   return (
-    <Suspense fallback={fallback}>
-      <ProtectedRoute {...routeProps} />
+    <Suspense fallback={loading}>
+      <ProtectedRoute fallback={fallback} {...routeProps} />
     </Suspense>
   );
 };
