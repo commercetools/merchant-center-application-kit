0e88a4cb060832cd5e179bc6501a7730d35925f7
Jonathan Creasman <jonathan.creasman@commercetools.com>
2025-08-06 14:23:14 -0400

    ci: update Codecov workflow based on PR feedback

--- playground/custom-application-config.mjs
+++ playground/custom-application-config.mjs
@@ -36,7 +36,6 @@ const config = {
   headers: {
     csp: {
       'connect-src': ['${env:HOST_GCP_STAGING}'],
-      'script-src': ["'unsafe-eval'"],
     },
   },
   icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
