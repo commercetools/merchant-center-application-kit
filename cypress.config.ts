f695324c15ef40d84fa8d7ad18dc0eebc825e356
Nicola Molinari <nicola.molinari@commercetools.com>
2025-08-07 10:17:38 +0200

    refactor(cypress): do not record video

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,8 +7,7 @@ import {
 
 export default defineConfig({
   retries: 1,
-  screenshotOnRunFailure: true,
-  video: true,
+  video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
