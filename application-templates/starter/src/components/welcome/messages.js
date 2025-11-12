82a806f1dab468766d39d48f8a3fb4a4e33187c8
Valorie <valorie.carli@commercetools.com>
2025-11-12 15:51:24 -0600

    test(msg): update welcome msg

--- application-templates/starter/src/components/welcome/messages.js
+++ application-templates/starter/src/components/welcome/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   title: {
     id: 'Welcome.title',
-    defaultMessage: 'Develop applications for the Merchant Center',
+    defaultMessage: 'sup?',
   },
   subtitle: {
     id: 'Welcome.subtitle',
