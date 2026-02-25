c34b6bef7a0b5e7379b02aa953180b14e463ce50
Byron Wall <byron.wall@commercetools.com>
2026-02-25 13:25:05 -0500

    chore(rebase): accept newer babel-preset-mc-app version

--- packages-backend/eslint-config-node/index.js
+++ packages-backend/eslint-config-node/index.js
@@ -1,132 +1,99 @@
-// This is a workaround for https://github.com/eslint/eslint/issues/3458
-require('@rushstack/eslint-patch/modern-module-resolution');
+const babelParser = require('@babel/eslint-parser');
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const prettierConfig = require('eslint-config-prettier');
+const importPlugin = require('eslint-plugin-import');
+const jestPlugin = require('eslint-plugin-jest');
+const nPlugin = require('eslint-plugin-n');
+const prettierPlugin = require('eslint-plugin-prettier');
+const globals = require('globals');
 
 const { statusCode, allSupportedExtensions } = require('./helpers/eslint');
 
 /**
- * @type {import("eslint").Linter.Config}
+ * @type {import("eslint").Linter.Config[]}
  */
-module.exports = {
-  root: true,
+module.exports = [
+  // Base configuration for all JS/TS files
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        sourceType: 'module',
+        requireConfigFile: false,
+        /**
+         * @type {import('@babel/core').TransformOptions}
+         */
+        babelOptions: {
+          presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+        },
+      },
+      globals: {
+        ...globals.commonjs,
+        ...globals.es2015,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      import: importPlugin,
+      jest: jestPlugin,
+      n: nPlugin,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: allSupportedExtensions,
+        },
+      },
+    },
+    rules: {
+      // Nodejs
+      'n/no-missing-import': statusCode.off,
+      'n/no-unsupported-features/es-syntax': statusCode.off,
 
-  parser: '@babel/eslint-parser',
+      // NOTE: The regular rule does not support do-expressions. The equivalent rule of babel does.
+      'no-unused-expressions': statusCode.off,
 
-  parserOptions: {
-    sourceType: 'module',
-    requireConfigFile: false,
-    /**
-     * @type {import('@babel/core').TransformOptions}
-     */
-    babelOptions: {
-      presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+      // Imports
+      'import/extensions': [
+        statusCode.error,
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+          mjs: 'never',
+          json: 'always',
+          svg: 'always',
+          graphql: 'always',
+        },
+      ],
+      'import/default': statusCode.off,
+      'import/first': statusCode.error,
+      // TODO: enable this once there is support for `import type`
+      // 'import/order': statusCode.error,
+      'import/named': statusCode.off,
+      'import/namespace': statusCode.off,
+      'import/no-extraneous-dependencies': statusCode.off,
+      'import/no-named-as-default': statusCode.off,
+      'import/no-named-as-default-member': statusCode.off,
+      'import/no-unresolved': statusCode.error,
+
+      // Jest
+      'jest/expect-expect': statusCode.off,
+      'jest/no-identical-title': statusCode.warn,
+      'jest/no-focused-tests': statusCode.error,
     },
   },
 
-  env: {
-    browser: false,
-    commonjs: true,
-    es6: true,
-    jest: true,
-    node: true,
-  },
-
-  extends: [
-    'eslint:recommended',
-    // https://github.com/mysticatea/eslint-plugin-n
-    'plugin:n/recommended',
-    // https://github.com/benmosher/eslint-plugin-import
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    // https://github.com/jest-community/eslint-plugin-jest
-    'plugin:jest/recommended',
-    // NOTE: this should go last.
-    'prettier',
-  ],
-
-  plugins: [
-    // https://github.com/import-js/eslint-plugin-import
-    'import',
-    // https://github.com/jest-community/eslint-plugin-jest
-    'jest',
-    // https://github.com/prettier/prettier-eslint
-    'prettier',
-  ],
-
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: allSupportedExtensions,
-      },
-    },
-  },
-
-  rules: {
-    // Nodejs
-    'n/no-missing-import': statusCode.off,
-    'n/no-unsupported-features/es-syntax': statusCode.off,
-
-    // NOTE: The regular rule does not support do-expressions. The equivalent rule of babel does.
-    'no-unused-expressions': statusCode.off,
-
-    // Imports
-    'import/extensions': [
-      statusCode.error,
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-        mjs: 'never',
-        json: 'always',
-        svg: 'always',
-        graphql: 'always',
-      },
-    ],
-    'import/default': statusCode.off,
-    'import/first': statusCode.error,
-    // TODO: enable this once there is support for `import type`
-    // 'import/order': statusCode.error,
-    'import/named': statusCode.off,
-    'import/namespace': statusCode.off,
-    'import/no-extraneous-dependencies': statusCode.off,
-    'import/no-named-as-default': statusCode.off,
-    'import/no-named-as-default-member': statusCode.off,
-    'import/no-unresolved': statusCode.error,
-
-    // Jest
-    'jest/expect-expect': statusCode.off,
-    'jest/no-identical-title': statusCode.warn,
-    'jest/no-focused-tests': statusCode.error,
-  },
-
-  overrides: [
-    {
-      files: ['*.{spec,test}.*'],
-      env: {
-        'jest/globals': true,
-      },
-      rules: {
-        'n/no-extraneous-require': [
-          statusCode.error,
-          {
-            allowModules: ['jest-each', 'msw'],
-          },
-        ],
-        // https://github.com/jest-community/eslint-plugin-jest
-        'jest/no-conditional-expect': statusCode.error,
-        'jest/no-identical-title': statusCode.error,
-        'jest/no-interpolation-in-snapshots': statusCode.error,
-        'jest/no-jasmine-globals': statusCode.error,
-        'jest/no-mocks-import': statusCode.error,
-        'jest/valid-describe-callback': statusCode.error,
-        'jest/valid-expect': statusCode.error,
-        'jest/valid-expect-in-promise': statusCode.error,
-        'jest/valid-title': statusCode.warn,
-      },
-    },
-    {
-      files: ['**/*.ts?(x)'],
-      parser: '@typescript-eslint/parser',
+  // TypeScript-specific configuration
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
       parserOptions: {
         ecmaVersion: 2022,
         sourceType: 'module',
@@ -142,79 +109,112 @@ module.exports = {
           ],
         },
       },
-      plugins: ['@typescript-eslint'],
-      rules: {
-        // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
-        'default-case': statusCode.off,
-        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
-        'no-dupe-class-members': statusCode.off,
-        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
-        'no-undef': statusCode.off,
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+    },
+    rules: {
+      // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+      'default-case': statusCode.off,
+      // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+      'no-dupe-class-members': statusCode.off,
+      // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+      'no-undef': statusCode.off,
 
-        // Add TypeScript specific rules (and turn off ESLint equivalents)
-        '@typescript-eslint/consistent-type-assertions': statusCode.warn,
-        'no-array-constructor': statusCode.off,
-        '@typescript-eslint/no-array-constructor': statusCode.warn,
-        'no-redeclare': statusCode.off,
-        '@typescript-eslint/no-redeclare': statusCode.warn,
-        'no-use-before-define': statusCode.off,
-        '@typescript-eslint/no-use-before-define': [
-          statusCode.error,
-          {
-            functions: false,
-            classes: false,
-            variables: false,
-            typedefs: false,
-          },
-        ],
-        'no-unused-expressions': statusCode.off,
-        '@typescript-eslint/no-unused-expressions': [
-          statusCode.error,
-          {
-            allowShortCircuit: true,
-            allowTernary: true,
-            allowTaggedTemplates: true,
-          },
-        ],
-        'no-unused-vars': statusCode.off,
-        '@typescript-eslint/no-unused-vars': [
-          statusCode.warn,
-          {
-            args: 'none',
-            ignoreRestSiblings: true,
-          },
-        ],
-        'no-useless-constructor': statusCode.off,
-        '@typescript-eslint/no-useless-constructor': statusCode.warn,
-
-        // TypeScript
-        '@typescript-eslint/ban-types': statusCode.off,
-        '@typescript-eslint/naming-convention': statusCode.off,
-        '@typescript-eslint/consistent-type-definitions': statusCode.off,
-        '@typescript-eslint/no-explicit-any': statusCode.error,
-        '@typescript-eslint/no-var-requires': statusCode.off,
-        '@typescript-eslint/unbound-method': statusCode.off,
-        '@typescript-eslint/ban-ts-comment': statusCode.off,
-        '@typescript-eslint/explicit-function-return-type': statusCode.off,
-        '@typescript-eslint/explicit-member-accessibility': [
-          statusCode.error,
-          { accessibility: 'no-public' },
-        ],
-        '@typescript-eslint/no-require-imports': statusCode.off,
-        '@typescript-eslint/promise-function-async': statusCode.off,
-      },
-      settings: {
-        'import/parsers': {
-          '@typescript-eslint/parser': allSupportedExtensions,
+      // Add TypeScript specific rules (and turn off ESLint equivalents)
+      '@typescript-eslint/consistent-type-assertions': statusCode.warn,
+      'no-array-constructor': statusCode.off,
+      '@typescript-eslint/no-array-constructor': statusCode.warn,
+      'no-redeclare': statusCode.off,
+      '@typescript-eslint/no-redeclare': statusCode.warn,
+      'no-use-before-define': statusCode.off,
+      '@typescript-eslint/no-use-before-define': [
+        statusCode.error,
+        {
+          functions: false,
+          classes: false,
+          variables: false,
+          typedefs: false,
         },
-        'import/resolver': {
-          'eslint-import-resolver-typescript': true,
-          typescript: {},
-          node: {
-            extensions: allSupportedExtensions,
-          },
+      ],
+      'no-unused-expressions': statusCode.off,
+      '@typescript-eslint/no-unused-expressions': [
+        statusCode.error,
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+      'no-unused-vars': statusCode.off,
+      '@typescript-eslint/no-unused-vars': [
+        statusCode.warn,
+        {
+          args: 'none',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-useless-constructor': statusCode.off,
+      '@typescript-eslint/no-useless-constructor': statusCode.warn,
+
+      // TypeScript
+      '@typescript-eslint/ban-types': statusCode.off,
+      '@typescript-eslint/naming-convention': statusCode.off,
+      '@typescript-eslint/consistent-type-definitions': statusCode.off,
+      '@typescript-eslint/no-explicit-any': statusCode.error,
+      '@typescript-eslint/no-var-requires': statusCode.off,
+      '@typescript-eslint/unbound-method': statusCode.off,
+      '@typescript-eslint/ban-ts-comment': statusCode.off,
+      '@typescript-eslint/explicit-function-return-type': statusCode.off,
+      '@typescript-eslint/explicit-member-accessibility': [
+        statusCode.error,
+        { accessibility: 'no-public' },
+      ],
+      '@typescript-eslint/no-require-imports': statusCode.off,
+      '@typescript-eslint/promise-function-async': statusCode.off,
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': allSupportedExtensions,
+      },
+      'import/resolver': {
+        'eslint-import-resolver-typescript': true,
+        typescript: {},
+        node: {
+          extensions: allSupportedExtensions,
         },
       },
     },
-  ],
-};
+  },
+
+  // Test files configuration
+  {
+    files: ['*.{spec,test}.*'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'n/no-extraneous-require': [
+        statusCode.error,
+        {
+          allowModules: ['jest-each', 'msw'],
+        },
+      ],
+      // https://github.com/jest-community/eslint-plugin-jest
+      'jest/no-conditional-expect': statusCode.error,
+      'jest/no-identical-title': statusCode.error,
+      'jest/no-interpolation-in-snapshots': statusCode.error,
+      'jest/no-jasmine-globals': statusCode.error,
+      'jest/no-mocks-import': statusCode.error,
+      'jest/valid-describe-callback': statusCode.error,
+      'jest/valid-expect': statusCode.error,
+      'jest/valid-expect-in-promise': statusCode.error,
+      'jest/valid-title': statusCode.warn,
+    },
+  },
+
+  // Prettier configuration (must be last)
+  prettierConfig,
+];
